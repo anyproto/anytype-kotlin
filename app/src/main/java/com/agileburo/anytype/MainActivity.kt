e16a241551266c7981e67f651e658a948f667c93
Konstantin <frankiet8702@gmail.com>
2019-02-20 21:02:55 +0300

    Init commit

---
+++ app/src/main/java/com/agileburo/anytype/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.agileburo.anytype
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ebolo.krichtexteditor.fragments.kRichEditorFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnART.setOnClickListener {
+            val editorFragment = kRichEditorFragment{
+            }
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, editorFragment)
+                .commit()
+
+        }
+    }
+}

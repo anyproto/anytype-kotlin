cfa5193b90a903e10c0f16740242fbae3f5fb845
Ivanov Konstantin <Ivanov.ky@tutu.ru>
2019-03-10 14:27:05 +0300

    show usage of editor toolbar + editor text watcher

--- app/src/main/java/com/agileburo/anytype/FragmentB.kt
+++ app/src/main/java/com/agileburo/anytype/FragmentB.kt
@@ -5,8 +5,19 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.agileburo.anytype.editor.EditorTextWatcher
+import com.agileburo.anytype.ui.EditorToolbar
+import kotlinx.android.synthetic.main.fragment_b.*
 
-class FragmentB: Fragment(){
+class FragmentB : Fragment() {
+
+    private var editorToolbar: EditorToolbar? = null
+    private lateinit var editorTextWatcher: EditorTextWatcher
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        editorTextWatcher = EditorTextWatcher()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +25,23 @@ class FragmentB: Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editText.addTextChangedListener(editorTextWatcher)
+
+        editorToolbar = view.findViewById(R.id.editor_toolbar)
+        editorToolbar!!.setMainActions(
+            { editorTextWatcher.isBoldActive = it },
+            { editorTextWatcher.isItalicActive = it },
+            { editorTextWatcher.isStrokeThroughActive = it }
+        )
+    }
+
+    override fun onDestroyView() {
+        editText.removeTextChangedListener(editorTextWatcher)
+        super.onDestroyView()
     }
 }

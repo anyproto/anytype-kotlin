6aa37132e51822fb75247a64e43b2791bc6dd41b
Konstantin <frankiet8702@gmail.com>
2019-03-14 21:22:23 +0300

    add Block model + test json

--- app/src/main/java/com/agileburo/anytype/model/Block.kt
+++ app/src/main/java/com/agileburo/anytype/model/Block.kt
@@ -1,5 +1,8 @@
 package com.agileburo.anytype.model
 
+import org.json.JSONException
+import org.json.JSONObject
+
 /**
  * Created by Konstantin Ivanov
  * email :  ki@agileburo.com
@@ -40,3 +43,50 @@ data class Block(
     val content: String = "",
     val children: List<Block> = emptyList()
 )
+
+fun Block.fromJson(jsonObject: JSONObject): Block? = with(jsonObject) {
+    var block: Block? = null
+    try {
+        block = Block(
+            id = getString("id"),
+            parentId = getString("parentId"),
+            content = getString("content"),
+            contentType = getInt("contentType").toContentType(),
+            type = getInt("type").toBlockType()
+        )
+    } catch (e: JSONException) {
+        e.printStackTrace()
+    }
+    return@with block
+}
+
+fun Int.toContentType(): ContentType =
+    when (this) {
+        1 -> ContentType.P
+        2 -> ContentType.Code
+        3 -> ContentType.H1
+        4 -> ContentType.H2
+        5 -> ContentType.H3
+        6 -> ContentType.OL
+        7 -> ContentType.UL
+        8 -> ContentType.HL
+        9 -> ContentType.Toggle
+        10 -> ContentType.Check
+        11 -> ContentType.H4
+        else -> ContentType.H1
+    }
+
+fun Int.toBlockType(): BlockType =
+    when (this) {
+        1 -> BlockType.HrGrid
+        2 -> BlockType.VrGrid
+        3 -> BlockType.Editable
+        4 -> BlockType.Div
+        5 -> BlockType.YouTube
+        6 -> BlockType.Image
+        7 -> BlockType.Page
+        8 -> BlockType.NewPage
+        9 -> BlockType.BookMark
+        10 -> BlockType.File
+        else -> BlockType.Editable
+    }

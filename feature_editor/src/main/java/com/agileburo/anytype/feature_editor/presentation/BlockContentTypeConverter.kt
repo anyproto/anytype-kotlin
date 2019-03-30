c0392f7a872b8d99cae3878bba58ee9a2c529093
Konstantin <frankiet8702@gmail.com>
2019-03-30 17:24:36 +0300

    edit block toolbar fixes #2

--- feature_editor/src/main/java/com/agileburo/anytype/feature_editor/presentation/BlockContentTypeConverter.kt
+++ feature_editor/src/main/java/com/agileburo/anytype/feature_editor/presentation/BlockContentTypeConverter.kt
@@ -5,6 +5,7 @@ import com.agileburo.anytype.feature_editor.domain.ContentType
 interface BlockContentTypeConverter {
 
     fun getPermittedTypes(typeInitial: ContentType): Set<ContentType>
+    fun getForbiddenTypes(typeInitial: ContentType): Set<ContentType>
 }
 
 class BlockContentTypeConverterImpl : BlockContentTypeConverter {
@@ -16,4 +17,12 @@ class BlockContentTypeConverterImpl : BlockContentTypeConverter {
             ContentType.H3, ContentType.OL, ContentType.UL, ContentType.Quote,
             ContentType.Toggle, ContentType.Check, ContentType.H4
         )
+
+    //Если вдруг появятся недопустимые варианты для конвертации, добавлять можно здесь
+    override fun getForbiddenTypes(typeInitial: ContentType): Set<ContentType> =
+        when (typeInitial) {
+            //Выключаем H1 при работе с блоком, используем H2, H2, H4
+            ContentType.P -> setOf(ContentType.H1)
+            else -> setOf(ContentType.H1)
+        }
 }

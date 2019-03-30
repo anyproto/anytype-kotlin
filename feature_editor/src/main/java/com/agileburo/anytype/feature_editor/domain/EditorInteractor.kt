bb98932acf46e80714ad3aee6027c1477d6118f5
Konstantin <frankiet8702@gmail.com>
2019-03-30 17:24:21 +0300

    do not show blocks with empty content

--- feature_editor/src/main/java/com/agileburo/anytype/feature_editor/domain/EditorInteractor.kt
+++ feature_editor/src/main/java/com/agileburo/anytype/feature_editor/domain/EditorInteractor.kt
@@ -1,6 +1,7 @@
 package com.agileburo.anytype.feature_editor.domain
 
 import com.agileburo.anytype.feature_editor.data.EditorRepo
+import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -12,7 +13,12 @@ interface EditorInteractor {
 
 class EditorInteractorImpl @Inject constructor(private val repo: EditorRepo) : EditorInteractor {
 
-    override fun getBlocks(): Single<List<Block>> = repo.getBlocks()
+    override fun getBlocks(): Single<List<Block>> =
+        repo.getBlocks()
+            .toObservable()
+            .flatMap { Observable.fromIterable(it) }
+            .filter { it.content.text.isNotEmpty() }
+            .toList()
 
     override fun saveState(list: MutableList<Block>) {
         repo.saveState(list)

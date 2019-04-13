8678d40dffcc17394bad16ebae30895c043e17e9
Ivanov Konstantin <Ivanov.ky@tutu.ru>
2019-04-13 14:07:35 +0300

    add Ui tests

---
+++ app/src/androidTest/java/com/agileburo/anytype/BaseNavigationTest.kt
@@ -0,0 +1,28 @@
+package com.agileburo.anytype
+
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+abstract class BaseNavigationTest {
+
+    @get:Rule
+    var activityActivityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    lateinit var navigator: FragmentNavigator
+
+    @Before
+    fun init() {
+        navigator =
+            (activityActivityTestRule.activity.supportFragmentManager.primaryNavigationFragment as NavHostFragment)
+                .navController.navigatorProvider.getNavigator(FragmentNavigator::class.java)
+        navigateToTestStartDestination()
+    }
+
+    abstract fun navigateToTestStartDestination()
+}
---
+++ app/src/androidTest/java/com/agileburo/anytype/RecyclerViewMatcher.java
@@ -0,0 +1,69 @@
+package com.agileburo.anytype;
+
+import android.content.res.Resources;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewMatcher {
+
+    private final int recyclerViewId;
+
+    public RecyclerViewMatcher(int recyclerViewId) {
+        this.recyclerViewId = recyclerViewId;
+    }
+
+    public Matcher<View> atPosition(final int position) {
+        return atPositionOnView(position, -1);
+    }
+
+    public Matcher<View> atPositionOnView(final int position, final int targetViewId) {
+
+        return new TypeSafeMatcher<View>() {
+            Resources resources = null;
+            View childView;
+
+            public void describeTo(Description description) {
+                String idDescription = Integer.toString(recyclerViewId);
+                if (this.resources != null) {
+                    try {
+                        idDescription = this.resources.getResourceName(recyclerViewId);
+                    } catch (Resources.NotFoundException var4) {
+                        idDescription = String.format("%s (resource name not found)",
+                                new Object[]{Integer.valueOf
+                                        (recyclerViewId)});
+                    }
+                }
+
+                description.appendText("with id: " + idDescription);
+            }
+
+            public boolean matchesSafely(View view) {
+
+                this.resources = view.getResources();
+
+                if (childView == null) {
+                    RecyclerView recyclerView =
+                            (RecyclerView) view.getRootView().findViewById(recyclerViewId);
+                    if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
+                        childView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (targetViewId == -1) {
+                    return view == childView;
+                } else {
+                    View targetView = childView.findViewById(targetViewId);
+                    return view == targetView;
+                }
+
+            }
+        };
+    }
+}

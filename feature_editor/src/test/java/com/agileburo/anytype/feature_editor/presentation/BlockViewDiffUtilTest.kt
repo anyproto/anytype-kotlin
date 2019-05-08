a82b4d7ae385cb31b8d1105fd7781106cf9b2e22
Ivanov Konstantin <Ivanov.ky@tutu.ru>
2019-05-08 20:50:35 +0300

    blockView equals unitb tests

---
+++ feature_editor/src/test/java/com/agileburo/anytype/feature_editor/presentation/BlockViewDiffUtilTest.kt
@@ -0,0 +1,53 @@
+package com.agileburo.anytype.feature_editor.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.agileburo.anytype.feature_editor.domain.ContentType
+import com.agileburo.anytype.feature_editor.domain.Mark
+import com.agileburo.anytype.feature_editor.presentation.model.BlockView
+import com.agileburo.anytype.feature_editor.presentation.util.BlockViewDiffUtil
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class BlockViewDiffUtilTest {
+
+    val block1 = BlockView(
+        id = "c0301f2b-b532-55e1-92ba-2b2fc4af237e",
+        contentType = ContentType.P,
+        content = BlockView.Content.Text(
+            text = "Первый и второй",
+            param = BlockView.ContentParam(map = mapOf("0" to false)),
+            marks = listOf(Mark(start = 0, end = 6, type = Mark.MarkType.STRIKE_THROUGH, param = ""))
+        )
+    )
+
+    val block2 = BlockView(
+        id = "c0301f2b-b532-55e1-92ba-2b2fc4af237e",
+        contentType = ContentType.P,
+        content = BlockView.Content.Text(
+            text = "Первый и второй",
+            param = BlockView.ContentParam(map = mapOf("0" to false)),
+            marks = listOf(Mark(start = 0, end = 6, type = Mark.MarkType.STRIKE_THROUGH, param = ""))
+        )
+    )
+
+    lateinit var diffUtil : DiffUtil.Callback
+
+    @Before
+    fun init() {
+        diffUtil = BlockViewDiffUtil(old = listOf(block1), new = listOf(block2))
+    }
+
+    @Test
+    fun `blocks should be the same`() {
+        val result = diffUtil.areItemsTheSame(0, 0)
+        assert(result)
+    }
+
+    @Test
+    fun `blocks should be equals`() {
+        val result = diffUtil.areContentsTheSame(0,0)
+        Assert.assertTrue(result)
+    }
+
+}

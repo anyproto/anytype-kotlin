f91c89c60014b62b705f8715e2855ff186a902fc
Evgenii Kozlov <ubuphobos@gmail.com>
2019-08-02 09:40:26 +0000

    Configure project with Crashlytics and Firebase

---
+++ core_utils/src/main/java/com/agileburo/anytype/core_utils/CrashlyticsTree.kt
@@ -0,0 +1,31 @@
+package com.agileburo.anytype.core_utils
+
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import timber.log.Timber
+
+class CrashlyticsTree : Timber.Tree() {
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+
+        if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+            return
+        }
+
+        Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
+        Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
+        Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
+
+        if (t == null) {
+            Crashlytics.logException(Exception(message))
+        } else {
+            Crashlytics.logException(t)
+        }
+    }
+
+    companion object {
+        private const val CRASHLYTICS_KEY_PRIORITY = "priority"
+        private const val CRASHLYTICS_KEY_TAG = "tag"
+        private const val CRASHLYTICS_KEY_MESSAGE = "message"
+    }
+}

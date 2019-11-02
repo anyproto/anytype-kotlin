4cc3615646a035c8a9afbed4fae435b1b12a42be
konstantiniiv <ki@agileburo.com>
2019-11-02 15:39:45 +0300

    add table view

---
+++ app/src/main/java/com/agileburo/anytype/ui/table/TableFragment.kt
@@ -0,0 +1,13 @@
+package com.agileburo.anytype.ui.table
+
+import com.agileburo.anytype.R
+import com.agileburo.anytype.core_utils.ui.BaseFragment
+
+class TableFragment : BaseFragment(R.layout.fragment_table) {
+
+    override fun injectDependencies() {
+    }
+
+    override fun releaseDependencies() {
+    }
+}

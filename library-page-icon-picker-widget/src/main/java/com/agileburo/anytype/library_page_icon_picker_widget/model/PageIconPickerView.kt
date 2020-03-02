e84e8fd6fcba3ff8cd32864fd6a007df1aa1d102
Evgenii Kozlov <ubuphobos@gmail.com>
2020-03-02 21:56:10 +0300

    Page-icon-picker widget (#260)

---
+++ library-page-icon-picker-widget/src/main/java/com/agileburo/anytype/library_page_icon_picker_widget/model/PageIconPickerView.kt
@@ -0,0 +1,53 @@
+package com.agileburo.anytype.library_page_icon_picker_widget.model
+
+import com.agileburo.anytype.core_ui.common.ViewType
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_CHOOSE_EMOJI
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_CATEGORY_HEADER
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_FILTER
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_ITEM
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_PICK_RANDOM_EMOJI
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_UPLOAD_PHOTO
+
+sealed class PageIconPickerView : ViewType {
+
+    /**
+     * @property alias short name or convenient name for an emoji.
+     */
+    data class Emoji(
+        val alias: String = "",
+        val unicode: String
+    ) : PageIconPickerView() {
+        override fun getViewType() = HOLDER_EMOJI_ITEM
+    }
+
+    /**
+     * @property category emoji category
+     */
+    data class GroupHeader(
+        val category: String
+    ) : PageIconPickerView() {
+        override fun getViewType() = HOLDER_EMOJI_CATEGORY_HEADER
+    }
+
+    /**
+     * Emoji filter.
+     */
+    object EmojiFilter : PageIconPickerView() {
+        override fun getViewType() = HOLDER_EMOJI_FILTER
+    }
+
+    /**
+     * User actions related to emoji picker feature.
+     */
+    sealed class Action : PageIconPickerView() {
+        object UploadPhoto : Action() {
+            override fun getViewType() = HOLDER_UPLOAD_PHOTO
+        }
+        object PickRandomly : Action() {
+            override fun getViewType() = HOLDER_PICK_RANDOM_EMOJI
+        }
+        object ChooseEmoji : Action() {
+            override fun getViewType() = HOLDER_CHOOSE_EMOJI
+        }
+    }
+}
---
+++ library-page-icon-picker-widget/src/main/java/com/agileburo/anytype/library_page_icon_picker_widget/ui/PageIconPickerAdapter.kt
@@ -0,0 +1,95 @@
+package com.agileburo.anytype.library_page_icon_picker_widget.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import com.agileburo.anytype.R
+import com.agileburo.anytype.library_page_icon_picker_widget.model.PageIconPickerView
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_CHOOSE_EMOJI
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_CATEGORY_HEADER
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_FILTER
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_EMOJI_ITEM
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_PICK_RANDOM_EMOJI
+import com.agileburo.anytype.library_page_icon_picker_widget.ui.PageIconPickerViewHolder.Companion.HOLDER_UPLOAD_PHOTO
+import kotlinx.android.synthetic.main.item_page_icon_picker_emoji_filter.view.*
+
+class PageIconPickerAdapter(
+    private var views: List<PageIconPickerView>,
+    private val onUploadPhotoClicked: () -> Unit,
+    private val onSetRandomEmojiClicked: () -> Unit,
+    private val onFilterQueryChanged: (String) -> Unit
+) : RecyclerView.Adapter<PageIconPickerViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PageIconPickerViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            HOLDER_UPLOAD_PHOTO -> PageIconPickerViewHolder.UploadPhoto(
+                view = inflater.inflate(
+                    R.layout.item_page_icon_picker_upload_photo,
+                    parent,
+                    false
+                )
+            ).apply {
+                itemView.setOnClickListener { onUploadPhotoClicked() }
+            }
+            HOLDER_PICK_RANDOM_EMOJI -> PageIconPickerViewHolder.PickRandom(
+                view = inflater.inflate(
+                    R.layout.item_page_icon_picker_pick_emoji_randomly,
+                    parent,
+                    false
+                )
+            ).apply {
+                itemView.setOnClickListener { onSetRandomEmojiClicked() }
+            }
+            HOLDER_CHOOSE_EMOJI -> PageIconPickerViewHolder.ChooseEmoji(
+                view = inflater.inflate(
+                    R.layout.item_page_icon_picker_choose_emoji,
+                    parent,
+                    false
+                )
+            )
+            HOLDER_EMOJI_CATEGORY_HEADER -> PageIconPickerViewHolder.CategoryHeader(
+                view = inflater.inflate(
+                    R.layout.item_page_icon_picker_emoji_category_header,
+                    parent,
+                    false
+                )
+            )
+            HOLDER_EMOJI_ITEM -> PageIconPickerViewHolder.EmojiItem(
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_page_icon_picker_emoji_item,
+                    parent,
+                    false
+                )
+            )
+            HOLDER_EMOJI_FILTER -> PageIconPickerViewHolder.EmojiFilter(
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_page_icon_picker_emoji_filter,
+                    parent,
+                    false
+                )
+            ).apply {
+                itemView.filterInputField.doOnTextChanged { text, _, _, _ ->
+                    onFilterQueryChanged(text.toString())
+                }
+            }
+            else -> throw IllegalStateException("Unexpected view type: $viewType")
+        }
+    }
+
+    override fun getItemCount(): Int = views.size
+    override fun getItemViewType(position: Int) = views[position].getViewType()
+
+    override fun onBindViewHolder(holder: PageIconPickerViewHolder, position: Int) {
+        when (holder) {
+            is PageIconPickerViewHolder.CategoryHeader -> {
+                holder.bind(views[position] as PageIconPickerView.GroupHeader)
+            }
+            is PageIconPickerViewHolder.EmojiItem -> {
+                holder.bind(views[position] as PageIconPickerView.Emoji)
+            }
+        }
+    }
+}

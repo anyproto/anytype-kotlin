e8437079b0148dab0262113625f9a9d68689511c
ubu <ubuphobos@gmail.com>
2020-04-01 22:09:20 +0200

    Fixes

--- app/src/main/java/com/agileburo/anytype/ui/page/modals/AddBlockFragment.kt
+++ app/src/main/java/com/agileburo/anytype/ui/page/modals/AddBlockFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.agileburo.anytype.R
 import com.agileburo.anytype.core_ui.features.page.modal.AddBlockOrTurnIntoAdapter
-import com.agileburo.anytype.core_ui.features.page.modal.AddBlockOrTurnIntoAdapter.AddBlockView
+import com.agileburo.anytype.core_utils.ext.toast
 import com.agileburo.anytype.core_utils.ui.BaseBottomSheetFragment
 import kotlinx.android.synthetic.main.fragment_add_block.*
 
 class AddBlockFragment : BaseBottomSheetFragment() {
+
+    private val addBlockOrTurnIntoAdapter = AddBlockOrTurnIntoAdapter(
+        onOptionClicked = {
+            toast("Option clicked")
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,22 +33,7 @@ class AddBlockFragment : BaseBottomSheetFragment() {
     private fun setupAdapter() {
         recycler.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = AddBlockOrTurnIntoAdapter(
-                views = listOf(
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1),
-                    AddBlockView.Item(1)
-                )
-            )
+            adapter = addBlockOrTurnIntoAdapter
         }
     }
 
--- core-ui/src/main/java/com/agileburo/anytype/core_ui/features/page/modal/AddBlockOrTurnIntoAdapter.kt
+++ core-ui/src/main/java/com/agileburo/anytype/core_ui/features/page/modal/AddBlockOrTurnIntoAdapter.kt
@@ -33,7 +33,8 @@ import kotlinx.android.synthetic.main.item_add_block_or_turn_into_item.view.*
 import kotlinx.android.synthetic.main.item_add_block_or_turn_into_section.view.*
 
 class AddBlockOrTurnIntoAdapter(
-    val views: List<AddBlockView>
+    private val views: List<AddBlockView> = defaultViews(),
+    private val onOptionClicked: (Int) -> Unit
 ) : RecyclerView.Adapter<AddBlockOrTurnIntoAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -62,7 +63,15 @@ class AddBlockOrTurnIntoAdapter(
     override fun getItemViewType(position: Int) = views[position].getViewType()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        when (holder) {
+            is ViewHolder.Section -> holder.bind(
+                section = views[position] as AddBlockView.Section
+            )
+            is ViewHolder.Item -> holder.bind(
+                item = views[position] as AddBlockView.Item,
+                onOptionClicked = onOptionClicked
+            )
+        }
     }
 
     sealed class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -88,98 +97,105 @@ class AddBlockOrTurnIntoAdapter(
             private val title = itemView.title
             private val subtitle = itemView.subtitle
 
-            fun bind(item: AddBlockView.Item) = when (item.type) {
-                OPTION_TEXT_TEXT -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_text)
-                    title.setText(R.string.option_text_text)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_text)
+            fun bind(
+                item: AddBlockView.Item,
+                onOptionClicked: (Int) -> Unit
+            ) {
+                when (item.type) {
+                    OPTION_TEXT_TEXT -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_text)
+                        title.setText(R.string.option_text_text)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_text)
+                    }
+                    OPTION_TEXT_HEADER_ONE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h1)
+                        title.setText(R.string.option_text_header_one)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_one)
+                    }
+                    OPTION_TEXT_HEADER_TWO -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h2)
+                        title.setText(R.string.option_text_header_two)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_two)
+                    }
+                    OPTION_TEXT_HEADER_THREE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h3)
+                        title.setText(R.string.option_text_header_three)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_three)
+                    }
+                    OPTION_TEXT_HIGHLIGHTED -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_highlighted)
+                        title.setText(R.string.option_text_highlighted)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_highlighted)
+                    }
+                    OPTION_LIST_CHECKBOX -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_checkbox)
+                        title.setText(R.string.option_list_checkbox)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_checkbox)
+                    }
+                    OPTION_LIST_BULLETED_LIST -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_bulleted)
+                        title.setText(R.string.option_list_bulleted_list)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_bulleted)
+                    }
+                    OPTION_LIST_NUMBERED_LIST -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_numbered)
+                        title.setText(R.string.option_list_numbered_list)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_numbered)
+                    }
+                    OPTION_LIST_TOGGLE_LIST -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_numbered)
+                        title.setText(R.string.option_list_numbered_list)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_numbered)
+                    }
+                    OPTION_PAGE_PAGE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_page)
+                        title.setText(R.string.option_tool_page)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_page)
+                    }
+                    OPTION_PAGE_EXISTING_PAGE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_existing_page)
+                        title.setText(R.string.option_tool_existing_page)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_existing_page)
+                    }
+                    OPTION_OBJECTS_FILE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_file)
+                        title.setText(R.string.option_media_file)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_file)
+                    }
+                    OPTION_OBJECTS_IMAGE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_picture)
+                        title.setText(R.string.option_media_picture)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_image)
+                    }
+                    OPTION_OBJECTS_VIDEO -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_video)
+                        title.setText(R.string.option_media_video)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_video)
+                    }
+                    OPTION_OBJECTS_BOOKMARK -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_bookmark)
+                        title.setText(R.string.option_media_bookmark)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_bookmark)
+                    }
+                    OPTION_OBJECTS_CODE -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_code)
+                        title.setText(R.string.option_media_code)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_code)
+                    }
+                    OPTION_OTHER_LINE_DIVIDER -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_line_divider)
+                        title.setText(R.string.option_other_divider)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_line_divider)
+                    }
+                    OPTION_OTHER_DOTS -> {
+                        icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_three_dots_divider)
+                        title.setText(R.string.option_other_dots)
+                        subtitle.setText(R.string.add_block_or_turn_into_subtitle_dots)
+                    }
+                    else -> throw IllegalStateException("Unexpected type: ${item.type}")
                 }
-                OPTION_TEXT_HEADER_ONE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h1)
-                    title.setText(R.string.option_text_header_one)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_one)
-                }
-                OPTION_TEXT_HEADER_TWO -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h2)
-                    title.setText(R.string.option_text_header_two)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_two)
-                }
-                OPTION_TEXT_HEADER_THREE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_h3)
-                    title.setText(R.string.option_text_header_three)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_header_three)
-                }
-                OPTION_TEXT_HIGHLIGHTED -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_highlighted)
-                    title.setText(R.string.option_text_highlighted)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_highlighted)
-                }
-                OPTION_LIST_CHECKBOX -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_checkbox)
-                    title.setText(R.string.option_list_checkbox)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_checkbox)
-                }
-                OPTION_LIST_BULLETED_LIST -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_bulleted)
-                    title.setText(R.string.option_list_bulleted_list)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_bulleted)
-                }
-                OPTION_LIST_NUMBERED_LIST -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_numbered)
-                    title.setText(R.string.option_list_numbered_list)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_numbered)
-                }
-                OPTION_LIST_TOGGLE_LIST -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_numbered)
-                    title.setText(R.string.option_list_numbered_list)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_numbered)
-                }
-                OPTION_PAGE_PAGE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_page)
-                    title.setText(R.string.option_tool_page)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_page)
-                }
-                OPTION_PAGE_EXISTING_PAGE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_existing_page)
-                    title.setText(R.string.option_tool_existing_page)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_existing_page)
-                }
-                OPTION_OBJECTS_FILE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_file)
-                    title.setText(R.string.option_media_file)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_file)
-                }
-                OPTION_OBJECTS_IMAGE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_picture)
-                    title.setText(R.string.option_media_picture)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_image)
-                }
-                OPTION_OBJECTS_VIDEO -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_video)
-                    title.setText(R.string.option_media_video)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_video)
-                }
-                OPTION_OBJECTS_BOOKMARK -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_bookmark)
-                    title.setText(R.string.option_media_bookmark)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_bookmark)
-                }
-                OPTION_OBJECTS_CODE -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_code)
-                    title.setText(R.string.option_media_code)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_code)
-                }
-                OPTION_OTHER_LINE_DIVIDER -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_line_divider)
-                    title.setText(R.string.option_other_divider)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_line_divider)
-                }
-                OPTION_OTHER_DOTS -> {
-                    icon.setBackgroundResource(R.drawable.ic_add_block_or_turn_into_three_dots_divider)
-                    title.setText(R.string.option_other_dots)
-                    subtitle.setText(R.string.add_block_or_turn_into_subtitle_dots)
-                }
-                else -> throw IllegalStateException("Unexpected type: ${item.type}")
+
+                itemView.setOnClickListener { onOptionClicked(item.type) }
             }
         }
 
@@ -229,5 +245,31 @@ class AddBlockOrTurnIntoAdapter(
     companion object {
         const val VIEW_HOLDER_SECTION = 0
         const val VIEW_HOLDER_ITEM = 1
+
+        fun defaultViews(): List<AddBlockView> = listOf(
+            AddBlockView.Section(SECTION_TEXT),
+            AddBlockView.Item(OPTION_TEXT_TEXT),
+            AddBlockView.Item(OPTION_TEXT_HEADER_ONE),
+            AddBlockView.Item(OPTION_TEXT_HEADER_TWO),
+            AddBlockView.Item(OPTION_TEXT_HEADER_THREE),
+            AddBlockView.Item(OPTION_TEXT_HIGHLIGHTED),
+            AddBlockView.Section(SECTION_LIST),
+            AddBlockView.Item(OPTION_LIST_CHECKBOX),
+            AddBlockView.Item(OPTION_LIST_BULLETED_LIST),
+            AddBlockView.Item(OPTION_LIST_NUMBERED_LIST),
+            AddBlockView.Item(OPTION_LIST_TOGGLE_LIST),
+            AddBlockView.Section(SECTION_PAGE),
+            AddBlockView.Item(OPTION_PAGE_PAGE),
+            AddBlockView.Item(OPTION_PAGE_EXISTING_PAGE),
+            AddBlockView.Section(SECTION_OBJECTS),
+            AddBlockView.Item(OPTION_OBJECTS_FILE),
+            AddBlockView.Item(OPTION_OBJECTS_IMAGE),
+            AddBlockView.Item(OPTION_OBJECTS_VIDEO),
+            AddBlockView.Item(OPTION_OBJECTS_BOOKMARK),
+            AddBlockView.Item(OPTION_OBJECTS_CODE),
+            AddBlockView.Section(SECTION_OTHER),
+            AddBlockView.Item(OPTION_OTHER_LINE_DIVIDER),
+            AddBlockView.Item(OPTION_OTHER_DOTS)
+        )
     }
 }

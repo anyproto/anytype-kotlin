a762519baf1a9fab97d3ed2d82dceb58e4d29953
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2020-08-26 20:01:34 +0300

    #594: Style config + tests (#736)
    * #594: style config + tests
    
    * #594: code style
    
    * #594: fix
    
    * #594: remove align for image and video
    
    * #594: fix
    
    * #594: fixes

---
+++ presentation/src/main/java/com/agileburo/anytype/presentation/page/editor/StyleConfig.kt
@@ -0,0 +1,262 @@
+package com.agileburo.anytype.presentation.page.editor
+
+import com.agileburo.anytype.core_ui.common.Alignment
+import com.agileburo.anytype.core_ui.features.page.styling.StylingType
+import com.agileburo.anytype.domain.block.model.Block
+
+data class StyleConfig(
+    val visibleTypes: List<StylingType>,
+    val enabledMarkup: List<Block.Content.Text.Mark.Type>,
+    val enabledAlignment: List<Alignment>
+) {
+
+    companion object {
+        fun emptyState() = StyleConfig(
+            visibleTypes = emptyList(),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+}
+
+fun Block.getStyleConfig(focus: Boolean?, selection: IntRange?): StyleConfig =
+    when (val cnt = content) {
+        is Block.Content.Text -> cnt.getTextStyleConfig(focus, selection)
+        is Block.Content.Link -> cnt.getLinkStyleConfig()
+        is Block.Content.File -> cnt.getFileStyleConfig(focus)
+        is Block.Content.Bookmark -> cnt.getBookmarkStyleConfig(focus)
+        else -> StyleConfig.emptyState()
+    }
+
+fun Block.Content.Bookmark.getBookmarkStyleConfig(focus: Boolean?): StyleConfig {
+    check(focus == null) { "Bookmark block should has null focus" }
+    return getStyleConfig()
+}
+
+fun Block.Content.Bookmark.getStyleConfig(): StyleConfig = StyleConfig.emptyState()
+
+fun Block.Content.Link.getLinkStyleConfig(): StyleConfig = StyleConfig(
+    visibleTypes = listOf(StylingType.BACKGROUND),
+    enabledMarkup = emptyList(),
+    enabledAlignment = emptyList()
+)
+
+fun Block.Content.File.getFileStyleConfig(focus: Boolean?): StyleConfig {
+    check(focus == null) { "File block should has null focus" }
+    return getStyleConfig()
+}
+
+fun Block.Content.File.getStyleConfig(): StyleConfig = when (type) {
+    Block.Content.File.Type.FILE -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.File.Type.IMAGE -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.File.Type.VIDEO -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    else -> StyleConfig.emptyState()
+}
+
+fun Block.Content.Text.getTextStyleConfig(focus: Boolean?, selection: IntRange?): StyleConfig {
+    check(focus != null) { "Text block should have focus" }
+    return if (focus) {
+        getStyleConfig(selection = selection)
+    } else {
+        getStyleConfig(selection = null)
+    }
+}
+
+fun Block.Content.Text.getStyleConfig(selection: IntRange? = null): StyleConfig {
+    return if (selection == null || selection.first == selection.last) {
+        getBlockStyle(style)
+    } else {
+        getMarkupStyle(style)
+    }
+}
+
+fun Block.Content.Text.getBlockStyle(style: Block.Content.Text.Style) = when (style) {
+    Block.Content.Text.Style.P -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = listOf(Alignment.START, Alignment.CENTER, Alignment.END)
+        )
+    }
+    Block.Content.Text.Style.H1, Block.Content.Text.Style.H2,
+    Block.Content.Text.Style.H3, Block.Content.Text.Style.H4 -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = listOf(Alignment.START, Alignment.CENTER, Alignment.END)
+        )
+    }
+    Block.Content.Text.Style.TITLE -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = listOf(Alignment.START, Alignment.CENTER, Alignment.END)
+        )
+    }
+    Block.Content.Text.Style.QUOTE -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = listOf(Alignment.START, Alignment.END)
+        )
+    }
+    Block.Content.Text.Style.CODE_SNIPPET -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.BULLET, Block.Content.Text.Style.NUMBERED,
+    Block.Content.Text.Style.TOGGLE, Block.Content.Text.Style.CHECKBOX -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = emptyList()
+        )
+    }
+}
+
+fun Block.Content.Text.getMarkupStyle(style: Block.Content.Text.Style) = when (style) {
+    Block.Content.Text.Style.P -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.H1, Block.Content.Text.Style.H2,
+    Block.Content.Text.Style.H3, Block.Content.Text.Style.H4 -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.TITLE -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.QUOTE -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.CODE_SNIPPET -> {
+        StyleConfig(
+            visibleTypes = listOf(StylingType.BACKGROUND),
+            enabledMarkup = emptyList(),
+            enabledAlignment = emptyList()
+        )
+    }
+    Block.Content.Text.Style.BULLET, Block.Content.Text.Style.NUMBERED,
+    Block.Content.Text.Style.TOGGLE, Block.Content.Text.Style.CHECKBOX -> {
+        StyleConfig(
+            visibleTypes = listOf(
+                StylingType.STYLE,
+                StylingType.TEXT_COLOR,
+                StylingType.BACKGROUND
+            ),
+            enabledMarkup = listOf(
+                Block.Content.Text.Mark.Type.BOLD,
+                Block.Content.Text.Mark.Type.ITALIC,
+                Block.Content.Text.Mark.Type.STRIKETHROUGH,
+                Block.Content.Text.Mark.Type.KEYBOARD,
+                Block.Content.Text.Mark.Type.LINK
+            ),
+            enabledAlignment = emptyList()
+        )
+    }
+}
+

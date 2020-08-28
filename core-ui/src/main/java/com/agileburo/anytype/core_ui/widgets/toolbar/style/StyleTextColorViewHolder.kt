1ea9ab8dcdf329c2fd02655bc5e69ac15b14759b
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2020-08-28 18:01:24 +0300

    #594: Style toolbar (#744)
    * #594: add widget + adapter + holders
    
    * #594: update background state with enable/disable
    
    * #594: add state disabled to icons
    
    * #594: fixes
    
    * #594: style toolbar, add tab layout
    
    * #594: fix
    
    * #594: sample
    
    * #594: pr fix
    
    * #594: pr fix

---
+++ core-ui/src/main/java/com/agileburo/anytype/core_ui/widgets/toolbar/style/StyleTextColorViewHolder.kt
@@ -0,0 +1,73 @@
+package com.agileburo.anytype.core_ui.widgets.toolbar.style
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.agileburo.anytype.core_ui.common.ThemeColor
+import com.agileburo.anytype.core_ui.features.page.styling.StylingEvent
+import kotlinx.android.synthetic.main.block_style_toolbar_color.view.*
+
+class StyleTextColorViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val default = itemView.textColorDefault
+    private val grey = itemView.textColorGrey
+    private val yellow = itemView.textColorYellow
+    private val orange = itemView.textColorOrange
+    private val red = itemView.textColorRed
+    private val pink = itemView.textColorPink
+    private val purple = itemView.textColorPurple
+    private val blue = itemView.textColorBlue
+    private val ice = itemView.textColorIce
+    private val teal = itemView.textColorTeal
+    private val green = itemView.textColorGreen
+
+    fun bind(
+        onStylingEvent: (StylingEvent) -> Unit,
+        color: String?
+    ) {
+        default.isSelected = color == ThemeColor.DEFAULT.title
+        grey.isSelected = color == ThemeColor.GREY.title
+        yellow.isSelected = color == ThemeColor.YELLOW.title
+        orange.isSelected = color == ThemeColor.ORANGE.title
+        red.isSelected = color == ThemeColor.RED.title
+        pink.isSelected = color == ThemeColor.PINK.title
+        purple.isSelected = color == ThemeColor.PURPLE.title
+        blue.isSelected = color == ThemeColor.BLUE.title
+        ice.isSelected = color == ThemeColor.ICE.title
+        teal.isSelected = color == ThemeColor.TEAL.title
+        green.isSelected = color == ThemeColor.GREEN.title
+
+        default.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.DEFAULT))
+        }
+        grey.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.GREY))
+        }
+        yellow.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.YELLOW))
+        }
+        orange.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.ORANGE))
+        }
+        red.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.RED))
+        }
+        pink.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.PINK))
+        }
+        purple.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.PURPLE))
+        }
+        blue.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.BLUE))
+        }
+        ice.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.ICE))
+        }
+        teal.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.TEAL))
+        }
+        green.setOnClickListener {
+            onStylingEvent(StylingEvent.Coloring.Text(color = ThemeColor.GREEN))
+        }
+    }
+}

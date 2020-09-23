0b5e3a717bfabb9b440371c4e09b74b27efe7739
Evgenii Kozlov <ubuphobos@gmail.com>
2020-09-23 21:00:25 +0200

    Fix/dashboard positioning (#913)

--- app/src/main/java/com/agileburo/anytype/ui/desktop/LinearConcatAdapter.kt
+++ app/src/main/java/com/agileburo/anytype/ui/desktop/LinearConcatAdapter.kt
@@ -3,6 +3,7 @@ package com.agileburo.anytype.ui.desktop
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.agileburo.anytype.R
@@ -12,9 +13,13 @@ class LinearConcatAdapter(val adapter: DashboardProfileAdapter) :
     RecyclerView.Adapter<LinearConcatAdapter.LinearConcatHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LinearConcatHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_profile_container, parent, false)
-        view.recyclerView.layoutManager = LinearLayoutManager(parent.context)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_profile_container, parent, false)
+        view.recyclerView.apply {
+            layoutManager = LinearLayoutManager(parent.context)
+            val lp = (layoutParams as FrameLayout.LayoutParams)
+            lp.height = (parent.height / 2) - lp.topMargin - lp.bottomMargin
+        }
         return LinearConcatHolder(view)
     }
 

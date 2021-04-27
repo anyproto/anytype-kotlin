c9722bcfab1ba72dfe751b7d54c9659d9215c300
Evgenii Kozlov <ubuphobos@gmail.com>
2021-04-27 14:29:21 +0300

    Editor | Redesign | Markup panel (#1428)

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/toolbar/MarkupToolbarWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/toolbar/MarkupToolbarWidget.kt
@@ -3,16 +3,21 @@ package com.anytypeio.anytype.core_ui.widgets.toolbar
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.LinearLayout
 import com.anytypeio.anytype.core_ui.R
+import com.anytypeio.anytype.core_ui.extensions.tint
 import com.anytypeio.anytype.core_ui.reactive.clicks
+import com.anytypeio.anytype.core_utils.ext.invisible
+import com.anytypeio.anytype.core_utils.ext.visible
 import com.anytypeio.anytype.presentation.page.editor.Markup
-import kotlinx.android.synthetic.main.widget_markup_toolbar.view.*
+import com.anytypeio.anytype.presentation.page.editor.ThemeColor
+import com.anytypeio.anytype.presentation.page.markup.MarkupStyleDescriptor
+import kotlinx.android.synthetic.main.widget_markup_toolbar_main.view.*
 import kotlinx.coroutines.flow.flattenMerge
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 
-class MarkupToolbarWidget : ConstraintLayout {
+class MarkupToolbarWidget : LinearLayout {
 
     constructor(
         context: Context
@@ -32,24 +37,70 @@ class MarkupToolbarWidget : ConstraintLayout {
     }
 
     private fun inflate() {
-        LayoutInflater.from(context).inflate(R.layout.widget_markup_toolbar, this)
+        LayoutInflater.from(context).inflate(R.layout.widget_markup_toolbar_main, this)
     }
 
-    fun markupClicks() = flowOf(bold(), italic(), strike(), link(), code()).flattenMerge()
+    fun markup() = flowOf(bold(), italic(), strike(), code()).flattenMerge()
     private fun bold() = bold.clicks().map { Markup.Type.BOLD }
     private fun italic() = italic.clicks().map { Markup.Type.ITALIC }
     private fun strike() = strike.clicks().map { Markup.Type.STRIKETHROUGH }
-    private fun link() = link.clicks().map { Markup.Type.LINK }
+    fun linkClicks() = url.clicks().map { Markup.Type.LINK }
     private fun code() = code.clicks().map { Markup.Type.KEYBOARD }
 
     fun colorClicks() = color.clicks()
-    fun hideKeyboardClicks() = keyboard.clicks()
+    fun highlightClicks() = highlight.clicks()
+
+    fun setProps(
+        props: MarkupStyleDescriptor?,
+        isBackgroundColorSelected: Boolean,
+        isTextColorSelected: Boolean
+    ) {
+        bold.isSelected = props?.isBold ?: false
+        italic.isSelected = props?.isItalic ?: false
+        strike.isSelected = props?.isStrikethrough ?: false
+        code.isSelected = props?.isCode ?: false
+        url.isSelected = props?.isLinked ?: false
+
+        if (props?.markupTextColor != null) {
+            val code = ThemeColor.values().first { it.title == props.markupTextColor }
+            textColorCircle.tint(code.text)
+        } else {
+            val code = ThemeColor.values().find { it.title == props?.blockTextColor }
+            if (code != null)
+                textColorCircle.tint(code.text)
+            else
+                textColorCircle.backgroundTintList = null
+        }
+
+        if (props?.markupHighlightColor != null) {
+            val code = ThemeColor.values().first { it.title == props.markupHighlightColor }
+            if (code == ThemeColor.DEFAULT)
+                backgroundColorCircle.backgroundTintList = null
+            else
+                backgroundColorCircle.tint(code.background)
+        } else {
+            val code = ThemeColor.values().find { it.title == props?.blockBackroundColor }
+            if (code != null)
+                backgroundColorCircle.tint(code.background)
+            else
+                backgroundColorCircle.backgroundTintList = null
+        }
+
+        if (isBackgroundColorSelected)
+            selectedBackgroundCircle.visible()
+        else
+            selectedBackgroundCircle.invisible()
+
+        if (isTextColorSelected)
+            selectedTextColorCircle.visible()
+        else
+            selectedTextColorCircle.invisible()
+    }
 
     sealed class Action
 
     sealed class Event {
         data class OnMarkupClicked(val type: Markup.Type) : Event()
-        object OnHideKeyboardClicked : Event()
         object OnColorClicked : Event()
     }
 }
---
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/page/markup/MarkupStyleDescriptor.kt
@@ -0,0 +1,31 @@
+package com.anytypeio.anytype.presentation.page.markup
+
+interface MarkupStyleDescriptor {
+
+    val range: IntRange
+    val isBold: Boolean
+    val isItalic: Boolean
+    val isStrikethrough: Boolean
+    val isCode: Boolean
+    val isLinked: Boolean
+    val markupTextColor: String?
+    val markupUrl: String?
+    val markupHighlightColor: String?
+
+    val blockTextColor: String?
+    val blockBackroundColor: String?
+
+    data class Default(
+        override val isBold: Boolean,
+        override val isItalic: Boolean,
+        override val isCode: Boolean,
+        override val isStrikethrough: Boolean,
+        override val isLinked: Boolean,
+        override val markupTextColor: String?,
+        override val markupHighlightColor: String?,
+        override val markupUrl: String?,
+        override val blockTextColor: String?,
+        override val blockBackroundColor: String?,
+        override val range: IntRange
+    ) : MarkupStyleDescriptor
+}

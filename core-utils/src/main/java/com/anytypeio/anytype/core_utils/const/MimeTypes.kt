1901eb93fa91be984dd5fd42b855041a448bff2f
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2021-09-16 10:37:32 +0400

    Editor | Fix | Update mime types (#1810)
    * update mime types
    
    * fix + ci
    
    * ci off

--- core-utils/src/main/java/com/anytypeio/anytype/core_utils/const/MimeTypes.kt
+++ core-utils/src/main/java/com/anytypeio/anytype/core_utils/const/MimeTypes.kt
@@ -5,11 +5,59 @@ object MimeTypes {
     private const val PDF = "application/pdf"
 
     private val IMAGES = listOf(
-        "image/jpeg"
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/webp",
+        "image/gif",
+        "image/avif",
+        "image/apng",
+        "image/bmp"
     )
 
     private val TEXTS = listOf(
-        "text/plain"
+        "text/plain",
+        "text/csv",
+        "text/html",
+        "application/msword",
+        "text/css",
+        "application/x-csh",
+    )
+
+    private val VIDEOS = listOf(
+        "video/mp4",
+        "video/3gpp",
+        "video/3gpp2",
+        "video/H261",
+        "video/H263",
+        "video/mpv",
+        "video/ogg",
+        "video/x-msvideo"
+    )
+
+    private val AUDIOS = listOf(
+        "audio/mp4",
+        "audio/wave",
+        "audio/wav",
+        "audio/webm",
+        "audio/ogg",
+        "audio/aac",
+        "audio/ac3",
+        "audio/mpeg",
+        "audio/midi",
+        "audio/x-midi"
+    )
+
+    private val ARCHIVE = listOf(
+        "application/zip",
+        "application/zlib",
+        "application/x-rar-compressed",
+        "application/octet-stream",
+        "application/x-zip-compressed",
+        "multipart/x-zip",
+        "application/x-bzip",
+        "application/x-bzip2",
+        "application/gzip"
     )
 
     enum class Category {
@@ -20,6 +68,9 @@ object MimeTypes {
         mime == PDF -> Category.PDF
         IMAGES.contains(mime) -> Category.IMAGE
         TEXTS.contains(mime) -> Category.TEXT
+        VIDEOS.contains(mime) -> Category.VIDEO
+        AUDIOS.contains(mime) -> Category.AUDIO
+        ARCHIVE.contains(mime) -> Category.ARCHIVE
         else -> Category.OTHER
     }
 }

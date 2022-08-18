80c69576a4f18e8fdbbfa9f37471b24e058f2d65
Evgenii Kozlov <ubuphobos@gmail.com>
2022-08-18 14:29:56 +0300

    DROID-302 Relations | Fix | Allow reloading content only for specific relation key (#2533)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/sets/RelationTextValueViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/sets/RelationTextValueViewModel.kt
@@ -101,7 +101,7 @@ class RelationTextValueViewModel(
                             actions.value = buildList {
                                 add(RelationValueAction.Url.Browse(value))
                                 add(RelationValueAction.Url.Copy(value))
-                                if (obj.type.contains(ObjectType.BOOKMARK_TYPE)) {
+                                if (relation.key == Relations.URL && obj.type.contains(ObjectType.BOOKMARK_TYPE)) {
                                     add(RelationValueAction.Url.Reload(value))
                                 }
                             }

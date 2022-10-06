47842af89d13f0b40aa532a373a0860b122339e6
Evgenii Kozlov <ubuphobos@gmail.com>
2022-10-06 15:16:06 +0300

    DROID-513 Sets | Fix | Date is displayed incorrectly in list view of a data view (#2639)

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/dv/ListViewItemRelationGroupWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/dv/ListViewItemRelationGroupWidget.kt
@@ -8,13 +8,13 @@ import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.helper.widget.Flow
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.anytypeio.anytype.core_models.ThemeColor
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.extensions.dark
 import com.anytypeio.anytype.core_ui.widgets.ListViewRelationObjectValueView
 import com.anytypeio.anytype.core_ui.widgets.ListViewRelationTagValueView
 import com.anytypeio.anytype.core_utils.ext.dimen
 import com.anytypeio.anytype.core_utils.ext.formatTimestamp
-import com.anytypeio.anytype.core_models.ThemeColor
 import com.anytypeio.anytype.presentation.objects.ObjectIcon
 import com.anytypeio.anytype.presentation.relations.model.DefaultObjectRelationValueView
 import com.anytypeio.anytype.presentation.sets.model.ObjectView
@@ -62,7 +62,7 @@ class ListViewItemRelationGroupWidget @JvmOverloads constructor(
                 }
                 is DefaultObjectRelationValueView.Date -> {
                     val value = relation.timeInMillis?.formatTimestamp(
-                        isMillis = true,
+                        isMillis = false,
                         format = relation.dateFormat
                     )
                     if (value != null) {

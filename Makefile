9fdbf01e2f47431b84d07c01a4fbc70a2df35902
Evgenii Kozlov <ubuphobos@gmail.com>
2023-04-26 16:57:42 +0200

    DROID-1218 Protocol | Enhancement | MW v0.26.0-rc11 + scripts for updating / downloading mw artefacts (#3126)

--- Makefile
+++ Makefile
@@ -12,9 +12,6 @@ distribute_debug:
 
 pr_check: compile_android_test_sources test_debug_all
 
-setup_local_mw:
-	./gradlew libs:publishToMavenLocal
-
 enable_analytics_for_debug:
 	sed -i -e 's/config.enableAnalyticsForDebugBuilds=false/config.enableAnalyticsForDebugBuilds=true/g' analytics/gradle.properties
 
@@ -26,3 +23,19 @@ encryption:
 
 decryption:
 	./scripts/common/decrypt.sh
+
+
+# MIDDLEWARE INTEGRATION
+
+# Use this only for custom (not yet released) middleware library.
+setup_local_mw:
+	./gradlew libs:publishToMavenLocal
+
+download_mw_artefacts:
+	./scripts/mw/update-mw.sh
+
+normalize_mw_imports:
+	./scripts/mw/normalize-imports.sh
+
+# Update mw when new release is ready
+update_mw: download_mw_artefacts normalize_mw_imports
---
+++ scripts/mw/update-mw.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+REPO="anytypeio/go-anytype-middleware"
+FILE="lib.tar.gz"
+GITHUB="api.github.com"
+
+echo "Enter your github token (check your github.properties file)"
+
+read -r TOKEN
+
+if [ "$TOKEN" = "" ]; then
+  echo "ERROR: token is empty"
+  exit 1
+fi;
+
+echo "Enter MW version without any prefix (for instance: 0.26.0)"
+
+read -r MW_VERSION
+
+if [ "$MW_VERSION" = "" ]; then
+  echo "ERROR: mw version is empty"
+  exit 1
+fi;
+
+version=`curl -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases/tags/v$MW_VERSION | jq .`
+
+tag=`echo $version | jq ".tag_name"`
+asset_id=`echo $version | jq ".assets | map(select(.name | match(\"android_lib_\";\"i\")))[0].id"`
+
+if [ "$asset_id" = "" ]; then
+  echo "ERROR: version not found"
+  exit 1
+fi;
+
+printf "Version: $tag\n"
+printf "Found asset: $asset_id\n"
+echo -n "Downloading file... "
+curl -sL -H 'Accept: application/octet-stream' https://$TOKEN:@$GITHUB/repos/$REPO/releases/assets/$asset_id > $FILE
+printf "Done\n"
+
+echo -n "Uncompressing... "
+
+mkdir /tmp/lib
+
+tar -zxf $FILE -C /tmp/lib/
+printf "Done\n"
+
+printf "Preparing files\n"
+
+# Moving proto files to protocol module
+mkdir -p libs/protobuf/protos
+mv /tmp/lib/protobuf/protos/* protocol/src/main/proto
+
+
+# Clearing temp folder
+rm -rf /tmp/lib
+rm -rf $FILE
+
+# For Android app internal usage, we don't need the following protos:
+rm -rf protocol/src/main/proto/service.proto
+rm -rf protocol/src/main/proto/block.proto
+rm -rf protocol/src/main/proto/file.proto
+rm -rf protocol/src/main/proto/snapshot.proto
+rm -rf protocol/src/main/proto/migration.proto
+
+echo "Done with downloading protobuf files!"
+
+echo ">>> Make sure to update mw version to version $tag in libs.versions.toml >>>"

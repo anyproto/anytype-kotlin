8e696d4a91947c7199c90817f344d1077f22f88d
konstantiniiv <ki@anytype.io>
2023-06-02 17:56:21 +0200

    DROID-1125 relation object icon

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/RelationObjectItem.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/RelationObjectItem.kt
@@ -3,25 +3,32 @@ package com.anytypeio.anytype.core_ui.widgets
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.databinding.WidgetRelationObjectBinding
 import com.anytypeio.anytype.core_ui.extensions.color
+import com.anytypeio.anytype.core_utils.ext.gone
 import com.anytypeio.anytype.core_utils.ext.visible
 import com.anytypeio.anytype.presentation.objects.ObjectIcon
 
 class RelationObjectItem @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     val binding = WidgetRelationObjectBinding.inflate(
-        LayoutInflater.from(context), this
+        LayoutInflater.from(context), this, true
     )
 
     fun setup(name: String, icon: ObjectIcon) = with(binding) {
         tvName.visible()
         tvName.text = name
-        objectIcon.setIcon(icon)
+        when (icon) {
+            ObjectIcon.None -> objectIcon.gone()
+            else -> {
+                objectIcon.visible()
+                objectIcon.setIcon(icon)
+            }
+        }
     }
 
     fun setupAsNonExistent() = with(binding) {

7ecc34a7f1c175a03060df6107f040775ba4f358
Evgenii Kozlov <ubuphobos@gmail.com>
2023-06-13 14:30:22 +0200

    DROID-1378 App | Tech | MDNS component stabilisation (#61)

--- middleware/src/main/java/com/anytypeio/anytype/middleware/discovery/MDNSResolver.kt
+++ middleware/src/main/java/com/anytypeio/anytype/middleware/discovery/MDNSResolver.kt
@@ -4,6 +4,7 @@ import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import android.net.wifi.WifiManager
 import service.DiscoveryObserver
+import timber.log.Timber
 
 class MDNSResolver(
     private val nsdManager: NsdManager,
@@ -19,37 +20,45 @@ class MDNSResolver(
     private var isStarted = false
 
     fun start(observer: DiscoveryObserver) {
-        if (!isStarted) {
-            isStarted = true
+        try {
+            if (!isStarted) {
+                isStarted = true
 
-            lock.acquire()
+                lock.acquire()
 
-            discoveryListener.registerObserver(observer)
-            val serviceInfo = collectNsdServiceInfo(observer)
+                discoveryListener.registerObserver(observer)
+                val serviceInfo = collectNsdServiceInfo(observer)
 
-            nsdManager.apply {
-                registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
-                discoverServices(
-                    serviceInfo.serviceType,
-                    NsdManager.PROTOCOL_DNS_SD,
-                    discoveryListener
-                )
+                nsdManager.apply {
+                    registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
+                    discoverServices(
+                        serviceInfo.serviceType,
+                        NsdManager.PROTOCOL_DNS_SD,
+                        discoveryListener
+                    )
+                }
             }
+        } catch (e: Exception) {
+            Timber.e(e, "Error while starting MDNS Resolver")
         }
     }
 
     fun stop() {
-        if (isStarted) {
-            isStarted = false
+        try {
+            if (isStarted) {
+                isStarted = false
 
-            discoveryListener.unregisterObserver()
+                discoveryListener.unregisterObserver()
 
-            nsdManager.apply {
-                unregisterService(registrationListener)
-                stopServiceDiscovery(discoveryListener)
+                nsdManager.apply {
+                    unregisterService(registrationListener)
+                    stopServiceDiscovery(discoveryListener)
+                }
+
+                lock.release()
             }
-
-            lock.release()
+        } catch (e: Exception) {
+            Timber.e(e, "Error while stopping MDNS resolver")
         }
     }
 

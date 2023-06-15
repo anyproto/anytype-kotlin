8c7e75bd231143324bf8facac15226b0dd400bdb
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2023-06-15 10:51:38 +0200

    DROID-1385 App | Fix | Sync status, colors (#59)

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/StatusBadgeWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/StatusBadgeWidget.kt
@@ -1,13 +1,14 @@
 package com.anytypeio.anytype.core_ui.widgets
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import com.anytypeio.anytype.core_models.SyncStatus
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.extensions.color
 import com.anytypeio.anytype.core_ui.extensions.tint
+import com.anytypeio.anytype.core_utils.ext.gone
+import com.anytypeio.anytype.core_utils.ext.visible
 
 class StatusBadgeWidget @JvmOverloads constructor(
     context: Context,
@@ -16,24 +17,29 @@ class StatusBadgeWidget @JvmOverloads constructor(
 
     init {
         setBackgroundResource(R.drawable.circle_solid_default)
-        tint(Color.WHITE)
+        tint(color = context.color(R.color.palette_dark_grey))
     }
 
     fun bind(status: SyncStatus?) {
         when (status) {
-            SyncStatus.UNKNOWN,
             SyncStatus.FAILED,
+            SyncStatus.INCOMPATIBLE_VERSION -> {
+                visible()
+                tint(color = context.color(R.color.palette_system_red))
+            }
+            SyncStatus.SYNCING -> {
+                visible()
+                tint(color = context.color(R.color.palette_system_amber_100))
+            }
+            SyncStatus.SYNCED -> {
+                visible()
+                tint(color = context.color(R.color.palette_system_green))
+            }
+            SyncStatus.UNKNOWN,
             SyncStatus.OFFLINE,
-            SyncStatus.INCOMPATIBLE_VERSION -> tint(
-                color = context.color(R.color.sync_status_red)
-            )
-            SyncStatus.SYNCING -> tint(
-                color = context.color(R.color.sync_status_orange)
-            )
-            SyncStatus.SYNCED -> tint(
-                color = context.color(R.color.sync_status_green)
-            )
-            else -> tint(Color.TRANSPARENT)
+            null -> {
+                gone()
+            }
         }
     }
 }

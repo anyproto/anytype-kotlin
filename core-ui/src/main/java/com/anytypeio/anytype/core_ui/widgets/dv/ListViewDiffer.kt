116cdfc1bf11b24106109896064525233f748ec2
konstantiniiv <ki@anytype.io>
2023-06-19 19:19:57 +0200

    DROID-1196 list view items differ

---
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/dv/ListViewDiffer.kt
@@ -0,0 +1,51 @@
+package com.anytypeio.anytype.core_ui.widgets.dv
+
+import androidx.recyclerview.widget.DiffUtil
+import com.anytypeio.anytype.presentation.sets.model.Viewer
+
+object ListViewDiffer : DiffUtil.ItemCallback<Viewer.ListView.Item>() {
+
+    const val PAYLOAD_CHECKED = 1
+    const val PAYLOAD_NAME = 2
+    const val PAYLOAD_ICON = 3
+    const val PAYLOAD_DESCRIPTION = 4
+    const val PAYLOAD_RELATION = 5
+
+
+    override fun areItemsTheSame(
+        oldItem: Viewer.ListView.Item,
+        newItem: Viewer.ListView.Item
+    ): Boolean = newItem.objectId == oldItem.objectId
+
+    override fun areContentsTheSame(
+        oldItem: Viewer.ListView.Item,
+        newItem: Viewer.ListView.Item
+    ): Boolean = oldItem == newItem
+
+    override fun getChangePayload(
+        oldItem: Viewer.ListView.Item,
+        newItem: Viewer.ListView.Item
+    ): List<Int> {
+        val payload = mutableListOf<Int>()
+        if (oldItem is Viewer.ListView.Item.Task && newItem is Viewer.ListView.Item.Task) {
+            if (oldItem.done != newItem.done) payload.add(PAYLOAD_CHECKED)
+            if (oldItem.name != newItem.name) payload.add(PAYLOAD_NAME)
+            if (oldItem.description != newItem.description) payload.add(PAYLOAD_DESCRIPTION)
+            if (oldItem.relations != newItem.relations) payload.add(PAYLOAD_RELATION)
+        }
+        if (oldItem is Viewer.ListView.Item.Profile && newItem is Viewer.ListView.Item.Profile) {
+            if (oldItem.name != newItem.name) payload.add(PAYLOAD_NAME)
+            if (oldItem.icon != newItem.icon) payload.add(PAYLOAD_ICON)
+            if (oldItem.description != newItem.description) payload.add(PAYLOAD_DESCRIPTION)
+            if (oldItem.relations != newItem.relations) payload.add(PAYLOAD_RELATION)
+        }
+        if (oldItem is Viewer.ListView.Item.Default && newItem is Viewer.ListView.Item.Default) {
+            if (oldItem.name != newItem.name) payload.add(PAYLOAD_NAME)
+            if (oldItem.icon != newItem.icon) payload.add(PAYLOAD_ICON)
+            if (oldItem.description != newItem.description) payload.add(PAYLOAD_DESCRIPTION)
+            if (oldItem.relations != newItem.relations) payload.add(PAYLOAD_RELATION)
+        }
+        return payload
+    }
+}
+

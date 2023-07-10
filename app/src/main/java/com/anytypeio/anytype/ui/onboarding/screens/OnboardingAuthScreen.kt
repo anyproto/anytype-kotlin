148c93ed55518a7c3e8546203f299aaa8bef75f7
uburoiubu <ubuphobos@gmail.com>
2023-07-10 14:36:07 +0200

    DROID-1493 fix

--- app/src/main/java/com/anytypeio/anytype/ui/onboarding/screens/OnboardingAuthScreen.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/onboarding/screens/OnboardingAuthScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.ClickableText
@@ -68,6 +70,7 @@ fun AuthScreen(
         Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
             Title(modifier = Modifier)
             Description()
+            Spacer(modifier = Modifier.height(72.dp))
         }
         Column(
             modifier = Modifier.fillMaxSize(),

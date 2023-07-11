3efad52c4e2e794f53456141e8c0b9867e132457
Evgenii Kozlov <ubuphobos@gmail.com>
2023-07-11 16:37:58 +0200

    DROID-1475 Onboarding | Enhancement | Blocking back button when sign-up is in progress (#168)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/onboarding/OnboardingViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/onboarding/OnboardingViewModel.kt
@@ -2,13 +2,35 @@ package com.anytypeio.anytype.presentation.onboarding
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.anytypeio.anytype.analytics.base.Analytics
 import com.anytypeio.anytype.presentation.common.BaseViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class OnboardingViewModel(
     private val analytics: Analytics
 ) : BaseViewModel() {
+
+    private val isInProgress = MutableStateFlow(false)
+
+    val navigation = MutableSharedFlow<Navigation>()
+
+    fun onBackPressed() {
+        if (!isInProgress.value) {
+            viewModelScope.launch {
+                navigation.emit(Navigation.Back)
+            }
+        } else {
+            sendToast(LOADING_MSG)
+        }
+    }
+
+    fun onLoadingStateChanged(isLoading: Boolean) {
+        isInProgress.value = isLoading
+    }
 
     class Factory @Inject constructor(
         private val analytics: Analytics
@@ -20,5 +42,13 @@ class OnboardingViewModel(
                 analytics = analytics
             ) as T
         }
+    }
+
+    sealed class Navigation {
+        object Back: Navigation()
+    }
+
+    companion object {
+        const val LOADING_MSG = "Loading... please wait."
     }
 }

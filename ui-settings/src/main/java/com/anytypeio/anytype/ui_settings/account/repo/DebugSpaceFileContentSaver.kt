0750ef29e2d3b7b0123ba42dc7ce7fceb9f0e2af
Evgenii Kozlov <ubuphobos@gmail.com>
2023-07-13 16:49:23 +0200

    DROID-1451 App | Tech | Space-debug refactoring (Part 1) (#192)

---
+++ ui-settings/src/main/java/com/anytypeio/anytype/ui_settings/account/repo/DebugSpaceFileContentSaver.kt
@@ -0,0 +1,62 @@
+package com.anytypeio.anytype.ui_settings.account.repo
+
+import android.content.Context
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.base.ResultInteractor
+import java.io.File
+import java.io.FileOutputStream
+
+class DebugSpaceFileContentSaver(
+    private val context: Context,
+    dispatchers: AppCoroutineDispatchers,
+) : ResultInteractor<DebugSpaceFileContentSaver.Params, File>(dispatchers.io) {
+
+    data class Params(
+        val content: String,
+        val filename: String,
+        val folderName: String = DEFAULT_FOLDER_NAME
+    )
+
+    override suspend fun doWork(params: Params): File {
+        val cacheDir = context.cacheDir
+
+        require(cacheDir != null) { "Impossible to cache files!" }
+
+        // Creating folder
+
+        val downloadFolder = File("${cacheDir.path}/${params.folderName}/").apply {
+            mkdirs()
+        }
+
+        val resultFilePath = "${cacheDir.path}/${params.folderName}/${params.filename}"
+        val resultFile = File(resultFilePath)
+
+        // Writing content
+
+        val tempFileFolderPath = "${downloadFolder.absolutePath}/${TEMP_FOLDER_NAME}"
+        val tempDir = File(tempFileFolderPath)
+        if (tempDir.exists()) tempDir.deleteRecursively()
+        tempDir.mkdirs()
+
+        val tempResult = File(tempFileFolderPath, params.filename)
+
+        FileOutputStream(tempResult).use { stream ->
+            stream.write(params.content.toByteArray())
+        }
+
+        tempResult.renameTo(resultFile)
+
+        // Clearing
+
+        tempDir.deleteRecursively()
+
+        // Sending file
+
+        return resultFile
+    }
+
+    companion object {
+        const val DEFAULT_FOLDER_NAME = "debug_space"
+        const val TEMP_FOLDER_NAME = "tmp"
+    }
+}
--- ui-settings/src/main/java/com/anytypeio/anytype/ui_settings/account/repo/DebugSpaceShareDownloader.kt
+++ ui-settings/src/main/java/com/anytypeio/anytype/ui_settings/account/repo/DebugSpaceShareDownloader.kt
@@ -4,25 +4,37 @@ import android.net.Uri
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.base.ResultInteractor
 import com.anytypeio.anytype.domain.debugging.DebugSpace
+import com.anytypeio.anytype.presentation.util.downloader.UriFileProvider
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Locale
 
 class DebugSpaceShareDownloader(
     private val debugSpace: DebugSpace,
-    private val fileSaver: FileSaver,
+    private val fileSaver: DebugSpaceFileContentSaver,
+    private val uriFileProvider: UriFileProvider,
     dispatchers: AppCoroutineDispatchers,
 ) : ResultInteractor<Unit, Uri>(dispatchers.io) {
 
     private fun getFileName(): String {
         val date = Calendar.getInstance().time
-        val dateFormat = SimpleDateFormat("dd-MM-yyyy-HH:mm:ss", Locale.getDefault())
+        val dateFormat = SimpleDateFormat(DATE_FORMAT, Locale.getDefault())
         val formattedDate = dateFormat.format(date)
-        val fileName = "DebugSpace$formattedDate.txt"
-        return fileName
+        return "DebugSpace$formattedDate.txt"
     }
 
     override suspend fun doWork(params: Unit): Uri {
         val content = debugSpace.run(Unit)
-        return fileSaver.run(FileSaver.Params(content = content, name = getFileName()))
+        val file = fileSaver.run(
+            DebugSpaceFileContentSaver.Params(
+                content = content,
+                filename = getFileName()
+            )
+        )
+        return uriFileProvider.getUriForFile(file)
+    }
+
+    companion object {
+        const val DATE_FORMAT = "dd-MM-yyyy-HH:mm:ss"
     }
 }

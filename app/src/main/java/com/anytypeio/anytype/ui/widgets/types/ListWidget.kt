061238136957611c45c2441c5493f7cd2aad22a0
Evgenii Kozlov <ubuphobos@gmail.com>
2023-07-17 13:50:35 +0200

    DROID-1504 Widgets | Fix | Layout fixes in tree or compact list widgets (#209)

--- app/src/main/java/com/anytypeio/anytype/ui/widgets/types/ListWidget.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/widgets/types/ListWidget.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.colorResource
@@ -149,7 +150,8 @@ fun CompactListWidgetList(
                 TreeWidgetObjectIcon(
                     icon = element.objectIcon,
                     paddingStart = 8.dp,
-                    paddingEnd = 4.dp
+                    paddingEnd = 4.dp,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
                 Text(
                     text = element.obj.getWidgetObjectName() ?: stringResource(id = R.string.untitled),

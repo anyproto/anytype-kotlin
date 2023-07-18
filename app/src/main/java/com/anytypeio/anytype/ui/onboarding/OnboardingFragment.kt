1c3f1d1f20c6599b297b9ca6d8969d9fd56844a5
Evgenii Kozlov <ubuphobos@gmail.com>
2023-07-18 14:47:55 +0200

    DROID-1553 Onboarding | Enhancement | Colorise recovery phrase with on "Join" flow (#221)

--- app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingFragment.kt
@@ -61,6 +61,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.anytypeio.anytype.R
 import com.anytypeio.anytype.core_ui.BuildConfig
+import com.anytypeio.anytype.core_ui.MNEMONIC_WORD_COUNT
+import com.anytypeio.anytype.core_ui.MnemonicPhrasePaletteColors
 import com.anytypeio.anytype.core_ui.views.BaseAlertDialog
 import com.anytypeio.anytype.core_utils.ext.toast
 import com.anytypeio.anytype.core_utils.insets.RootViewDeferringInsetsCallback
@@ -104,6 +106,14 @@ class OnboardingFragment : Fragment() {
     lateinit var factory: OnboardingViewModel.Factory
 
     private val onBoardingViewModel by viewModels<OnboardingViewModel> { factory }
+
+    private val mnemonicColorPalette by lazy {
+        buildList {
+            repeat(MNEMONIC_WORD_COUNT) {
+                add(MnemonicPhrasePaletteColors.random())
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -314,7 +324,11 @@ class OnboardingFragment : Fragment() {
             ) {
                 currentPage.value = OnboardingPage.MNEMONIC
                 backButtonCallback.value = { navController.popBackStack() }
-                Mnemonic(navController, ContentPaddingTop())
+                Mnemonic(
+                    navController = navController,
+                    contentPaddingTop = ContentPaddingTop(),
+                    mnemonicColorPalette = mnemonicColorPalette
+                )
             }
             composable(
                 route = OnboardingNavigation.createSoul,
@@ -541,7 +555,11 @@ class OnboardingFragment : Fragment() {
     }
 
     @Composable
-    private fun Mnemonic(navController: NavHostController, contentPaddingTop: Int) {
+    private fun Mnemonic(
+        navController: NavHostController,
+        contentPaddingTop: Int,
+        mnemonicColorPalette: List<Color>
+    ) {
         val component = componentManager().onboardingMnemonicComponent.ReleaseOn(
             viewLifecycleOwner = viewLifecycleOwner,
             state = Lifecycle.State.DESTROYED
@@ -555,7 +573,8 @@ class OnboardingFragment : Fragment() {
                 vm.sendAnalyticsOnboardingScreen()
             },
             copyMnemonicToClipboard = ::copyMnemonicToClipboard,
-            vm = vm
+            vm = vm,
+            mnemonicColorPalette = mnemonicColorPalette
         )
     }
 

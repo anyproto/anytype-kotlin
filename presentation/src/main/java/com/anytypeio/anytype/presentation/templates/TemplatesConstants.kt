8e5f97a775eb13b3efb95417d79126979cc0df04
konstantiniiv <ki@anytype.io>
2023-07-24 14:55:26 +0200

    DROID-1431 template constants

--- presentation/src/main/java/com/anytypeio/anytype/presentation/templates/TemplatesConstants.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/templates/TemplatesConstants.kt
@@ -1,4 +1,8 @@
 package com.anytypeio.anytype.presentation.templates
 
-object TemplatesConstants {
+object TemplateConstants {
+    const val BLANK_TITLE = "Blank template"
+    const val BLANK_ROOT_ID = "blank_template_root_id"
+    const val HEADER_ID = "header"
+    const val TITLE_ID = "blockTitle"
 }

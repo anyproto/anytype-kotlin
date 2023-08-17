b72e940c8f1bd4e7388f112d1892e11dac6ca986
uburoiubu <ubuphobos@gmail.com>
2023-08-17 19:33:44 +0200

    DROID-1656 fix

--- app/src/main/java/com/anytypeio/anytype/ui/spaces/SelectSpaceFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/spaces/SelectSpaceFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -14,7 +15,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
+import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
 import com.anytypeio.anytype.core_utils.ui.BaseComposeFragment
 import com.anytypeio.anytype.di.common.componentManager
 import com.anytypeio.anytype.presentation.onboarding.OnboardingViewModel
@@ -23,7 +26,7 @@ import com.anytypeio.anytype.ui.settings.typography
 import javax.inject.Inject
 import timber.log.Timber
 
-class SelectSpaceFragment : BaseComposeFragment() {
+class SelectSpaceFragment : BaseBottomSheetComposeFragment() {
 
     @Inject
     lateinit var factory: SelectSpaceViewModel.Factory
@@ -41,11 +44,15 @@ class SelectSpaceFragment : BaseComposeFragment() {
                 typography = typography
             ) {
                 val spaces = vm.spaces.collectAsState().value
-                Box(modifier = Modifier.fillMaxSize()) {
-                    LazyColumn {
-                        items(spaces) { item ->
-                            Text(text = item.toString())
+                Box(modifier = Modifier.padding(20.dp)) {
+                    if (spaces.isNotEmpty()) {
+                        LazyColumn {
+                            items(spaces) { item ->
+                                Text(text = item.toString())
+                            }
                         }
+                    } else {
+                        Text(text = "Empty!")
                     }
                 }
             }

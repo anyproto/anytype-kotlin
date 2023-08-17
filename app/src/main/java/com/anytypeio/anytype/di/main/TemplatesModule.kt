9d25c9065fadb19dea93d704df70e108e075b74a
konstantiniiv <ki@anytype.io>
2023-08-17 09:56:43 +0200

    DROID-1590 templates container + di

---
+++ app/src/main/java/com/anytypeio/anytype/di/main/TemplatesModule.kt
@@ -0,0 +1,41 @@
+package com.anytypeio.anytype.di.main
+
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.block.repo.BlockRepository
+import com.anytypeio.anytype.domain.library.StorelessSubscriptionContainer
+import com.anytypeio.anytype.domain.library.StorelessSubscriptionContainer.Companion.SUBSCRIPTION_TEMPLATES
+import com.anytypeio.anytype.domain.search.SubscriptionEventChannel
+import com.anytypeio.anytype.domain.workspace.WorkspaceManager
+import com.anytypeio.anytype.presentation.templates.DefaultObjectTypeTemplatesContainer
+import com.anytypeio.anytype.presentation.templates.ObjectTypeTemplatesContainer
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+object TemplatesModule {
+
+    @JvmStatic
+    @Provides
+    @Singleton
+    @Named(SUBSCRIPTION_TEMPLATES)
+    fun provideStorelessContainer(
+        repo: BlockRepository,
+        channel: SubscriptionEventChannel,
+        dispatchers: AppCoroutineDispatchers,
+    ): StorelessSubscriptionContainer =
+        StorelessSubscriptionContainer.Impl(repo, channel, dispatchers)
+
+    @JvmStatic
+    @Provides
+    @Singleton
+    fun provideTemplatesContainer(
+        @Named(SUBSCRIPTION_TEMPLATES) storage: StorelessSubscriptionContainer,
+        workspaceManager: WorkspaceManager
+    ): ObjectTypeTemplatesContainer =
+        DefaultObjectTypeTemplatesContainer(
+            storage = storage,
+            workspaceManager = workspaceManager
+        )
+}

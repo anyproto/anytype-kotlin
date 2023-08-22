fbde7c32ac29c692782d8fc2892c09d2db50af43
uburoiubu <ubuphobos@gmail.com>
2023-08-22 14:36:22 +0200

    DROID-1658 fixes

--- domain/src/main/java/com/anytypeio/anytype/domain/spaces/CreateSpace.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/spaces/CreateSpace.kt
@@ -5,8 +5,9 @@ import com.anytypeio.anytype.core_models.Struct
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.base.ResultInteractor
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
+import javax.inject.Inject
 
-class CreateSpace(
+class CreateSpace @Inject constructor(
     private val repo: BlockRepository,
     private val dispatchers: AppCoroutineDispatchers
 ) : ResultInteractor<CreateSpace.Params, Id>(dispatchers.io) {
--- presentation/src/main/java/com/anytypeio/anytype/presentation/spaces/CreateSpaceViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/spaces/CreateSpaceViewModel.kt
@@ -2,16 +2,49 @@ package com.anytypeio.anytype.presentation.spaces
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.anytypeio.anytype.core_models.Id
+import com.anytypeio.anytype.core_models.Relations
+import com.anytypeio.anytype.domain.base.fold
+import com.anytypeio.anytype.domain.spaces.CreateSpace
 import com.anytypeio.anytype.presentation.common.BaseViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class CreateSpaceViewModel @Inject constructor() : BaseViewModel() {
+class CreateSpaceViewModel @Inject constructor(
+    private val createSpace: CreateSpace
+) : BaseViewModel() {
+
+    fun onCreateSpace(name: String) {
+        viewModelScope.launch {
+            createSpace.async(
+                CreateSpace.Params(
+                    details = mapOf(
+                        Relations.NAME to name
+                    )
+                )
+            ).fold(
+                onSuccess = { space: Id ->
+                    Timber.d("Successfully created space: $space")
+                },
+                onFailure = {
+                    Timber.e(it, "Error while creating space").also {
+                        sendToast("Error while creating space, please try again.")
+                    }
+                }
+            )
+        }
+    }
 
     class Factory @Inject constructor(
+        private val createSpace: CreateSpace
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(
             modelClass: Class<T>
-        ) = CreateSpaceViewModel() as T
+        ) = CreateSpaceViewModel(
+            createSpace = createSpace
+        ) as T
     }
 }

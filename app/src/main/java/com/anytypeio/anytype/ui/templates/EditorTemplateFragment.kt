91fc0a9d11b158dd24b60b48ae5693e2e6760e75
konstantiniiv <ki@anytype.io>
2023-08-23 13:40:37 +0200

    DROID-1591 refactoring

--- app/src/main/java/com/anytypeio/anytype/ui/templates/EditorTemplateFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/templates/EditorTemplateFragment.kt
@@ -20,33 +20,7 @@ class EditorTemplateFragment : EditorFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.root.apply {
-            background = null
-        }
-        binding.recycler.updateLayoutParams<ConstraintLayout.LayoutParams> {
-            topMargin = dimen(R.dimen.default_toolbar_height)
-        }
-        binding.topToolbar.apply {
-            container.alpha = 1f
-        }
-        binding.recycler.apply {
-            removeOnScrollListener(titleVisibilityDetector)
-        }
-        binding.topToolbar.title.updateLayoutParams<LinearLayout.LayoutParams> {
-            width = LinearLayout.LayoutParams.WRAP_CONTENT
-            height = LinearLayout.LayoutParams.MATCH_PARENT
-        }
-        binding.topToolbar.title.apply {
-            setTextAppearance(R.style.TextView_UXStyle_Titles_2_Medium)
-        }
-        binding.btnSelectTemplate.setOnClickListener {
-            Timber.d("Select template clicked, get back to Set")
-            findNavController().apply {
-
-                previousBackStackEntry?.savedStateHandle?.set(ARG_TEMPLATE_ID, ctx)
-                popBackStack(R.id.editorModalScreen, true)
-            }
-        }
+        initializeBinding()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -74,10 +48,36 @@ class EditorTemplateFragment : EditorFragment() {
         }
     }
 
-    companion object {
-        fun newInstance(id: String): EditorTemplateFragment = EditorTemplateFragment().apply {
-            arguments = bundleOf(ID_KEY to id)
+    private fun initializeBinding() {
+        with(binding) {
+            root.background = null
+            recycler.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                topMargin = dimen(R.dimen.default_toolbar_height)
+            }
+            topToolbar.apply {
+                container.alpha = 1f
+            }
+            recycler.removeOnScrollListener(titleVisibilityDetector)
+            topToolbar.title.updateLayoutParams<LinearLayout.LayoutParams> {
+                width = LinearLayout.LayoutParams.WRAP_CONTENT
+                height = LinearLayout.LayoutParams.MATCH_PARENT
+            }
+            topToolbar.title.setTextAppearance(R.style.TextView_UXStyle_Titles_2_Medium)
+            btnSelectTemplate.setOnClickListener {
+                Timber.d("Select template clicked, get back to Set")
+                findNavController().apply {
+                    previousBackStackEntry?.savedStateHandle?.set(ARG_TEMPLATE_ID, ctx)
+                    popBackStack(R.id.editorModalScreen, true)
+                }
+            }
         }
+    }
+
+    companion object {
+        fun newInstance(id: String): EditorTemplateFragment =
+            EditorTemplateFragment().apply {
+                arguments = bundleOf(ID_KEY to id)
+            }
 
         const val ARG_TEMPLATE_ID = "template_id"
     }

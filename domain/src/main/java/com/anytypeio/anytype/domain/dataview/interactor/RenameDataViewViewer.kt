b7d5463e5261a2ef338750bf978701f845082b12
konstantiniiv <ki@anytype.io>
2023-08-24 16:13:28 +0200

    DROID-1622 viewer usecases

--- domain/src/main/java/com/anytypeio/anytype/domain/dataview/interactor/RenameDataViewViewer.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/dataview/interactor/RenameDataViewViewer.kt
@@ -1,22 +1,24 @@
 package com.anytypeio.anytype.domain.dataview.interactor
 
-import com.anytypeio.anytype.domain.base.BaseUseCase
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
 import com.anytypeio.anytype.core_models.DVViewer
 import com.anytypeio.anytype.core_models.Id
 import com.anytypeio.anytype.domain.dataview.interactor.RenameDataViewViewer.Params
 import com.anytypeio.anytype.core_models.Payload
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.base.ResultInteractor
 
 /**
  * Use-case for renaming data view's view.
  * @see [Params] for details.
  */
 class RenameDataViewViewer(
-    private val repo: BlockRepository
-) : BaseUseCase<Payload, Params>() {
+    private val repo: BlockRepository,
+    dispatchers: AppCoroutineDispatchers
+) : ResultInteractor<Params, Payload>(dispatchers.io) {
 
-    override suspend fun run(params: Params) = safe {
-        repo.updateDataViewViewer(
+    override suspend fun doWork(params: Params): Payload {
+        return repo.updateDataViewViewer(
             context = params.context,
             target = params.target,
             viewer = params.viewer

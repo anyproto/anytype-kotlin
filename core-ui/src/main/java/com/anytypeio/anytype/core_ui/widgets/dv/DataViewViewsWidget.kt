d80955004561a72d45fc3d1c3273ffad8ccdb492
konstantiniiv <ki@anytype.io>
2023-08-24 16:12:06 +0200

    DROID-1622 viewer widget

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/dv/DataViewViewsWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/dv/DataViewViewsWidget.kt
@@ -1,7 +1,6 @@
 package com.anytypeio.anytype.core_ui.widgets.dv
 
 import androidx.compose.foundation.layout.Box
-import androidx.annotation.ColorRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -9,32 +8,18 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
@@ -44,38 +29,36 @@ import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.Visibility
 import com.anytypeio.anytype.core_ui.R
+import com.anytypeio.anytype.core_ui.foundation.Divider
 import com.anytypeio.anytype.core_ui.foundation.noRippleClickable
 import com.anytypeio.anytype.core_ui.views.BodyCalloutRegular
+import com.anytypeio.anytype.core_ui.views.HeadlineSubheading
 import com.anytypeio.anytype.core_ui.views.Title1
 import com.anytypeio.anytype.core_ui.widgets.DragStates
 import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.DoneMode
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.EditMode
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.Delete
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.Edit
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.Position
+import com.anytypeio.anytype.presentation.sets.DVViewsWidgetUiState.Clicks.Dismiss
+import com.anytypeio.anytype.presentation.sets.viewer.ViewerView
 import kotlin.math.roundToInt
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -85,7 +68,7 @@ import kotlinx.coroutines.launch
 fun DataViewViewsWidget(
     scope: CoroutineScope,
     state: DVViewsWidgetUiState,
-    onDismiss: () -> Unit,
+    click: (DVViewsWidgetUiState.Clicks) -> Unit
 ) {
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomStart)
@@ -94,10 +77,25 @@ fun DataViewViewsWidget(
         val currentState by rememberUpdatedState(state)
         val swipeableState = rememberSwipeableState(DragStates.VISIBLE)
 
+        AnimatedVisibility(
+            visible = currentState.showWidget,
+            enter = fadeIn(),
+            exit = fadeOut(
+                tween(200)
+            )
+        ) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.4f))
+                    .noRippleClickable { click.invoke(Dismiss) }
+            )
+        }
+
         if (swipeableState.isAnimationRunning) {
             DisposableEffect(Unit) {
                 onDispose {
-                    onDismiss()
+                    click(Dismiss)
                 }
             }
         }
@@ -132,6 +130,11 @@ fun DataViewViewsWidget(
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
+                    .shadow(
+                        elevation = 40.dp,
+                        spotColor = Color(0x40000000),
+                        ambientColor = Color(0x40000000)
+                    )
                     .padding(start = 8.dp, end = 8.dp, bottom = 31.dp)
                     .background(
                         color = colorResource(id = R.color.background_secondary),
@@ -142,7 +145,7 @@ fun DataViewViewsWidget(
                     modifier = Modifier
                         .fillMaxWidth()
                         .wrapContentHeight()
-                        .padding(bottom = 24.dp)
+                        .padding(bottom = 16.dp)
                 ) {
                     Box(
                         modifier = Modifier
@@ -156,12 +159,12 @@ fun DataViewViewsWidget(
                             if (currentState.isEditing) {
                                 ActionText(
                                     text = stringResource(id = R.string.done),
-                                    click = {}
+                                    click = { click(DoneMode) }
                                 )
                             } else {
                                 ActionText(
                                     text = stringResource(id = R.string.edit),
-                                    click = {}
+                                    click = { click(EditMode) }
                                 )
                             }
                         }
@@ -187,6 +190,7 @@ fun DataViewViewsWidget(
                     }
                     val itemsScroll = rememberLazyListState()
 
+                    ViewsList(state = currentState, click = click)
 //                    if (currentState.isMoreMenuVisible && itemsScroll.isScrollInProgress) {
 //                        onDismiss()
 //                    }
@@ -197,8 +201,94 @@ fun DataViewViewsWidget(
 }
 
 @Composable
-private fun ViewsList(scrollState: ScrollState: DVViewsWidgetUiState) {
-    LazyColumn(state = state.items) {
+private fun ViewsList(
+    state: DVViewsWidgetUiState,
+    click: (DVViewsWidgetUiState.Clicks) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        itemsIndexed(state.items) { index, view ->
+            ViewsItem(state = state, view = view, click = click)
+            if (index != state.items.size - 1) {
+                Divider()
+            }
+        }
+    }
+}
+
+@Composable
+private fun ViewsItem(
+    state: DVViewsWidgetUiState,
+    view: ViewerView,
+    click: (DVViewsWidgetUiState.Clicks) -> Unit
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .height(52.dp)
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp)
+    ) {
+        val (delete, text, edit, dnd) = createRefs()
+        Image(
+            modifier = Modifier
+                .noRippleClickable {
+                    click.invoke(Delete(view.id))
+                }
+                .constrainAs(delete) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    visibility =
+                        if (state.isEditing && !view.isActive) Visibility.Visible else Visibility.Gone
+                },
+            painter = painterResource(id = R.drawable.ic_relation_delete),
+            contentDescription = "Delete view"
+        )
+        Image(
+            modifier = Modifier.constrainAs(dnd) {
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                visibility = if (state.isEditing) Visibility.Visible else Visibility.Gone
+
+            },
+            painter = painterResource(id = R.drawable.ic_dnd),
+            contentDescription = "Dnd view"
+        )
+        Image(
+            modifier = Modifier
+                .noRippleClickable {
+                    click.invoke(Edit(view.id))
+                }
+                .constrainAs(edit) {
+                    end.linkTo(dnd.start, margin = 16.dp)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    visibility = if (state.isEditing) Visibility.Visible else Visibility.Gone
+                },
+            painter = painterResource(id = R.drawable.ic_edit_24),
+            contentDescription = "Edit view"
+        )
+        Text(
+            modifier = Modifier
+                .noRippleClickable {
+                    //click(Position(view.id)
+                }
+                .constrainAs(text) {
+                    start.linkTo(delete.end, margin = 12.dp, goneMargin = 0.dp)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(edit.start, margin = 20.dp, goneMargin = 0.dp)
+                    width = Dimension.fillToConstraints
+                },
+            text = view.name,
+            color = colorResource(id = if (view.isActive) R.color.text_primary else R.color.glyph_active),
+            style = HeadlineSubheading,
+            textAlign = TextAlign.Left
+        )
     }
 }
 
@@ -207,7 +297,7 @@ private fun ActionText(text: String, click: () -> Unit) {
     Text(
         modifier = Modifier
             .padding(
-                start = 15.dp,
+                start = 16.dp,
                 top = 12.dp,
                 bottom = 12.dp,
                 end = 16.dp

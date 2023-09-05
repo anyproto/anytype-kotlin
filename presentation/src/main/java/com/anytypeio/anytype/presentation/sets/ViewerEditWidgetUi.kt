531fca215e29f931757c137219a7f22cfc4496a8
konstantiniiv <ki@anytype.io>
2023-09-05 17:21:25 +0200

    DROID-1617 viewer ui state

--- presentation/src/main/java/com/anytypeio/anytype/presentation/sets/ViewerEditWidgetUi.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/sets/ViewerEditWidgetUi.kt
@@ -7,48 +7,55 @@ import com.anytypeio.anytype.core_models.ObjectTypeIds
 import com.anytypeio.anytype.core_models.ObjectWrapper
 import com.anytypeio.anytype.domain.objects.StoreOfObjectTypes
 import com.anytypeio.anytype.domain.objects.StoreOfRelations
-import com.anytypeio.anytype.presentation.sets.viewer.ViewerView
 
 data class ViewerEditWidgetUi(
     val showWidget: Boolean,
-    val viewerView: ViewerView? = null,
+    val id: Id? = null,
     val name: String = "",
-    val defaultObjectType: ObjectWrapper.Type?,
-    val isDefaultObjectTypeEnabled: Boolean = false,
     val layout: DVViewerType?,
     val relations: List<String> = emptyList(),
     val filters: List<String> = emptyList(),
     val sorts: List<String> = emptyList(),
-    val defaultTemplate: Id? = null
+    val defaultTemplate: Id? = null,
+    val defaultObjectType: ObjectWrapper.Type?,
+    val isDefaultObjectTypeEnabled: Boolean = false,
 ) {
 
     fun empty() = this.copy(
+        id = null,
         name = "",
-        defaultObjectType = null,
-        isDefaultObjectTypeEnabled = false,
         layout = null,
         relations = emptyList(),
         filters = emptyList(),
-        sorts = emptyList()
+        sorts = emptyList(),
+        defaultTemplate = null,
+        defaultObjectType = null,
+        isDefaultObjectTypeEnabled = false,
     )
 
     companion object {
         fun init() = ViewerEditWidgetUi(
             showWidget = false,
+            id = null,
+            name = "",
+            layout = null,
+            relations = emptyList(),
+            filters = emptyList(),
+            sorts = emptyList(),
+            defaultTemplate = null,
             defaultObjectType = null,
             isDefaultObjectTypeEnabled = false,
-            layout = null
         )
     }
 
     sealed class Action {
         object Dismiss : Action()
-        data class UpdateName(val name: String) : Action()
-        object DefaultObjectType : Action()
-        object Layout : Action()
-        object Relations : Action()
-        object Filters : Action()
-        object Sorts : Action()
+        data class UpdateName(val id: Id?, val name: String) : Action()
+        data class DefaultObjectType(val id: Id?) : Action()
+        data class Layout(val id: Id?) : Action()
+        data class Relations(val id: Id?) : Action()
+        data class Filters(val id: Id?) : Action()
+        data class Sorts(val id: Id?) : Action()
     }
 }
 
@@ -71,12 +78,12 @@ suspend fun ViewerEditWidgetUi.updateState(
     val viewerDefaultObjectTypeId = dvViewer.defaultObjectType ?: ObjectTypeIds.PAGE
     val viewerDefaultTemplateId = dvViewer.defaultTemplate
     val defaultObjectType = storeOfObjectTypes.get(viewerDefaultObjectTypeId)
-
     return this.copy(
+        id = dvViewer.id,
         name = dvViewer.name,
         sorts = dvViewer.sorts.toView(storeOfRelations) { it.relationKey },
         filters = dvViewer.filters.toView(storeOfRelations) { it.relation },
-        relations = dvViewer.viewerRelations.toView (storeOfRelations) { it.key },
+        relations = dvViewer.viewerRelations.toView(storeOfRelations) { it.key },
         layout = dvViewer.type,
         defaultObjectType = defaultObjectType,
         isDefaultObjectTypeEnabled = isDefaultObjectTypeEnabled,

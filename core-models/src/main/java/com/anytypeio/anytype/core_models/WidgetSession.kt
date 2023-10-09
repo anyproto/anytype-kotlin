2953f6ca5652bd6191af014e8018618e374937cb
Evgenii Kozlov <ubuphobos@gmail.com>
2023-10-09 14:58:17 +0200

    DROID-1778 Multispaces | Enhancement | Collapsed widget state should be saved and restored when restarting the app or switching between spaces. (#425)

--- core-models/src/main/java/com/anytypeio/anytype/core_models/WidgetSession.kt
+++ core-models/src/main/java/com/anytypeio/anytype/core_models/WidgetSession.kt
@@ -4,7 +4,6 @@ package com.anytypeio.anytype.core_models
  * @property [collapsed] list of collapsed widgets
  * @property [widgetsToActiveViews] maps specific widget to the selected view inside this widget
  */
-// TODO scope to space
 data class WidgetSession(
     val collapsed: List<Id> = emptyList(),
     val widgetsToActiveViews: Map<Id, Id> = emptyMap()

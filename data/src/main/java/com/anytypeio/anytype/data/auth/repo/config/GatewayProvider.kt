1ae291f5d837a3cd980fa8bbca9628856135f1ce
uburoiubu <ubuphobos@gmail.com>
2023-10-18 17:40:43 +0200

    DROID-1777 support per-space gateway

--- data/src/main/java/com/anytypeio/anytype/data/auth/repo/config/GatewayProvider.kt
+++ data/src/main/java/com/anytypeio/anytype/data/auth/repo/config/GatewayProvider.kt
@@ -1,10 +1,19 @@
 package com.anytypeio.anytype.data.auth.repo.config
 
-import com.anytypeio.anytype.domain.config.ConfigStorage
 import com.anytypeio.anytype.domain.config.Gateway
+import com.anytypeio.anytype.domain.debugging.Logger
+import com.anytypeio.anytype.domain.workspace.SpaceManager
 
 class GatewayProvider(
-    private val configStorage: ConfigStorage
+    private val spaceManager: SpaceManager,
+    private val logger: Logger
 ) : Gateway {
-    override fun provide(): String = configStorage.get().gateway
+    override fun provide(): String {
+        val gateway = spaceManager.getConfig()?.gateway
+        if (gateway != null) {
+            logger.logException(IllegalStateException("Gateway not found"))
+        }
+        return gateway.orEmpty()
+    }
+
 }

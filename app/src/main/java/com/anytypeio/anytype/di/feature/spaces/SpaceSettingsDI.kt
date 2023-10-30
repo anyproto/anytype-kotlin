005758a26fe1443944c4ac43a92a9ea3960f0022
uburoiubu <ubuphobos@gmail.com>
2023-10-30 11:36:25 +0100

    DROID-1840 fix

--- app/src/main/java/com/anytypeio/anytype/di/feature/spaces/SpaceSettingsDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/spaces/SpaceSettingsDI.kt
@@ -1,17 +1,24 @@
 package com.anytypeio.anytype.di.feature.spaces
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import com.anytypeio.anytype.analytics.base.Analytics
 import com.anytypeio.anytype.core_utils.di.scope.PerScreen
+import com.anytypeio.anytype.device.share.debug.DebugSpaceDeviceFileContentSaver
 import com.anytypeio.anytype.di.common.ComponentDependencies
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
 import com.anytypeio.anytype.domain.config.ConfigStorage
+import com.anytypeio.anytype.domain.debugging.DebugSpace
+import com.anytypeio.anytype.domain.debugging.DebugSpaceContentSaver
+import com.anytypeio.anytype.domain.debugging.DebugSpaceShareDownloader
 import com.anytypeio.anytype.domain.library.StorelessSubscriptionContainer
 import com.anytypeio.anytype.domain.misc.UrlBuilder
 import com.anytypeio.anytype.domain.workspace.SpaceManager
 import com.anytypeio.anytype.presentation.spaces.SpaceGradientProvider
 import com.anytypeio.anytype.presentation.spaces.SpaceSettingsViewModel
+import com.anytypeio.anytype.presentation.util.downloader.UriFileProvider
+import com.anytypeio.anytype.providers.DefaultUriFileProvider
 import com.anytypeio.anytype.ui.settings.space.SpaceSettingsFragment
 import dagger.Binds
 import dagger.Component
@@ -42,8 +49,32 @@ object SpaceSettingsModule {
     @PerScreen
     fun provideSpaceGradientProvider(): SpaceGradientProvider = SpaceGradientProvider.Default
 
+    @JvmStatic
+    @Provides
+    @PerScreen
+    fun provide(
+        debugSpace: DebugSpace,
+        debugSpaceContentSaver: DebugSpaceContentSaver,
+        dispatchers: AppCoroutineDispatchers
+    ) : DebugSpaceShareDownloader = DebugSpaceShareDownloader(
+        debugSpace = debugSpace,
+        debugSpaceContentSaver = debugSpaceContentSaver,
+        dispatchers = dispatchers
+    )
+
     @Module
     interface Bindings {
+        @PerScreen
+        @Binds
+        fun bindUriFileProvider(
+            defaultProvider: DefaultUriFileProvider
+        ): UriFileProvider
+
+        @PerScreen
+        @Binds
+        fun bindSpaceDebugDeviceSharer(
+            saver: DebugSpaceDeviceFileContentSaver
+        ): DebugSpaceContentSaver
         @PerScreen
         @Binds
         fun factory(factory: SpaceSettingsViewModel.Factory): ViewModelProvider.Factory
@@ -58,4 +89,5 @@ interface SpaceSettingsDependencies : ComponentDependencies {
     fun spaceManager(): SpaceManager
     fun container(): StorelessSubscriptionContainer
     fun config(): ConfigStorage
+    fun context(): Context
 }

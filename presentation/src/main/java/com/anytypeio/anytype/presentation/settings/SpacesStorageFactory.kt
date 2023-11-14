21b15bd7443090c33df02e7ee2250883e352c0fa
konstantiniiv <ki@anytype.io>
2023-11-14 23:10:16 +0100

    DROID-1886 factory

---
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/settings/SpacesStorageFactory.kt
@@ -0,0 +1,41 @@
+package com.anytypeio.anytype.presentation.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.anytypeio.anytype.analytics.base.Analytics
+import com.anytypeio.anytype.device.BuildProvider
+import com.anytypeio.anytype.domain.auth.interactor.GetAccount
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.library.StorelessSubscriptionContainer
+import com.anytypeio.anytype.domain.misc.UrlBuilder
+import com.anytypeio.anytype.domain.workspace.InterceptFileLimitEvents
+import com.anytypeio.anytype.domain.workspace.SpaceManager
+import com.anytypeio.anytype.domain.workspace.SpacesUsageInfo
+import javax.inject.Inject
+
+class SpacesStorageFactory @Inject constructor(
+    private val analytics: Analytics,
+    private val spaceManager: SpaceManager,
+    private val urlBuilder: UrlBuilder,
+    private val appCoroutineDispatchers: AppCoroutineDispatchers,
+    private val spacesUsageInfo: SpacesUsageInfo,
+    private val interceptFileLimitEvents: InterceptFileLimitEvents,
+    private val buildProvider: BuildProvider,
+    private val storelessSubscriptionContainer: StorelessSubscriptionContainer,
+    private val getAccount: GetAccount,
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ): T = SpacesStorageViewModel(
+        analytics = analytics,
+        spaceManager = spaceManager,
+        urlBuilder = urlBuilder,
+        appCoroutineDispatchers = appCoroutineDispatchers,
+        spacesUsageInfo = spacesUsageInfo,
+        interceptFileLimitEvents = interceptFileLimitEvents,
+        buildProvider = buildProvider,
+        storelessSubscriptionContainer = storelessSubscriptionContainer,
+        getAccount = getAccount,
+    ) as T
+}

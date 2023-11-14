a8ec0fe6333bc656de90fb7c1707bae0b20073f6
Evgenii Kozlov <ubuphobos@gmail.com>
2023-11-14 18:57:23 +0100

    DROID-1944 Multispaces | Fix | Re-set global subscriptions for type and relations when space is switched  (#546)

--- domain/src/main/java/com/anytypeio/anytype/domain/search/ObjectTypesSubscriptionContainer.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/search/ObjectTypesSubscriptionContainer.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.scan
+import kotlinx.coroutines.withContext
 
 class ObjectTypesSubscriptionContainer(
     private val repo: BlockRepository,
@@ -126,6 +127,14 @@ class ObjectTypesSubscriptionContainer(
      * Returns events for subscriptions and dependent subscriptions
      */
     private fun subscribe(subscriptions: List<Id>) = channel.subscribe(subscriptions)
+
+    suspend fun unsubscribe() = withContext(dispatchers.io) {
+        runCatching {
+            repo.cancelObjectSearchSubscription(
+                listOf(SUBSCRIPTION_ID)
+            )
+        }
+    }
 
     data class Params(
         val subscription: Id,

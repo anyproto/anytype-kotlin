9ba038520190247ecfa2b37e9febbbc30f317964
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2023-11-21 17:16:58 +0100

    DROID-1984 Protocol  | Fix | Type source object as list (#581)

--- domain/src/test/java/com/anytypeio/anytype/domain/misc/GetObjectTypeTest.kt
+++ domain/src/test/java/com/anytypeio/anytype/domain/misc/GetObjectTypeTest.kt
@@ -9,6 +9,7 @@ import com.anytypeio.anytype.domain.block.repo.BlockRepository
 import com.anytypeio.anytype.domain.util.dispatchers
 import com.anytypeio.anytype.test_utils.MockDataFactory
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
@@ -85,6 +86,139 @@ class GetObjectTypeTest {
                 limit = defaultParams.limit,
                 offset = defaultParams.offset,
                 fulltext = ""
+            )
+        }
+    }
+
+    @Test
+    fun `should return source object when Struct returns String`() {
+
+        val sourceObject = MockDataFactory.randomUuid()
+        val type1 = ObjectWrapper.Type(
+            mapOf(
+                Relations.ID to MockDataFactory.randomUuid(),
+                Relations.UNIQUE_KEY to MockDataFactory.randomUuid(),
+                Relations.SOURCE_OBJECT to sourceObject
+            )
+        )
+
+        runBlocking {
+            stubGetObjectTypes(types = listOf(type1))
+
+            val firstTimeResult = usecase.execute(params = defaultParams)
+            firstTimeResult.fold(
+                onFailure = { Assert.fail() },
+                onSuccess = { results ->
+                    assertEquals(
+                        expected = sourceObject,
+                        actual = results.first().sourceObject
+                    )
+                }
+            )
+        }
+    }
+
+    @Test
+    fun `type should has source object when Struct returns list of one element`() {
+
+        val sourceObject = MockDataFactory.randomUuid()
+        val type1 = ObjectWrapper.Type(
+            mapOf(
+                Relations.ID to MockDataFactory.randomUuid(),
+                Relations.UNIQUE_KEY to MockDataFactory.randomUuid(),
+                Relations.SOURCE_OBJECT to listOf(sourceObject)
+            )
+        )
+
+        runBlocking {
+            stubGetObjectTypes(types = listOf(type1))
+
+            val firstTimeResult = usecase.execute(params = defaultParams)
+            firstTimeResult.fold(
+                onFailure = { Assert.fail() },
+                onSuccess = { results ->
+                    assertEquals(
+                        expected = sourceObject,
+                        actual = results.first().sourceObject
+                    )
+                }
+            )
+        }
+    }
+
+    @Test
+    fun `type should has source object when Struct returns list of several elements`() {
+
+        val sourceObject = MockDataFactory.randomUuid()
+        val sourceObject2 = MockDataFactory.randomUuid()
+        val sourceObject3 = MockDataFactory.randomUuid()
+        val type1 = ObjectWrapper.Type(
+            mapOf(
+                Relations.ID to MockDataFactory.randomUuid(),
+                Relations.UNIQUE_KEY to MockDataFactory.randomUuid(),
+                Relations.SOURCE_OBJECT to listOf(sourceObject, sourceObject2, sourceObject3)
+            )
+        )
+
+        runBlocking {
+            stubGetObjectTypes(types = listOf(type1))
+
+            val firstTimeResult = usecase.execute(params = defaultParams)
+            firstTimeResult.fold(
+                onFailure = { Assert.fail() },
+                onSuccess = { results ->
+                    assertEquals(
+                        expected = sourceObject,
+                        actual = results.first().sourceObject
+                    )
+                }
+            )
+        }
+    }
+
+    @Test
+    fun `type should has no source object when Struct returns empty list of`() {
+
+        val type1 = ObjectWrapper.Type(
+            mapOf(
+                Relations.ID to MockDataFactory.randomUuid(),
+                Relations.UNIQUE_KEY to MockDataFactory.randomUuid(),
+                Relations.SOURCE_OBJECT to emptyList<String>()
+            )
+        )
+
+        runBlocking {
+            stubGetObjectTypes(types = listOf(type1))
+
+            val firstTimeResult = usecase.execute(params = defaultParams)
+            firstTimeResult.fold(
+                onFailure = { Assert.fail() },
+                onSuccess = { results ->
+                    assertNull(results.first().sourceObject)
+                }
+            )
+        }
+    }
+
+    @Test
+    fun `type should has no source object when Struct returns null`() {
+
+        val type1 = ObjectWrapper.Type(
+            mapOf(
+                Relations.ID to MockDataFactory.randomUuid(),
+                Relations.UNIQUE_KEY to MockDataFactory.randomUuid()
+            )
+        )
+
+        runBlocking {
+            stubGetObjectTypes(types = listOf(type1))
+
+            val firstTimeResult = usecase.execute(params = defaultParams)
+            firstTimeResult.fold(
+                onFailure = { Assert.fail() },
+                onSuccess = { results ->
+                    assertNull(results.first().sourceObject)
+                }
             )
         }
     }

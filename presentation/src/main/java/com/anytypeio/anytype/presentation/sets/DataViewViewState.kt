cd80222be53f9693369037d9f5f7670c04f4e8f8
konstantiniiv <ki@anytype.io>
2023-11-22 22:02:20 +0100

    DROID-1975 data view state

--- presentation/src/main/java/com/anytypeio/anytype/presentation/sets/DataViewViewState.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/sets/DataViewViewState.kt
@@ -7,15 +7,15 @@ sealed class DataViewViewState {
 
     sealed class Collection : DataViewViewState() {
         object NoView : Collection()
-        data class NoItems(val title: String, val hasTemplates: Boolean = false) : Collection()
-        data class Default(val viewer: Viewer?, val hasTemplates: Boolean = false) : Collection()
+        data class NoItems(val title: String, val isCreateObjectAllowed: Boolean) : Collection()
+        data class Default(val viewer: Viewer?, val isCreateObjectAllowed: Boolean) : Collection()
     }
 
     sealed class Set : DataViewViewState() {
         object NoQuery : Set()
         object NoView : Set()
-        data class NoItems(val title: String, val hasTemplates: Boolean) : Set()
-        data class Default(val viewer: Viewer?, val hasTemplates: Boolean) : Set()
+        data class NoItems(val title: String, val isCreateObjectAllowed: Boolean) : Set()
+        data class Default(val viewer: Viewer?, val isCreateObjectAllowed: Boolean) : Set()
     }
 
     object Init: DataViewViewState()

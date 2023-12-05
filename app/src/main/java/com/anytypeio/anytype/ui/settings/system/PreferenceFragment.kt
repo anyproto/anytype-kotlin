f4d06964e9ec438e40880e6e66ea0749e033c9e2
konstantiniiv <ki@anytype.io>
2023-12-05 13:51:13 +0100

    DROID-1602 fix

--- app/src/main/java/com/anytypeio/anytype/ui/settings/system/PreferenceFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/settings/system/PreferenceFragment.kt
@@ -1,12 +1,49 @@
 package com.anytypeio.anytype.ui.settings.system
 
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.DropDownPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.anytypeio.anytype.R
+import com.anytypeio.anytype.persistence.repo.DefaultUserSettingsCache
 
 class PreferenceFragment : PreferenceFragmentCompat() {
 
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        getSharedPrefs()
+        val context = preferenceManager.context
+        val screen = preferenceManager.createPreferenceScreen(context)
+        val dropDownPreference = DropDownPreference(context)
+        dropDownPreference.apply {
+            key = "key"
+            title = getString(R.string.settings_network_mode)
+            setEntries(R.array.settings_networks)
+            setEntryValues(R.array.settings_networks)
+        }
+        screen.addPreference(dropDownPreference)
+        val singleLinePreference = Preference(context).apply {
+            title = "Network file"
+            isSingleLineTitle = true
+        }
+        screen.addPreference(singleLinePreference)
+        preferenceScreen = screen
+
+        //setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    private fun getSharedPrefs() {
+        val activityContext = activity
+        if (activityContext != null) {
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activityContext)
+            val filePath = sharedPreferences.getString(NETWORK_CONFIG_FILE_PATH, "")
+            Log.d("Test1983", "Network file path : $filePath")
+        }
+    }
+
+    companion object {
+        const val NETWORK_CONFIG_FILE_PATH = "network_config_file_path"
     }
 }

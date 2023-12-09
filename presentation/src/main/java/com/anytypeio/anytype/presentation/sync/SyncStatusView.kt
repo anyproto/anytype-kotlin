0be94cbc4206ffdb3e4f225c2c7f45880da2bd2d
konstantiniiv <ki@anytype.io>
2023-12-09 20:49:19 +0100

    DROID-2020 fix

--- presentation/src/main/java/com/anytypeio/anytype/presentation/sync/SyncStatusView.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/sync/SyncStatusView.kt
@@ -17,19 +17,23 @@ sealed class SyncStatusView {
     object Failed : SyncStatusView()
     object IncompatibleVersion : SyncStatusView()
 }
+
 fun SyncStatus.toView(networkId: Id?): SyncStatusView {
-    val syncedStatus = when (networkId) {
-        BuildConfig.NODE_NETWORK_ID -> SyncStatusView.Synced.AnyNetwork
-        BuildConfig.STAGING_NETWORK_ID -> SyncStatusView.Synced.StagingNetwork
-        "" -> SyncStatusView.Synced.LocalOnly
-        else -> SyncStatusView.Synced.SelfHostedNetwork
-    }
     return when (this) {
         SyncStatus.UNKNOWN -> SyncStatusView.Unknown
         SyncStatus.OFFLINE -> SyncStatusView.Offline
         SyncStatus.SYNCING -> SyncStatusView.Syncing
-        SyncStatus.SYNCED -> syncedStatus
+        SyncStatus.SYNCED -> networkId.syncedStatus()
         SyncStatus.FAILED -> SyncStatusView.Failed
         SyncStatus.INCOMPATIBLE_VERSION -> SyncStatusView.IncompatibleVersion
+    }
+}
+
+private fun Id?.syncedStatus(): SyncStatusView.Synced {
+    return when (this) {
+        BuildConfig.NODE_NETWORK_ID -> SyncStatusView.Synced.AnyNetwork
+        BuildConfig.STAGING_NETWORK_ID -> SyncStatusView.Synced.StagingNetwork
+        "" -> SyncStatusView.Synced.LocalOnly
+        else -> SyncStatusView.Synced.SelfHostedNetwork
     }
 }

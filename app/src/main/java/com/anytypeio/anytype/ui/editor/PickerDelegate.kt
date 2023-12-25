cd4209ff9ada14173eed3d89c27cdf745287f15c
konstantiniiv <ki@anytype.io>
2023-12-25 13:18:45 +0100

    DROID-2107 fix start file picker

--- app/src/main/java/com/anytypeio/anytype/ui/editor/PickerDelegate.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/editor/PickerDelegate.kt
@@ -100,7 +100,12 @@ interface PickerDelegate : PickiTCallbacks {
         }
 
         override fun openFilePicker(mimeType: Mimetype, requestCode: Int?) {
-            permissionHelper.openFilePicker(mimeType, requestCode)
+            try {
+                permissionHelper.openFilePicker(mimeType, requestCode)
+            } catch (e: Throwable) {
+                Timber.e(e, "Error while opening file picker")
+                fragment.toast("Error while opening file picker")
+            }
         }
 
         override fun initPicker(ctx: Id) {

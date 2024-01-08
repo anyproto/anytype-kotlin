3fed8ca3c322c29c59ec4eb0873fdde43e94179b
konstantiniiv <ki@anytype.io>
2024-01-08 12:37:19 +0100

    DROID-1018 code style

--- domain/src/main/java/com/anytypeio/anytype/domain/misc/DateProvider.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/misc/DateProvider.kt
@@ -1,11 +1,9 @@
 package com.anytypeio.anytype.domain.misc
 
 interface DateProvider {
-
     fun getRelativeTimeSpanString(date: Long): CharSequence
     fun calculateDateType(date: Long): DateType
     fun getCurrentTimestampInSeconds(): Long
-
     fun getTimestampForTodayAtStartOfDay(): Long
     fun getTimestampForTomorrowAtStartOfDay(): Long
     fun getTimestampForYesterdayAtStartOfDay(): Long

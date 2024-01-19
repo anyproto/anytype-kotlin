74189d74772a73af69bc77fcce6e8c1ae4a63f27
konstantiniiv <ki@anytype.io>
2024-01-19 16:15:24 +0100

    DROID-2122 relation values widget

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/RelationValueListWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/RelationValueListWidget.kt
@@ -14,6 +14,7 @@ import com.anytypeio.anytype.core_ui.extensions.light
 import com.anytypeio.anytype.core_utils.ext.gone
 import com.anytypeio.anytype.core_utils.ext.setDrawableColor
 import com.anytypeio.anytype.core_utils.ext.visible
+import com.anytypeio.anytype.presentation.editor.editor.listener.ListenerType
 import com.anytypeio.anytype.presentation.objects.ObjectIcon
 import com.anytypeio.anytype.presentation.relations.ObjectRelationView
 import com.anytypeio.anytype.presentation.sets.model.ObjectView
@@ -52,18 +53,31 @@ class RelationValueListWidget @JvmOverloads constructor(
 
     fun setRelation(
         relation: ObjectRelationView,
-        isLast: Boolean = false
+        isLast: Boolean = false,
+        clickListener: ((ListenerType.Relation) -> Unit)? = null
     ) {
         when (relation) {
+            is ObjectRelationView.Default -> setDefaultRelation(relation)
             is ObjectRelationView.Object -> setObjectRelation(relation)
             is ObjectRelationView.Tags -> setTagRelation(relation)
             is ObjectRelationView.File -> setRelationFile(relation)
             is ObjectRelationView.Status -> setStatusRelation(relation)
             is ObjectRelationView.Checkbox -> setCheckboxRelation(relation)
-            else -> TODO()
+            is ObjectRelationView.ObjectType -> setObjectTypeRelation(relation)
+            is ObjectRelationView.Links -> setLinksRelation(relation)
+            is ObjectRelationView.Source -> setSourceRelation(relation, clickListener)
         }
         if (!isLast) dot.visible()
     }
+
+    //region DEFAULT
+    private fun setDefaultRelation(relation: ObjectRelationView.Default) {
+        setupSingleTextItem(
+            name = relation.value ?: relation.name,
+            textColor = if (relation.value != null) textColorPrimary else textColorSecondary
+        )
+    }
+    //endregion
 
     //region OBJECTS
     private fun setObjectRelation(relation: ObjectRelationView.Object) {
@@ -147,14 +161,22 @@ class RelationValueListWidget @JvmOverloads constructor(
         }
     }
 
-    private fun setupSingleTextItem(name: String, marginStart: Int = DEFAULT_MARGIN_START) {
+    private fun setupSingleTextItem(
+        name: String,
+        marginStart: Int = DEFAULT_MARGIN_START,
+        textColor: Int = textColorSecondary,
+        click: (() -> Unit)? = null
+    ) {
         text1.apply {
             updateLayoutParams<LayoutParams> {
                 setMarginStart(marginStart)
             }
-            setTextColor(textColorSecondary)
+            setTextColor(textColor)
             text = name
             visible()
+            if (click != null) {
+                setOnClickListener { click.invoke() }
+            }
         }
     }
 
@@ -277,12 +299,109 @@ class RelationValueListWidget @JvmOverloads constructor(
 
     //endregion
 
+    //region OBJECT TYPE
+    private fun setObjectTypeRelation(objType: ObjectRelationView.ObjectType) {
+        when (objType) {
+            is ObjectRelationView.ObjectType.Base,
+            is ObjectRelationView.ObjectType.Collection,
+            is ObjectRelationView.ObjectType.Set -> {
+                text1.apply {
+                    visible()
+                    setTextColor(textColorPrimary)
+                    text = objType.name.ifEmpty {
+                        context.resources.getString(R.string.untitled)
+                    }
+                    updateLayoutParams<LayoutParams> {
+                        marginStart = 0
+                    }
+                }
+            }
+            is ObjectRelationView.ObjectType.Deleted -> {
+                text1.apply {
+                    visible()
+                    setTextColor(context.getColor(R.color.palette_dark_red))
+                    text = context.resources.getString(R.string.deleted_type)
+                    updateLayoutParams<LayoutParams> {
+                        marginStart = 0
+                    }
+                }
+            }
+        }
+    }
+    //endregion
+
+    //region SOURCE
+    private fun setSourceRelation(
+        objectSource: ObjectRelationView.Source,
+        clickListener: ((ListenerType.Relation) -> Unit)? = null
+    ) {
+        when {
+            objectSource.sources.isEmpty() -> {
+                setupSingleTextItem(
+                    name = resources.getString(R.string.query)
+                ) {
+                    clickListener?.invoke(ListenerType.Relation.SetQuery())
+                }
+            }
+            else -> {
+                val sourceName = objectSource.sources.first().name
+                if (objectSource.isSourceByRelation) {
+                    setupSingleTextItem(
+                        name = resources.getString(R.string.set_by_relation, sourceName),
+                        textColor = textColorPrimary
+                    ) {
+                        clickListener?.invoke(ListenerType.Relation.ChangeQueryByRelation)
+                    }
+                } else {
+                    setupSingleTextItem(
+                        name = resources.getString(R.string.set_by_type, sourceName),
+                        textColor = textColorPrimary
+                    ) {
+                        clickListener?.invoke(ListenerType.Relation.SetQuery(objectSource.sources))
+                    }
+                }
+            }
+        }
+
+    }
+    //endregion
+
     //region CHECKBOX
     private fun setCheckboxRelation(relation: ObjectRelationView.Checkbox) {
         icon1.visible()
         val icon = if (relation.isChecked) ObjectIcon.Checkbox(true) else ObjectIcon.Checkbox(false)
         icon1.setIcon(icon = icon)
     }
+    //endregion
+
+    //region LINKS
+    private fun setLinksRelation(relation: ObjectRelationView.Links) {
+        when (relation) {
+            is ObjectRelationView.Links.From -> {
+                val count = context.resources.getQuantityString(
+                    R.plurals.links_from_count,
+                    relation.count,
+                    relation.count
+                )
+                setupSingleTextItem(
+                    name = count,
+                    textColor = textColorPrimary
+                )
+            }
+            is ObjectRelationView.Links.To -> {
+                val count = context.resources.getQuantityString(
+                    R.plurals.links_to_count,
+                    relation.count,
+                    relation.count
+                )
+                setupSingleTextItem(
+                    name = count,
+                    textColor = textColorPrimary
+                )
+            }
+        }
+    }
+    //endregion
 
     companion object {
         private const val MAX_ITEMS = 2

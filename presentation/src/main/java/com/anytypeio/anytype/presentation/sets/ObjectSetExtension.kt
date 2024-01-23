d6c6bb90d503ff5f1990ef502db615bece2217d5
konstantiniiv <ki@anytype.io>
2024-01-23 15:14:45 +0100

    DROID-1865 move to ext

--- presentation/src/main/java/com/anytypeio/anytype/presentation/sets/ObjectSetExtension.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/sets/ObjectSetExtension.kt
@@ -4,6 +4,7 @@ import com.anytypeio.anytype.core_models.Block
 import com.anytypeio.anytype.core_models.CoverType
 import com.anytypeio.anytype.core_models.DVFilter
 import com.anytypeio.anytype.core_models.DVFilterCondition
+import com.anytypeio.anytype.core_models.DVFilterQuickOption
 import com.anytypeio.anytype.core_models.DVRecord
 import com.anytypeio.anytype.core_models.DVSort
 import com.anytypeio.anytype.core_models.DVViewer
@@ -15,9 +16,12 @@ import com.anytypeio.anytype.core_models.Event.Command.DataView.UpdateView.DVVie
 import com.anytypeio.anytype.core_models.Id
 import com.anytypeio.anytype.core_models.ObjectType
 import com.anytypeio.anytype.core_models.ObjectWrapper
+import com.anytypeio.anytype.core_models.Relation
 import com.anytypeio.anytype.core_models.RelationFormat
 import com.anytypeio.anytype.core_models.RelationLink
 import com.anytypeio.anytype.core_models.Relations
+import com.anytypeio.anytype.core_models.Struct
+import com.anytypeio.anytype.core_models.ext.DateParser
 import com.anytypeio.anytype.core_models.ext.mapToObjectWrapperType
 import com.anytypeio.anytype.core_models.ext.title
 import com.anytypeio.anytype.core_models.primitives.TypeId
@@ -26,6 +30,7 @@ import com.anytypeio.anytype.core_utils.ext.addAfterIndexInLine
 import com.anytypeio.anytype.core_utils.ext.mapInPlace
 import com.anytypeio.anytype.core_utils.ext.moveAfterIndexInLine
 import com.anytypeio.anytype.core_utils.ext.moveOnTop
+import com.anytypeio.anytype.domain.misc.DateProvider
 import com.anytypeio.anytype.domain.misc.UrlBuilder
 import com.anytypeio.anytype.domain.objects.StoreOfObjectTypes
 import com.anytypeio.anytype.domain.objects.StoreOfRelations
@@ -601,4 +606,102 @@ fun ObjectState.DataView.isChangingDefaultTypeAvailable(): Boolean {
         }
     }
 }
+
+suspend fun DVViewer.prefillNewObjectDetails(
+    storeOfRelations: StoreOfRelations,
+    dateProvider: DateProvider,
+    dataViewRelationLinks: List<RelationLink>
+): Struct =
+    buildMap {
+        filters.forEach { filter ->
+            val relationObject = storeOfRelations.getByKey(filter.relation) ?: return@forEach
+            if (!relationObject.isReadonlyValue && permittedConditions.contains(
+                    filter.condition
+                )
+            ) {
+                //Relation format should be taken from DataView relation links
+                val filterRelationFormat =
+                    dataViewRelationLinks.firstOrNull { it.key == filter.relation }?.format
+                when (filterRelationFormat) {
+                    Relation.Format.DATE -> {
+                        val value = DateParser.parse(filter.value)
+                        val updatedValue = filter.quickOption.getTimestampForQuickOption(
+                            value = value,
+                            dateProvider = dateProvider
+                        )
+                        if (updatedValue != null) {
+                            put(filter.relation, updatedValue.toDouble())
+                        }
+                    }
+                    else -> {
+                        filter.value?.let { put(filter.relation, it) }
+                    }
+                }
+            }
+        }
+    }
+
+suspend fun DVViewer.resolveSetByRelationPrefilledObjectData(
+    storeOfRelations: StoreOfRelations,
+    dateProvider: DateProvider,
+    objSetByRelation: ObjectWrapper.Relation,
+    dataViewRelationLinks: List<RelationLink>
+): Struct {
+    val prefillWithSetOf = buildMap {
+        val relationFormat = objSetByRelation.relationFormat
+        val defaultValue = resolveDefaultValueByFormat(relationFormat)
+        put(objSetByRelation.key, defaultValue)
+    }
+    val prefillNewObjectDetails = prefillNewObjectDetails(storeOfRelations, dateProvider, dataViewRelationLinks)
+    return prefillWithSetOf + prefillNewObjectDetails
+}
+
+private fun resolveDefaultValueByFormat(format: RelationFormat): Any? = when (format) {
+    Relation.Format.LONG_TEXT,
+    Relation.Format.SHORT_TEXT,
+    Relation.Format.URL,
+    Relation.Format.EMAIL,
+    Relation.Format.PHONE,
+    Relation.Format.EMOJI -> EMPTY_STRING_VALUE
+    Relation.Format.CHECKBOX -> false
+    Relation.Format.NUMBER -> null
+    else -> null
+}
+
+fun DVFilterQuickOption.getTimestampForQuickOption(value: Long?, dateProvider: DateProvider): Long? {
+    val option = this
+    val time = dateProvider.getCurrentTimestampInSeconds()
+    return when (option) {
+        DVFilterQuickOption.DAYS_AGO -> {
+            if (value == null) return null
+            time - SECONDS_IN_DAY * value
+        }
+        DVFilterQuickOption.LAST_MONTH -> time - SECONDS_IN_DAY * DAYS_IN_MONTH
+        DVFilterQuickOption.LAST_WEEK -> time - SECONDS_IN_DAY * DAYS_IN_WEEK
+        DVFilterQuickOption.YESTERDAY -> time - SECONDS_IN_DAY
+        DVFilterQuickOption.CURRENT_WEEK,
+        DVFilterQuickOption.CURRENT_MONTH,
+        DVFilterQuickOption.TODAY -> time
+        DVFilterQuickOption.TOMORROW -> time + SECONDS_IN_DAY
+        DVFilterQuickOption.NEXT_WEEK -> time + SECONDS_IN_DAY * DAYS_IN_WEEK
+        DVFilterQuickOption.NEXT_MONTH -> time + SECONDS_IN_DAY * DAYS_IN_MONTH
+        DVFilterQuickOption.DAYS_AHEAD -> {
+            if (value == null) return null
+            time + SECONDS_IN_DAY * value
+        }
+        DVFilterQuickOption.EXACT_DATE -> value
+    }
+}
+
+const val SECONDS_IN_DAY = 86400
+const val DAYS_IN_MONTH = 30
+const val DAYS_IN_WEEK = 7
+const val EMPTY_STRING_VALUE = ""
+val permittedConditions = listOf(
+    DVFilterCondition.ALL_IN,
+    DVFilterCondition.IN,
+    DVFilterCondition.EQUAL,
+    DVFilterCondition.GREATER_OR_EQUAL,
+    DVFilterCondition.LESS_OR_EQUAL
+)
 

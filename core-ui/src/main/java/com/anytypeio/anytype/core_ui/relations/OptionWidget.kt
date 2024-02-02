9219d007d3a20c5dadb71247e9ac492f88d268ca
konstantiniiv <ki@anytype.io>
2024-02-02 14:10:10 +0100

    DROID-2121 option widget

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/relations/OptionWidget.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/relations/OptionWidget.kt
@@ -1,31 +1,25 @@
 package com.anytypeio.anytype.core_ui.relations
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Text
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,115 +29,58 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.anytypeio.anytype.core_models.ThemeColor
 import com.anytypeio.anytype.core_ui.R
+import com.anytypeio.anytype.core_ui.extensions.dark
 import com.anytypeio.anytype.core_ui.foundation.Divider
 import com.anytypeio.anytype.core_ui.foundation.Dragger
-import com.anytypeio.anytype.core_ui.foundation.noRippleClickable
+import com.anytypeio.anytype.core_ui.views.ButtonPrimary
+import com.anytypeio.anytype.core_ui.views.ButtonSize
+import com.anytypeio.anytype.core_ui.views.Caption1Regular
 import com.anytypeio.anytype.core_ui.views.Title1
-import com.anytypeio.anytype.core_ui.widgets.DragStates
 import com.anytypeio.anytype.presentation.relations.value.tagstatus.OptionWidgetAction
 import com.anytypeio.anytype.presentation.relations.value.tagstatus.OptionWidgetViewState
-import kotlin.math.roundToInt
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun OptionWidget(
     state: OptionWidgetViewState,
     action: (OptionWidgetAction) -> Unit,
-    scope: CoroutineScope
 ) {
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
-    val isVisible = true
 
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomStart,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        contentAlignment = Alignment.TopCenter,
     ) {
 
         val currentState by rememberUpdatedState(state)
-        val swipeableState = rememberSwipeableState(DragStates.VISIBLE)
         val keyboardController = LocalSoftwareKeyboardController.current
 
-        AnimatedVisibility(
-            visible = isVisible,
-            enter = fadeIn(),
-            exit = fadeOut(tween(100))
-        ) {
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.4f))
-                    .noRippleClickable {
-                        //action(OptionWidgetAction.Dismiss)
-                    }
-            )
-        }
-
-        if (swipeableState.isAnimationRunning && swipeableState.targetValue == DragStates.DISMISSED) {
-            DisposableEffect(Unit) {
-                onDispose {
-                    keyboardController?.hide()
-                    focusManager.clearFocus()
-                    //action(OptionWidgetAction.Dismiss)
-                }
-            }
-        }
-
-        if (!isVisible) {
-            DisposableEffect(Unit) {
-                onDispose {
-                    scope.launch { swipeableState.snapTo(DragStates.VISIBLE) }
-                }
-            }
-        }
-
-        val sizePx =
-            with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp.toPx() }
-
-        AnimatedVisibility(
-            visible = isVisible,
-            enter = slideInVertically { it },
-            exit = slideOutVertically { it },
-            modifier = Modifier
-                .swipeable(state = swipeableState,
-                    orientation = Orientation.Vertical,
-                    anchors = mapOf(
-                        0f to DragStates.VISIBLE, sizePx to DragStates.DISMISSED
-                    ),
-                    thresholds = { _, _ -> FractionalThreshold(0.3f) })
-                .offset { IntOffset(0, swipeableState.offset.value.roundToInt()) }
-        ) {
-            if (isVisible) {
-                OptionWidgetContent(
-                    state = currentState as OptionWidgetViewState,
-                    action = action,
-                    focusRequester = focusRequester,
-                    keyboardController = keyboardController
-                )
-            }
-        }
+        OptionWidgetContent(
+            state = currentState,
+            action = action,
+            focusRequester = focusRequester,
+            keyboardController = keyboardController
+        )
     }
 }
 
@@ -155,13 +92,14 @@ fun OptionWidgetContent(
     focusRequester: FocusRequester,
     keyboardController: SoftwareKeyboardController?
 ) {
+    val (title, buttonText) = getTexts(state)
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(bottom = 20.dp)
+            .padding(bottom = 20.dp, start = 20.dp, end = 20.dp)
     ) {
-        Header(state = state, action = action)
+        Header(text = title, action = action)
         TextInput(
             state = state,
             action = action,
@@ -169,6 +107,54 @@ fun OptionWidgetContent(
             keyboardController = keyboardController
         )
         Divider(paddingEnd = 0.dp, paddingStart = 0.dp)
+        Spacer(modifier = Modifier.height(26.dp))
+        Text(
+            text = stringResource(id = R.string.color),
+            style = Caption1Regular,
+            color = colorResource(id = R.color.text_secondary)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        CirclesContainer(selectedColor = state.color)
+        Spacer(modifier = Modifier.height(115.dp))
+        ButtonPrimary(
+            text = buttonText,
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { },
+            size = ButtonSize.Large
+        )
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun CirclesContainer(selectedColor: ThemeColor) {
+    FlowRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        maxItemsInEachRow = 5
+    ) {
+        val itemModifier = Modifier
+            .weight(1f)
+            .fillMaxWidth()
+            .clip(CircleShape)
+            .aspectRatio(1f)
+            .align(Alignment.CenterVertically)
+        ThemeColor.values().drop(1).forEach { color ->
+            Box(itemModifier.background(color = dark(color = color))
+            ) {
+                if (selectedColor == color) {
+                    Log.d("OptionWidget", "selectedColor == color $selectedColor == $color")
+                    Image(
+                        modifier = Modifier.align(Alignment.Center),
+                        painter = painterResource(id = R.drawable.ic_tick_24),
+                        contentDescription = "option selected"
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -192,10 +178,10 @@ private fun TextInput(
     BasicTextField(
         value = innerValue,
         onValueChange = { innerValue = it },
-        textStyle = Title1.copy(color = colorResource(id = R.color.text_primary)),
+        textStyle = Title1.copy(color = dark(color = state.color)),
         singleLine = true,
         enabled = true,
-        cursorBrush = SolidColor(colorResource(id = R.color.text_primary)),
+        cursorBrush = SolidColor(dark(color = state.color)),
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
@@ -212,7 +198,7 @@ private fun TextInput(
         decorationBox = { innerTextField ->
             if (innerValue.isEmpty()) {
                 Text(
-                    text = stringResource(id = R.string.new_view),
+                    text = "",
                     style = Title1,
                     color = colorResource(id = R.color.text_tertiary),
                     modifier = Modifier
@@ -226,17 +212,13 @@ private fun TextInput(
 }
 
 @Composable
-private fun Header(state: OptionWidgetViewState, action: (OptionWidgetAction) -> Unit) {
+private fun Header(text: String, action: (OptionWidgetAction) -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 6.dp)
     ) {
         Dragger(modifier = Modifier.align(Alignment.Center))
-    }
-    val text = when (state) {
-        is OptionWidgetViewState.Create -> stringResource(id = R.string.option_widget_create)
-        is OptionWidgetViewState.Edit -> stringResource(id = R.string.option_widget_edit)
     }
 
     // Main content box
@@ -258,16 +240,27 @@ private fun Header(state: OptionWidgetViewState, action: (OptionWidgetAction) ->
     }
 }
 
+@Composable
+private fun getTexts(state: OptionWidgetViewState): Pair<String, String> {
+    return when (state) {
+        is OptionWidgetViewState.Create -> {
+            stringResource(id = R.string.option_widget_create) to stringResource(id = R.string.create)
+        }
+
+        is OptionWidgetViewState.Edit -> {
+            stringResource(id = R.string.option_widget_edit) to stringResource(id = R.string.apply)
+        }
+    }
+}
+
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, device = Devices.PIXEL_4_XL)
 @Composable
 fun PreviewOptionWidget() {
     OptionWidget(
         state = OptionWidgetViewState.Create(
-            optionId = "1",
             text = "Urgent",
             color = ThemeColor.BLUE,
         ),
         action = {},
-        scope = CoroutineScope(Dispatchers.Main)
     )
 }

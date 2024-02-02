cdd408516155e123a2ab0719619a5b7b46953a2c
konstantiniiv <ki@anytype.io>
2024-02-02 16:17:57 +0100

    DROID-2121 fic

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/relations/ItemMenu.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/relations/ItemMenu.kt
@@ -33,7 +33,7 @@ fun ItemMenu(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 11.dp),
                 onClick = {
                     isMenuExpanded.value = false
-                    action(TagStatusAction.Edit(item.optionId))
+                    action(TagStatusAction.Edit(item))
                 },
             ) {
                 Text(

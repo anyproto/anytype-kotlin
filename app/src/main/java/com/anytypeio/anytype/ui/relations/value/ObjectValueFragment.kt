e05a37152f54e9b67f58f979eb4fc98798b8725a
konstantiniiv <ki@anytype.io>
2024-02-09 15:51:42 +0100

    DROID-1960 object value screen

---
+++ app/src/main/java/com/anytypeio/anytype/ui/relations/value/ObjectValueFragment.kt
@@ -0,0 +1,63 @@
+package com.anytypeio.anytype.ui.relations.value
+
+import androidx.fragment.app.viewModels
+import com.anytypeio.anytype.core_utils.ext.argBoolean
+import com.anytypeio.anytype.core_utils.ext.argString
+import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
+import com.anytypeio.anytype.di.common.componentManager
+import com.anytypeio.anytype.presentation.relations.value.`object`.ObjectValueViewModel
+import com.anytypeio.anytype.presentation.relations.value.`object`.ObjectValueViewModelFactory
+import com.anytypeio.anytype.presentation.relations.value.tagstatus.RelationContext
+import javax.inject.Inject
+
+class ObjectValueFragment : BaseBottomSheetComposeFragment() {
+
+    @Inject
+    lateinit var factory: ObjectValueViewModelFactory
+    private val vm by viewModels<ObjectValueViewModel> { factory }
+
+    private val ctx get() = argString(CTX_KEY)
+    private val relationKey get() = argString(RELATION_KEY)
+    private val objectId get() = argString(OBJECT_ID_KEY)
+    private val isLocked get() = argBoolean(IS_LOCKED_KEY)
+    private val relationContext get() = requireArguments().getSerializable(RELATION_CONTEXT_KEY) as RelationContext
+
+    override fun injectDependencies() {
+        val params = ObjectValueViewModel.ViewModelParams(
+            ctx = ctx,
+            objectId = objectId,
+            relationKey = relationKey,
+            isLocked = isLocked,
+            relationContext = relationContext
+        )
+        inject(params)
+    }
+
+    private fun inject(params: ObjectValueViewModel.ViewModelParams) = when (relationContext) {
+        RelationContext.OBJECT -> componentManager()
+            .objectValueObjectComponent.get(params)
+            .inject(this)
+
+        RelationContext.OBJECT_SET -> componentManager()
+            .objectValueSetComponent.get(params)
+            .inject(this)
+
+        RelationContext.DATA_VIEW -> componentManager()
+            .objectValueDataViewComponent.get(params)
+            .inject(this)
+    }
+
+    override fun releaseDependencies() = when (relationContext) {
+        RelationContext.OBJECT -> componentManager().objectValueObjectComponent.release()
+        RelationContext.OBJECT_SET -> componentManager().objectValueSetComponent.release()
+        RelationContext.DATA_VIEW -> componentManager().objectValueDataViewComponent.release()
+    }
+
+    companion object {
+        const val CTX_KEY = "arg.relation.object.ctx"
+        const val RELATION_KEY = "arg.relation.object.relation.key"
+        const val OBJECT_ID_KEY = "arg.relation.object.object"
+        const val IS_LOCKED_KEY = "arg.relation.object.is-locked"
+        const val RELATION_CONTEXT_KEY = "arg.relation.object.relation-context"
+    }
+}
---
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/relations/value/object/ObjectValueViewModelFactory.kt
@@ -0,0 +1,35 @@
+package com.anytypeio.anytype.presentation.relations.value.`object`
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.anytypeio.anytype.analytics.base.Analytics
+import com.anytypeio.anytype.core_models.Payload
+import com.anytypeio.anytype.domain.`object`.UpdateDetail
+import com.anytypeio.anytype.domain.workspace.SpaceManager
+import com.anytypeio.anytype.presentation.relations.providers.ObjectRelationProvider
+import com.anytypeio.anytype.presentation.relations.providers.ObjectValueProvider
+import com.anytypeio.anytype.presentation.util.Dispatcher
+import javax.inject.Inject
+
+class ObjectValueViewModelFactory @Inject constructor(
+    private val params: ObjectValueViewModel.ViewModelParams,
+    private val relations: ObjectRelationProvider,
+    private val values: ObjectValueProvider,
+    private val dispatcher: Dispatcher<Payload>,
+    private val setObjectDetails: UpdateDetail,
+    private val analytics: Analytics,
+    private val spaceManager: SpaceManager
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ) = ObjectValueViewModel(
+        viewModelParams = params,
+        relations = relations,
+        values = values,
+        dispatcher = dispatcher,
+        setObjectDetails = setObjectDetails,
+        analytics = analytics,
+        spaceManager = spaceManager
+    ) as T
+}

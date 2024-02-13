a51887569cf632a02b1c447203c7ce2d6f4cca91
uburoiubu <ubuphobos@gmail.com>
2024-02-13 18:18:15 +0100

    DROID-2149 fix

--- app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingNetworkSetupDialog.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingNetworkSetupDialog.kt
@@ -7,12 +7,18 @@ import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.anytypeio.anytype.R
 import com.anytypeio.anytype.core_models.NetworkModeConfig
 import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
 import com.anytypeio.anytype.ui.onboarding.screens.signin.NetworkSetupScreen
 import com.anytypeio.anytype.ui.settings.typography
 
 class OnboardingNetworkSetupDialog : BaseBottomSheetComposeFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.NetworkSettingDialogTheme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,

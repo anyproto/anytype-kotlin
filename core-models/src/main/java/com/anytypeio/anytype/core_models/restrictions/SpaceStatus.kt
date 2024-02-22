4cf789bfc9e65e8a41a9a49ad83c3b8be0519603
Evgenii Kozlov <ubuphobos@gmail.com>
2024-02-22 12:54:36 +0100

    DROID-2226 Multispaces | Tech | Support 'SpaceRemoving' status (#897)

--- core-models/src/main/java/com/anytypeio/anytype/core_models/restrictions/SpaceStatus.kt
+++ core-models/src/main/java/com/anytypeio/anytype/core_models/restrictions/SpaceStatus.kt
@@ -9,4 +9,9 @@ enum class SpaceStatus(val code: Number) {
     REMOTE_WAITING_DELETION(5),
     REMOTE_DELETED(6),
     SPACE_DELETED(7),
+    SPACE_ACTIVE(8),
+    SPACE_JOINING(9),
+    SPACE_REMOVING(10);
+
+    fun isDeletedOrRemoving() : Boolean = this == SPACE_DELETED || this == SPACE_REMOVING
 }

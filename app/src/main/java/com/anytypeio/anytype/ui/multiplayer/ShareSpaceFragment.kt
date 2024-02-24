e5a3a361df036bddd72e0062aa7d25b15c0e996a
uburoiubu <ubuphobos@gmail.com>
2024-02-24 16:26:49 +0100

    DROID-2263 fixes

--- app/src/main/java/com/anytypeio/anytype/ui/multiplayer/ShareSpaceFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/multiplayer/ShareSpaceFragment.kt
@@ -7,16 +7,24 @@ import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
 import com.anytypeio.anytype.core_models.primitives.SpaceId
 import com.anytypeio.anytype.core_ui.features.multiplayer.ShareSpaceScreen
 import com.anytypeio.anytype.core_utils.ext.arg
 import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
 import com.anytypeio.anytype.di.common.componentManager
+import com.anytypeio.anytype.presentation.multiplayer.ShareSpaceViewModel
 import com.anytypeio.anytype.ui.settings.typography
+import javax.inject.Inject
 
 class ShareSpaceFragment : BaseBottomSheetComposeFragment() {
 
     private val space get() = arg<String>(SPACE_ID_KEY)
+
+    @Inject
+    lateinit var factory: ShareSpaceViewModel.Factory
+
+    private val vm by viewModels<ShareSpaceViewModel> { factory }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- presentation/src/main/java/com/anytypeio/anytype/presentation/multiplayer/ShareSpaceViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/multiplayer/ShareSpaceViewModel.kt
@@ -3,11 +3,12 @@ package com.anytypeio.anytype.presentation.multiplayer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.anytypeio.anytype.core_models.primitives.SpaceId
+import com.anytypeio.anytype.presentation.common.BaseViewModel
 import javax.inject.Inject
 
 class ShareSpaceViewModel(
     private val params: Params
-) {
+) : BaseViewModel() {
 
     class Factory @Inject constructor(
         private val params: Params

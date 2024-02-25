f836915b0db416ad57d4fad4c946c581856581f7
uburoiubu <ubuphobos@gmail.com>
2024-02-25 18:55:56 +0100

    DROID-2265 fixes

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/features/multiplayer/Joining.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/features/multiplayer/Joining.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,12 +21,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.anytypeio.anytype.core_ui.ColorTextInputCursor
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.extensions.throttledClick
 import com.anytypeio.anytype.core_ui.foundation.noRippleClickable
@@ -72,6 +75,7 @@ fun JoinSpaceScreen(
         Box(
             modifier = Modifier
                 .height(128.dp)
+                .fillMaxWidth()
                 .padding(horizontal = 20.dp)
                 .border(width = 1.dp, color = colorResource(id = R.color.shape_primary))
                 .clip(RoundedCornerShape(10.dp))
@@ -80,12 +84,32 @@ fun JoinSpaceScreen(
                 text = stringResource(R.string.multiplayer_private_comment_for_a_space_owner),
                 style = Caption1Medium,
                 color = colorResource(id = R.color.text_secondary),
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 10.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 10.dp)
             )
             TextField(
                 value = commentInputValue,
                 onValueChange = { commentInputValue = it },
-                textStyle = PreviewTitle2Regular
+                textStyle = PreviewTitle2Regular.copy(
+                    color = colorResource(id = R.color.text_primary)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 31.dp),
+                placeholder = {
+                    Text(
+                        text = stringResource(R.string.multiplayer_tap_to_write_request_to_join_comment),
+                        color = colorResource(id = R.color.text_tertiary)
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    disabledBorderColor = Color.Transparent,
+                    errorBorderColor = Color.Transparent,
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+                    cursorColor = ColorTextInputCursor
+                )
             )
         }
         Spacer(modifier = Modifier.height(20.dp))
@@ -119,6 +143,7 @@ private fun Header(
     Box(
         modifier = Modifier
             .height(64.dp)
+            .fillMaxWidth()
             .padding(horizontal = 20.dp)
     ) {
         Text(

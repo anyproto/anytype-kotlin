4ed6544d69475f9d0b2e0ae450e9e2616655bb20
Evgenii Kozlov <ubuphobos@gmail.com>
2024-02-26 10:09:06 +0100

    DROID-2265 Multiplayer | Design |  Layout for join-a-space screen (#923)

---
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/features/multiplayer/Joining.kt
@@ -0,0 +1,164 @@
+package com.anytypeio.anytype.core_ui.features.multiplayer
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.anytypeio.anytype.core_ui.ColorTextInputCursor
+import com.anytypeio.anytype.core_ui.R
+import com.anytypeio.anytype.core_ui.extensions.throttledClick
+import com.anytypeio.anytype.core_ui.foundation.noRippleClickable
+import com.anytypeio.anytype.core_ui.views.BodyRegular
+import com.anytypeio.anytype.core_ui.views.ButtonPrimary
+import com.anytypeio.anytype.core_ui.views.ButtonSize
+import com.anytypeio.anytype.core_ui.views.Caption1Medium
+import com.anytypeio.anytype.core_ui.views.Caption1Regular
+import com.anytypeio.anytype.core_ui.views.HeadlineHeading
+import com.anytypeio.anytype.core_ui.views.PreviewTitle2Regular
+
+@Composable
+@Preview
+fun JoinSpaceScreenPreview() {
+    JoinSpaceScreen(
+        onRequestJoinSpaceClicked = {},
+        onCloseClicked = {},
+        spaceName = "Anytype Android App",
+        createdByName = "Konstantin"
+    )
+}
+
+@Composable
+fun JoinSpaceScreen(
+    onRequestJoinSpaceClicked: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    spaceName: String,
+    createdByName: String
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var commentInputValue by remember { mutableStateOf("") }
+        Header(onCloseClicked = onCloseClicked)
+        Text(
+            style = BodyRegular,
+            color = colorResource(id = R.color.text_primary),
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            text = stringResource(
+                id = R.string.multiplayer_space_request_to_join_msg, spaceName, createdByName
+            )
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Box(
+            modifier = Modifier
+                .height(128.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .border(width = 1.dp, color = colorResource(id = R.color.shape_primary))
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+            Text(
+                text = stringResource(R.string.multiplayer_private_comment_for_a_space_owner),
+                style = Caption1Medium,
+                color = colorResource(id = R.color.text_secondary),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 10.dp)
+            )
+            TextField(
+                value = commentInputValue,
+                onValueChange = { commentInputValue = it },
+                textStyle = PreviewTitle2Regular.copy(
+                    color = colorResource(id = R.color.text_primary)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 31.dp),
+                placeholder = {
+                    Text(
+                        text = stringResource(R.string.multiplayer_tap_to_write_request_to_join_comment),
+                        color = colorResource(id = R.color.text_tertiary)
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    disabledBorderColor = Color.Transparent,
+                    errorBorderColor = Color.Transparent,
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+                    cursorColor = ColorTextInputCursor
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(
+            modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp, bottom = 16.dp)
+        ) {
+            ButtonPrimary(
+                onClick = throttledClick(
+                    onClick = { onRequestJoinSpaceClicked(commentInputValue) }
+                ),
+                size = ButtonSize.Large,
+                text = stringResource(R.string.multiplayer_space_request_to_join),
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = stringResource(R.string.mulitplayer_request_to_join_explanation),
+            style = Caption1Regular,
+            color = colorResource(id = R.color.text_secondary),
+            modifier = Modifier.padding(horizontal = 28.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+private fun Header(
+    onCloseClicked: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .height(64.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.multiplayer_join_a_space),
+            style = HeadlineHeading,
+            color = colorResource(id = R.color.text_primary),
+            modifier = Modifier.padding(top = 30.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_navigation_close_cross),
+            contentDescription = "Close button",
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 16.dp)
+                .noRippleClickable { onCloseClicked() }
+        )
+    }
+}

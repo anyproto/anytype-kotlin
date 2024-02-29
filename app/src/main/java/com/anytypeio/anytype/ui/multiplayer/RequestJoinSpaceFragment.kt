dde265e25959fb6350e4f3fa09f5717348af7599
uburoiubu <ubuphobos@gmail.com>
2024-03-01 00:39:14 +0100

    DROID-2265 mvvm components

---
+++ app/src/main/java/com/anytypeio/anytype/ui/multiplayer/RequestJoinSpaceFragment.kt
@@ -0,0 +1,10 @@
+package com.anytypeio.anytype.ui.multiplayer
+
+import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
+
+class RequestJoinSpaceFragment : BaseBottomSheetComposeFragment() {
+
+
+
+
+}

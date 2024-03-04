a0c57e2c4790161aac28640d2fe625b49bc8bafc
uburoiubu <ubuphobos@gmail.com>
2024-03-04 21:01:32 +0100

    DROID-2250 added use-cases

--- presentation/src/main/java/com/anytypeio/anytype/presentation/multiplayer/RequestJoinSpaceViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/multiplayer/RequestJoinSpaceViewModel.kt
@@ -2,17 +2,24 @@ package com.anytypeio.anytype.presentation.multiplayer
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.anytypeio.anytype.domain.multiplayer.GetSpaceInviteView
+import com.anytypeio.anytype.domain.multiplayer.SendJoinSpaceRequest
 import com.anytypeio.anytype.presentation.common.BaseViewModel
 import javax.inject.Inject
 
-class RequestJoinSpaceViewModel : BaseViewModel() {
+class RequestJoinSpaceViewModel(
+    private val getSpaceInviteView: GetSpaceInviteView,
+    private val sendJoinSpaceRequest: SendJoinSpaceRequest
+) : BaseViewModel() {
 
     class Factory @Inject constructor(
-
+        private val getSpaceInviteView: GetSpaceInviteView,
+        private val sendJoinSpaceRequest: SendJoinSpaceRequest
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T = RequestJoinSpaceViewModel(
-
+            getSpaceInviteView = getSpaceInviteView,
+            sendJoinSpaceRequest = sendJoinSpaceRequest
         ) as T
     }
 

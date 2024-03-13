bd6cf8b164afe34afaa58a076e9f2397724deb58
konstantiniiv <ki@anytype.io>
2024-03-13 14:55:39 +0100

    DROID-2274 use material s

--- payments/src/main/java/com/anytypeio/anytype/screens/MainPaymensScreen.kt
+++ payments/src/main/java/com/anytypeio/anytype/screens/MainPaymensScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

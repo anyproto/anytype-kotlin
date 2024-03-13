f9738824d4b090efba1f0f52ea0dd24536c6178e
konstantiniiv <ki@anytype.io>
2024-03-13 14:42:17 +0100

    DROID-2274 enter code screen

--- payments/src/main/java/com/anytypeio/anytype/screens/EnterCode.kt
+++ payments/src/main/java/com/anytypeio/anytype/screens/EnterCode.kt
@@ -1,5 +1,8 @@
 package com.anytypeio.anytype.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,10 +14,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -34,6 +39,7 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -68,6 +74,7 @@ fun EnterCodeModal(
 private fun ModalCodeContent(state: PaymentsCodeState, actionCode: (String) -> Unit) {
     val focusRequesters = remember { List(4) { FocusRequester() } }
     val enteredDigits = remember { mutableStateListOf<Char>() }
+    val focusManager = LocalFocusManager.current
 
     LaunchedEffect(key1 = enteredDigits.size) {
         if (enteredDigits.size == 4) {
@@ -75,70 +82,93 @@ private fun ModalCodeContent(state: PaymentsCodeState, actionCode: (String) -> U
         }
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Spacer(modifier = Modifier.padding(118.dp))
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp),
-            text = stringResource(id = com.anytypeio.anytype.localization.R.string.payments_code_title),
-            style = BodyBold,
-            color = colorResource(
-                id = R.color.text_primary
-            ),
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(44.dp))
-        val modifier = Modifier
-            .width(48.dp)
-            .height(64.dp)
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            focusRequesters.forEachIndexed { index, focusRequester ->
-                CodeNumber(
-                    modifier = modifier,
-                    focusRequester = focusRequester,
-                    onDigitEntered = { digit ->
-                        if (enteredDigits.size < 4) {
-                            enteredDigits.add(digit)
-                        }
-                        if (index < 3) focusRequesters[index + 1].requestFocus()
-                    },
-                    onBackspace = {
-                        if (enteredDigits.isNotEmpty()) enteredDigits.removeLast()
-                        if (index > 0) focusRequesters[index - 1].requestFocus()
-                    }
-                )
-                if (index < 3) Spacer(modifier = Modifier.width(8.dp))
-            }
+    LaunchedEffect(key1 = state) {
+        if (state is PaymentsCodeState.Loading) {
+            focusManager.clearFocus(true)
         }
-        if (state is PaymentsCodeState.Error) {
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.padding(118.dp))
             Text(
-                text = state.message,
-                color = colorResource(id = R.color.palette_system_red),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 20.dp, end = 20.dp, top = 7.dp),
+                    .padding(start = 16.dp, end = 16.dp),
+                text = stringResource(id = com.anytypeio.anytype.localization.R.string.payments_code_title),
+                style = BodyBold,
+                color = colorResource(
+                    id = R.color.text_primary
+                ),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(44.dp))
+            val modifier = Modifier
+                .width(48.dp)
+                .height(64.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                focusRequesters.forEachIndexed { index, focusRequester ->
+                    CodeNumber(
+                        isEnabled = state !is PaymentsCodeState.Loading,
+                        modifier = modifier,
+                        focusRequester = focusRequester,
+                        onDigitEntered = { digit ->
+                            if (enteredDigits.size < 4) {
+                                enteredDigits.add(digit)
+                            }
+                            if (index < 3) focusRequesters[index + 1].requestFocus()
+                        },
+                        onBackspace = {
+                            if (enteredDigits.isNotEmpty()) enteredDigits.removeLast()
+                            if (index > 0) focusRequesters[index - 1].requestFocus()
+                        }
+                    )
+                    if (index < 3) Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+            if (state is PaymentsCodeState.Error) {
+                Text(
+                    text = state.message,
+                    color = colorResource(id = R.color.palette_system_red),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp, top = 7.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Spacer(modifier = Modifier.height(149.dp))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = com.anytypeio.anytype.localization.R.string.payments_code_resend),
+                style = PreviewTitle1Regular,
+                color = colorResource(id = R.color.text_tertiary),
                 textAlign = TextAlign.Center
             )
         }
-        Spacer(modifier = Modifier.height(149.dp))
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = stringResource(id = com.anytypeio.anytype.localization.R.string.payments_code_resend),
-            style = PreviewTitle1Regular,
-            color = colorResource(id = R.color.text_tertiary),
-            textAlign = TextAlign.Center
-        )
+        AnimatedVisibility(
+            modifier = Modifier.align(Alignment.Center),
+            visible = state is PaymentsCodeState.Loading,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(24.dp),
+                color = colorResource(R.color.shape_secondary),
+                trackColor = colorResource(R.color.shape_primary)
+            )
+        }
     }
 }
 
 @Composable
 private fun CodeNumber(
+    isEnabled: Boolean,
     focusRequester: FocusRequester,
     onDigitEntered: (Char) -> Unit,
     onBackspace: () -> Unit,
@@ -173,7 +203,7 @@ private fun CodeNumber(
                 } else false
             },
         singleLine = true,
-        enabled = true,
+        enabled = isEnabled,
         cursorBrush = SolidColor(colorResource(id = R.color.text_primary)),
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Next,
@@ -197,7 +227,7 @@ private fun CodeNumber(
 @Composable
 fun EnterCodeModalPreview() {
     ModalCodeContent(
-        state = PaymentsCodeState.Error("Invalid code"),
+        state = PaymentsCodeState.Loading,
         actionCode = {}
     )
 }

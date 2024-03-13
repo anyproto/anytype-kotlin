650e35ec131fb93c8c72dce51228d4c2de6ae2f0
konstantiniiv <ki@anytype.io>
2024-03-13 16:24:35 +0100

    DROID-2274 payment welcome screen

---
+++ payments/src/main/java/com/anytypeio/anytype/screens/PaymentWelcomeScreen.kt
@@ -0,0 +1,104 @@
+package com.anytypeio.anytype.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.anytypeio.anytype.core_ui.views.BodyRegular
+import com.anytypeio.anytype.core_ui.views.ButtonSecondary
+import com.anytypeio.anytype.core_ui.views.ButtonSize
+import com.anytypeio.anytype.core_ui.views.HeadlineHeading
+import com.anytypeio.anytype.models.Tier
+import com.anytypeio.anytype.peyments.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PaymentWelcomeScreen(tier: Tier) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    ModalBottomSheet(
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        sheetState = sheetState,
+        onDismissRequest = {},
+        containerColor = colorResource(id = R.color.background_secondary),
+        content = {
+            WelcomeContent(tier)
+        },
+        shape = RoundedCornerShape(16.dp),
+        dragHandle = null
+    )
+}
+
+@Composable
+private fun WelcomeContent(tier: Tier) {
+    val tierResources = mapTierToResources(tier)
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(36.dp))
+        Icon(
+            modifier = Modifier.wrapContentSize(),
+            painter = painterResource(id = tierResources.mediumIcon!!),
+            contentDescription = "logo",
+            tint = tierResources.radialGradient
+        )
+        Spacer(modifier = Modifier.height(14.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            text = stringResource(id = R.string.payments_welcome_title, tierResources.title),
+            color = colorResource(id = R.color.text_primary),
+            style = HeadlineHeading,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(7.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            text = stringResource(id = R.string.payments_welcome_subtitle),
+            color = colorResource(id = R.color.text_primary),
+            style = BodyRegular,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        ButtonSecondary(
+            text = stringResource(id = R.string.payments_welcome_button),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            onClick = { /*TODO*/ },
+            size = ButtonSize.LargeSecondary
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+
+@Preview
+@Composable
+fun PaymentWelcomeScreenPreview() {
+    PaymentWelcomeScreen(Tier.Explorer("Free", true))
+}

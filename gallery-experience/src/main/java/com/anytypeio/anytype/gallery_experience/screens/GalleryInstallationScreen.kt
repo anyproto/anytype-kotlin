697c1d640a77927d0b18fab96ffcb9383738e2ba
konstantiniiv <ki@anytype.io>
2024-03-15 11:32:31 +0100

    DROID-2320 compose

---
+++ gallery-experience/src/main/java/com/anytypeio/anytype/gallery_experience/screens/GalleryInstallationScreen.kt
@@ -0,0 +1,14 @@
+package com.anytypeio.anytype.gallery_experience.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GalleryInstallationScreen() {}
+
+
+@Preview
+@Composable
+fun GalleryInstallationScreenPreview() {
+    GalleryInstallationScreen()
+}

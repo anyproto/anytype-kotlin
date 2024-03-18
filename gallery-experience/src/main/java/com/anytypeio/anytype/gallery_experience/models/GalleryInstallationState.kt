61a30365d244e14cafbea458b8a621c1c3ccc695
konstantiniiv <ki@anytype.io>
2024-03-18 12:59:00 +0100

    DROID-1776 compose

--- gallery-experience/src/main/java/com/anytypeio/anytype/gallery_experience/models/GalleryInstallationState.kt
+++ gallery-experience/src/main/java/com/anytypeio/anytype/gallery_experience/models/GalleryInstallationState.kt
@@ -1,9 +1,11 @@
 package com.anytypeio.anytype.gallery_experience.models
 
+import com.anytypeio.anytype.core_models.ManifestInfo
+
 sealed class GalleryInstallationState {
     object Hidden : GalleryInstallationState()
     object Loading : GalleryInstallationState()
-    object Success : GalleryInstallationState()
+    data class Success(val info: ManifestInfo) : GalleryInstallationState()
 }
 
 sealed class GalleryInstallationNavigation(val route: String) {

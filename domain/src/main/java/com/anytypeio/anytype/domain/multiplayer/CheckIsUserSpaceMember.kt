1b57a57035df5155f50e7cfbcc3cbfb3a87c1173
uburoiubu <ubuphobos@gmail.com>
2024-04-08 22:06:24 +0200

    DROID-2393 Multiplayer | Fix | Fixed use-case for checking whether current user is space member

--- domain/src/main/java/com/anytypeio/anytype/domain/multiplayer/CheckIsUserSpaceMember.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/multiplayer/CheckIsUserSpaceMember.kt
@@ -3,24 +3,28 @@ package com.anytypeio.anytype.domain.multiplayer
 import com.anytypeio.anytype.core_models.DVFilter
 import com.anytypeio.anytype.core_models.DVFilterCondition
 import com.anytypeio.anytype.core_models.ObjectType
+import com.anytypeio.anytype.core_models.ObjectWrapper
 import com.anytypeio.anytype.core_models.Relations
+import com.anytypeio.anytype.core_models.multiplayer.SpaceMemberPermissions
 import com.anytypeio.anytype.core_models.primitives.SpaceId
 import com.anytypeio.anytype.domain.auth.repo.AuthRepository
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.base.ResultInteractor
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
+import com.anytypeio.anytype.domain.debugging.Logger
 import javax.inject.Inject
 
 class CheckIsUserSpaceMember @Inject constructor(
     private val repo: BlockRepository,
     private val auth: AuthRepository,
+    private val logger: Logger,
     dispatchers: AppCoroutineDispatchers
 ) : ResultInteractor<SpaceId, Boolean>(dispatchers.io) {
 
     override suspend fun doWork(params: SpaceId): Boolean {
         val account = auth.getCurrentAccountId()
         val results = repo.searchObjects(
-            limit = 1,
+            limit = 0,
             filters = buildList {
                 add(
                     DVFilter(
@@ -44,7 +48,25 @@ class CheckIsUserSpaceMember @Inject constructor(
                     )
                 )
             },
+            keys = listOf(
+                Relations.ID,
+                Relations.NAME,
+                Relations.SPACE_ID,
+                Relations.PARTICIPANT_PERMISSIONS,
+                Relations.IDENTITY,
+                Relations.LAYOUT
+            )
         )
-        return results.isNotEmpty()
+        val struct = results.firstOrNull()
+        return if (!struct.isNullOrEmpty()) {
+            val member = ObjectWrapper.SpaceMember(struct)
+            val permission = member.permissions
+            if (permission == null || permission == SpaceMemberPermissions.NO_PERMISSIONS)
+                false
+            else
+                true
+        } else {
+            false
+        }
     }
 }

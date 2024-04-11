0d8912921cd394556f9f47da727eb2b464d155c9
uburoiubu <ubuphobos@gmail.com>
2024-04-11 18:25:35 +0200

    DROID-2176 remove notifications from icon

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/notifications/OwnerNotifications.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/notifications/OwnerNotifications.kt
@@ -1,17 +1,13 @@
 package com.anytypeio.anytype.core_ui.notifications
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,14 +33,6 @@ fun OwnerUserRequestToJoin(
             .noRippleClickable { onViewClicked() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Box(
-            modifier = Modifier
-                .size(32.dp)
-                .background(
-                    color = Color.Red,
-                    shape = CircleShape
-                )
-        )
         val placeholder = stringResource(id = R.string.untitled)
         Text(
             text = stringResource(
@@ -53,7 +41,6 @@ fun OwnerUserRequestToJoin(
                 spaceName.ifEmpty { placeholder }
             ),
             modifier = Modifier
-                .padding(start = 12.dp)
                 .weight(1f),
             color = colorResource(id = R.color.text_secondary),
             style = Caption1Regular
@@ -84,14 +71,6 @@ fun OwnerUserRequestToLeave(
             .noRippleClickable { onViewClicked() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Box(
-            modifier = Modifier
-                .size(32.dp)
-                .background(
-                    color = Color.Red,
-                    shape = CircleShape
-                )
-        )
         val placeholder = stringResource(id = R.string.untitled)
         Text(
             text = stringResource(
@@ -99,9 +78,7 @@ fun OwnerUserRequestToLeave(
                 name.ifEmpty { placeholder },
                 spaceName.ifEmpty { placeholder }
             ),
-            modifier = Modifier
-                .padding(start = 12.dp)
-                .weight(1f),
+            modifier = Modifier.weight(1f),
             color = colorResource(id = R.color.text_secondary),
             style = Caption1Regular
         )

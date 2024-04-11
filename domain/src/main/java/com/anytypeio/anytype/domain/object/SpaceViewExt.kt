0ee8ec13e7cd5235d4caedf6d4390690f4bbe612
konstantiniiv <ki@anytype.io>
2024-04-11 14:51:54 +0200

    DROID-2345 fix

--- domain/src/main/java/com/anytypeio/anytype/domain/object/SpaceViewExt.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/object/SpaceViewExt.kt
@@ -2,30 +2,7 @@ package com.anytypeio.anytype.domain.`object`
 
 import com.anytypeio.anytype.core_models.ObjectWrapper.SpaceView
 import com.anytypeio.anytype.core_models.ObjectWrapper.SpaceMember
-import com.anytypeio.anytype.core_models.multiplayer.SpaceAccessType
 import com.anytypeio.anytype.core_models.multiplayer.SpaceMemberPermissions
-import com.anytypeio.anytype.core_models.restrictions.SpaceStatus
-
-fun SpaceView.getTitle(): String = name.orEmpty()
-
-fun SpaceView.canBeShared(isOwner: Boolean): Boolean =
-    isOwner && (spaceAccessType == SpaceAccessType.SHARED || spaceAccessType == SpaceAccessType.PRIVATE)
-
-fun SpaceView.canStopSharing(isOwner: Boolean): Boolean =
-    isOwner && (spaceAccessType == SpaceAccessType.SHARED)
-
-fun SpaceView.isShared(): Boolean = spaceAccessType == SpaceAccessType.SHARED
-
-fun SpaceView.canBeDelete(): Boolean = spaceAccessType == SpaceAccessType.PRIVATE
-
-fun SpaceView.canBeArchive(): Boolean = spaceAccountStatus == SpaceStatus.SPACE_REMOVING
-
-fun SpaceView.canCancelJoinRequest(): Boolean = spaceAccountStatus == SpaceStatus.SPACE_JOINING
-
-fun SpaceView.canStopShare(): Boolean = this.isShared()
-
-//val SpaceView.isActive: Boolean
-//    get() = this.localStatus == LocalStatus.OK && this.accountStatus != AccountStatus.SPACE_REMOVING && this.accountStatus != AccountStatus.SPACE_DELETED
 
 fun SpaceView.canAddWriters(participants: List<SpaceMember>): Boolean {
     if (!canAddReaders(participants)) return false

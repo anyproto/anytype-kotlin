4884325b08583782bf7f4ebd92d5c30cacfe959b
uburoiubu <ubuphobos@gmail.com>
2024-04-22 16:43:59 +0200

    DROID-2323 handling only deep link from args for home screen

--- app/src/main/java/com/anytypeio/anytype/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/home/HomeScreenFragment.kt
@@ -31,7 +31,6 @@ import com.anytypeio.anytype.presentation.home.HomeScreenViewModel.Navigation
 import com.anytypeio.anytype.presentation.widgets.DropDownMenuAction
 import com.anytypeio.anytype.ui.base.navigation
 import com.anytypeio.anytype.ui.gallery.GalleryInstallationFragment
-import com.anytypeio.anytype.ui.main.MainActivity
 import com.anytypeio.anytype.ui.multiplayer.RequestJoinSpaceFragment
 import com.anytypeio.anytype.ui.multiplayer.ShareSpaceFragment
 import com.anytypeio.anytype.ui.objects.creation.SelectObjectTypeFragment
@@ -154,17 +153,10 @@ class HomeScreenFragment : BaseComposeFragment() {
 
     private fun proceedWithDeepLinks() {
         val deepLinkFromFragment = deepLink
-        val deepLinkFromActivity = (requireActivity() as? MainActivity)?.deepLink
-
-        when {
-            deepLinkFromFragment != null -> {
-                vm.onResume(DefaultDeepLinkResolver.resolve(deepLinkFromFragment))
-                arguments?.putString(DEEP_LINK_KEY, null)
-            }
-            deepLinkFromActivity != null -> {
-                vm.onResume(DefaultDeepLinkResolver.resolve(deepLinkFromActivity))
-                (requireActivity() as? MainActivity)?.deepLink = null
-            }
+        if (deepLinkFromFragment != null) {
+            Timber.d("Deeplink  from fragment args")
+            vm.onResume(DefaultDeepLinkResolver.resolve(deepLinkFromFragment))
+            arguments?.putString(DEEP_LINK_KEY, null)
         }
     }
 

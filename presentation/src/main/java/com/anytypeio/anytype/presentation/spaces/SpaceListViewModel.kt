90ad3ee7625e6f138d25bd7f7471968cb8f90a9b
Evgenii Kozlov <ubuphobos@gmail.com>
2024-04-25 18:54:30 +0200

    DROID-2332 Multiplayer | Fix |  Fix actions on space-list screen (#1182)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/spaces/SpaceListViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/spaces/SpaceListViewModel.kt
@@ -70,10 +70,19 @@ class SpaceListViewModel(
                                     if (isOwner) {
                                         add(SpaceListItemView.Action.DeleteSpace)
                                     } else {
-                                        if (spaceView.spaceAccountStatus == SpaceStatus.SPACE_JOINING) {
-                                            add(SpaceListItemView.Action.CancelJoinRequest)
-                                        } else {
-                                            add(SpaceListItemView.Action.LeaveSpace)
+                                        when (spaceView.spaceAccountStatus) {
+                                            SpaceStatus.SPACE_JOINING -> {
+                                                add(SpaceListItemView.Action.CancelJoinRequest)
+                                            }
+                                            SpaceStatus.SPACE_REMOVING -> {
+                                                add(SpaceListItemView.Action.DeleteSpace)
+                                            }
+                                            SpaceStatus.SPACE_ACTIVE, SpaceStatus.UNKNOWN -> {
+                                                add(SpaceListItemView.Action.LeaveSpace)
+                                            }
+                                            else -> {
+                                                // Do nothing.
+                                            }
                                         }
                                     }
                                 } else if (spaceView.spaceAccessType == SpaceAccessType.PRIVATE) {

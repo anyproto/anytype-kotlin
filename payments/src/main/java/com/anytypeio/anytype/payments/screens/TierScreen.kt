9aaa5c6e13aef40c2d522eaaec99595b90cb8500
konstantiniiv <ki@anytype.io>
2024-05-03 11:14:04 +0200

    DROID-2275 fix

--- payments/src/main/java/com/anytypeio/anytype/payments/screens/TierScreen.kt
+++ payments/src/main/java/com/anytypeio/anytype/payments/screens/TierScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
@@ -17,6 +18,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text2.input.TextFieldState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
@@ -79,7 +81,7 @@ fun TierViewScreen(
                     actionTier = actionTier,
                     anyNameTextField = anyNameTextField
                 )
-            }
+            },
         )
     }
 }

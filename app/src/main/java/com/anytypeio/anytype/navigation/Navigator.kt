0db1134036d952295cefc7c3b1b7714891e1c545
uburoiubu <ubuphobos@gmail.com>
2024-05-30 18:24:47 +0200

    DROID-2545 fixes

--- app/src/main/java/com/anytypeio/anytype/navigation/Navigator.kt
+++ app/src/main/java/com/anytypeio/anytype/navigation/Navigator.kt
@@ -143,7 +143,10 @@ class Navigator : AppNavigation {
     }
 
     override fun openPageSearch() {
-        navController?.navigate(R.id.globalSearchScreen)
+        // Old search
+        navController?.navigate(R.id.pageSearchFragment)
+        // Uncomment to use new search
+//        navController?.navigate(R.id.globalSearchScreen)
     }
 
     override fun openObjectSet(

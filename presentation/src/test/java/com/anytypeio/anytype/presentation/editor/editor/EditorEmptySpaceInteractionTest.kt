357dd74b3e2dcdb2516600afc535c6c96bdcfe69
uburoiubu <ubuphobos@gmail.com>
2024-06-04 16:03:20 +0200

    DROID-2561 fixes

--- presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/EditorEmptySpaceInteractionTest.kt
+++ presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/EditorEmptySpaceInteractionTest.kt
@@ -26,7 +26,7 @@ import org.mockito.kotlin.times
 import org.mockito.kotlin.verifyBlocking
 import org.mockito.kotlin.verifyNoInteractions
 
-class EditorEmptySpaceModeTest : EditorPresentationTestSetup() {
+class EditorEmptySpaceInteractionTest : EditorPresentationTestSetup() {
 
     val title = Block(
         id = MockDataFactory.randomUuid(),

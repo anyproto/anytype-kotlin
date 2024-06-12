ecc6132bf717351ee9c2da60aba38b3b1f6505ce
Evgenii Kozlov <ubuphobos@gmail.com>
2024-06-12 17:27:10 +0200

    DROID-2572 Release | Tech | Disable landscape support, new global search and edge-to-edge (#1290)

--- app/src/main/java/com/anytypeio/anytype/navigation/Navigator.kt
+++ app/src/main/java/com/anytypeio/anytype/navigation/Navigator.kt
@@ -144,9 +144,9 @@ class Navigator : AppNavigation {
 
     override fun openPageSearch() {
         // Old search
-//        navController?.navigate(R.id.pageSearchFragment)
+        navController?.navigate(R.id.pageSearchFragment)
         // Uncomment to use new search
-        navController?.navigate(R.id.globalSearchScreen)
+//        navController?.navigate(R.id.globalSearchScreen)
     }
 
     override fun openObjectSet(

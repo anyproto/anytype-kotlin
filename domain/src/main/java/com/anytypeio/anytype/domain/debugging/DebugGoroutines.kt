69ebabe00b22ffcfa1711a036109983fe8530012
konstantiniiv <ki@anytype.io>
2024-06-20 20:22:08 +0200

    DROID-2593 use case

--- domain/src/main/java/com/anytypeio/anytype/domain/debugging/DebugGoroutines.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/debugging/DebugGoroutines.kt
@@ -3,16 +3,39 @@ package com.anytypeio.anytype.domain.debugging
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.base.ResultInteractor
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
+import java.io.File
 import javax.inject.Inject
 
 class DebugGoroutines @Inject constructor(
+    private val cacheDir: String,
     private val repo: BlockRepository,
     dispatchers: AppCoroutineDispatchers
-) : ResultInteractor<DebugGoroutines.Params, Unit>(dispatchers.io) {
+) : ResultInteractor<DebugGoroutines.Params, String>(dispatchers.io) {
 
-    override suspend fun doWork(params: Params) {
-        repo.debugStackGoroutines(params.path)
+    override suspend fun doWork(params: Params): String {
+        val path: String
+        val resultFilePath: String
+
+        if (params.path == null) {
+            // Construct the path using the current time to ensure uniqueness
+            path = "${cacheDir}/debug/goroutines/${System.currentTimeMillis()}/"
+            // Middleware put the log in a subdirectory called logs
+            resultFilePath = "$path/logs/"
+
+            // Create the directories if they do not exist
+            File(path).apply { mkdirs() }
+        } else {
+            path = params.path
+            resultFilePath = path
+        }
+
+        // Perform the debug operation
+        repo.debugStackGoroutines(path)
+
+        // Return the result file path
+        return resultFilePath
     }
 
-    data class Params(val path: String)
+    // Data class for the parameters, with a default value of null for the path
+    data class Params(val path: String? = null)
 }

7eb8fef59667a4fb69959fd90178d34ae09c4b17
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2024-07-02 20:17:26 +0200

    DROID-2619 fix

--- presentation/src/main/java/com/anytypeio/anytype/presentation/home/HomeScreenViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/home/HomeScreenViewModel.kt
@@ -409,7 +409,9 @@ class HomeScreenViewModel(
                             getSpaceView = getSpaceView,
                             onRequestCache = {
                                 cached.find { view ->
-                                    view.id == widget.id && view is WidgetView.Tree && view.source == widget.source
+                                    view.id == widget.id
+                                            && view is WidgetView.Tree
+                                            && view.source == widget.source
                                 } as? WidgetView.Tree
                             }
                         )
@@ -425,7 +427,9 @@ class HomeScreenViewModel(
                                 getSpaceView = getSpaceView,
                                 onRequestCache = {
                                     cached.find { view ->
-                                        view.id == widget.id && view is WidgetView.ListOfObjects  && view.source == widget.source
+                                        view.id == widget.id
+                                                && view is WidgetView.ListOfObjects
+                                                && view.source == widget.source
                                     } as? WidgetView.ListOfObjects
                                 }
                             )
@@ -440,7 +444,9 @@ class HomeScreenViewModel(
                                 urlBuilder = urlBuilder,
                                 onRequestCache = {
                                     cached.find { view ->
-                                        view.id == widget.id && view is WidgetView.SetOfObjects  && view.source == widget.source
+                                        view.id == widget.id
+                                                && view is WidgetView.SetOfObjects
+                                                && view.source == widget.source
                                     } as? WidgetView.SetOfObjects
                                 }
                             )

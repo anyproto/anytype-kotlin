176a0c122e5db24d06f891f4607bb6e16b94bb9d
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2024-07-22 14:13:49 +0200

    DROID-2603 fixes

--- presentation/src/main/java/com/anytypeio/anytype/presentation/search/ObjectSearchSection.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/search/ObjectSearchSection.kt
@@ -3,9 +3,9 @@ package com.anytypeio.anytype.presentation.search
 import com.anytypeio.anytype.presentation.navigation.DefaultSearchItem
 
 sealed class ObjectSearchSection : DefaultSearchItem {
-    object RecentlyOpened: ObjectSearchSection()
+    data object RecentlyOpened: ObjectSearchSection()
     sealed class SelectWidgetSource : ObjectSearchSection() {
-        object FromMyObjects: SelectWidgetSource()
-        object FromLibrary : SelectWidgetSource()
+        data object FromMyObjects: SelectWidgetSource()
+        data object FromLibrary : SelectWidgetSource()
     }
 }

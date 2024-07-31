0949b477c2c2a89838466bff58165fbec975e676
konstantiniiv <ki@anytype.io>
2024-07-31 11:38:22 +0200

    DROID-2663 di

--- app/src/main/java/com/anytypeio/anytype/di/feature/history/VersionHistoryDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/history/VersionHistoryDI.kt
@@ -1,24 +1,12 @@
 package com.anytypeio.anytype.di.feature.history
 
 import androidx.lifecycle.ViewModelProvider
-import com.anytypeio.anytype.analytics.base.Analytics
 import com.anytypeio.anytype.core_utils.di.scope.PerModal
-import com.anytypeio.anytype.core_utils.di.scope.PerScreen
-import com.anytypeio.anytype.di.common.ComponentDependencies
-import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
-import com.anytypeio.anytype.domain.block.repo.BlockRepository
-import com.anytypeio.anytype.domain.misc.DateProvider
-import com.anytypeio.anytype.domain.misc.LocaleProvider
-import com.anytypeio.anytype.domain.misc.UrlBuilder
-import com.anytypeio.anytype.domain.objects.StoreOfObjectTypes
-import com.anytypeio.anytype.domain.objects.StoreOfRelations
-import com.anytypeio.anytype.presentation.editor.toggle.ToggleStateHolder
 import com.anytypeio.anytype.presentation.history.VersionHistoryVMFactory
 import com.anytypeio.anytype.presentation.history.VersionHistoryViewModel
 import com.anytypeio.anytype.ui.history.VersionHistoryFragment
 import dagger.Binds
 import dagger.BindsInstance
-import dagger.Component
 import dagger.Module
 import dagger.Subcomponent
 
@@ -38,14 +26,6 @@ interface VersionHistoryComponent {
         fun build(): VersionHistoryComponent
     }
 
-//    @Component.Factory
-//    interface Factory {
-//        fun create(
-//            dependencies: VersionHistoryComponentDependencies,
-//            @BindsInstance vmParams: VersionHistoryViewModel.VmParams
-//        ): VersionHistoryComponent
-//    }
-
     fun inject(fragment: VersionHistoryFragment)
 }
 
@@ -62,16 +42,4 @@ object VersionHistoryModule {
         ): ViewModelProvider.Factory
     }
 }
-//
-//interface VersionHistoryComponentDependencies : ComponentDependencies {
-//    fun analytics(): Analytics
-//    fun blockRepository(): BlockRepository
-//    fun appCoroutineDispatchers(): AppCoroutineDispatchers
-//    fun dateProvider(): DateProvider
-//    fun localeProvider(): LocaleProvider
-//    fun provideUrlBuilder(): UrlBuilder
-//    fun provideStoreOfRelations(): StoreOfRelations
-//    fun provideStoreOfTypes(): StoreOfObjectTypes
-//    fun provideToggleStateHolder(): ToggleStateHolder
-//}
 

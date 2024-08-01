edc061d2d67a1b32d393c35fa4410f997ec1d003
Evgenii Kozlov <ubuphobos@gmail.com>
2024-08-01 18:23:52 +0200

    DROID-2708 Widgets | Fix | Clearing back stack correctly when switching space after space creation (#1452)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/widgets/SelectWidgetSourceViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/widgets/SelectWidgetSourceViewModel.kt
@@ -175,7 +175,7 @@ class SelectWidgetSourceViewModel(
                 }
             }
             Config.None -> {
-                // Do nothing.
+                Timber.w("Missing config for widget source")
             }
         }
     }

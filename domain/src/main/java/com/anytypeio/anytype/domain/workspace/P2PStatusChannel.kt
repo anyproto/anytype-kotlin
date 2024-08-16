fab0fb9db4f9a3645b92eb5794551cfd3f47e680
konstantiniiv <ki@anytype.io>
2024-08-16 09:01:21 +0200

    DROID-2738 interfaces

--- domain/src/main/java/com/anytypeio/anytype/domain/workspace/P2PStatusChannel.kt
+++
@@ -1,8 +0,0 @@
-package com.anytypeio.anytype.domain.workspace
-
-import com.anytypeio.anytype.core_models.multiplayer.P2PStatusUpdate
-import kotlinx.coroutines.flow.Flow
-
-interface P2PStatusChannel {
-    fun observe(): Flow<P2PStatusUpdate>
-}
--- domain/src/main/java/com/anytypeio/anytype/domain/workspace/SpaceSyncStatusChannel.kt
+++
@@ -1,9 +0,0 @@
-package com.anytypeio.anytype.domain.workspace
-
-import com.anytypeio.anytype.core_models.Id
-import com.anytypeio.anytype.core_models.multiplayer.SpaceSyncUpdate
-import kotlinx.coroutines.flow.Flow
-
-interface SpaceSyncStatusChannel {
-    fun observe(activeSpaceId: Id): Flow<SpaceSyncUpdate>
-}

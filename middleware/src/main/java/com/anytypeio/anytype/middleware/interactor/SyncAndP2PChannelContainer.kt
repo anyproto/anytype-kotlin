689c66efe95b61087e88243db8eaa01111e0741e
konstantiniiv <ki@anytype.io>
2024-08-19 13:53:56 +0200

    DROID-2738 fix

--- middleware/src/main/java/com/anytypeio/anytype/middleware/interactor/SyncAndP2PChannelContainer.kt
+++ middleware/src/main/java/com/anytypeio/anytype/middleware/interactor/SyncAndP2PChannelContainer.kt
@@ -1,11 +1,14 @@
 package com.anytypeio.anytype.middleware.interactor
 
+import com.anytypeio.anytype.core_models.Id
 import com.anytypeio.anytype.core_models.multiplayer.P2PStatusUpdate
 import com.anytypeio.anytype.core_models.multiplayer.SpaceSyncUpdate
+import com.anytypeio.anytype.core_utils.ext.cancel
 import com.anytypeio.anytype.data.auth.status.SyncAndP2PStatusEventsStore
 import com.anytypeio.anytype.middleware.mappers.MP2PStatusUpdate
 import com.anytypeio.anytype.middleware.mappers.MSyncStatusUpdate
 import com.anytypeio.anytype.middleware.mappers.toCoreModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -18,39 +21,29 @@ import timber.log.Timber
 
 class SyncAndP2PStatusEventsStoreImpl(
     private val scope: CoroutineScope,
-    private val channel: EventHandlerChannel
+    private val channel: EventHandlerChannel,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : SyncAndP2PStatusEventsStore {
 
-    private val _p2pStatus = MutableStateFlow<MutableMap<String, P2PStatusUpdate>>(mutableMapOf())
-    private val _syncStatus = MutableStateFlow<MutableMap<String, SpaceSyncUpdate>>(mutableMapOf())
+    private val _p2pStatus = MutableStateFlow<MutableMap<Id, P2PStatusUpdate>>(mutableMapOf())
+    private val _syncStatus = MutableStateFlow<MutableMap<Id, SpaceSyncUpdate>>(mutableMapOf())
 
-    override val p2pStatus: Flow<Map<String, P2PStatusUpdate>> get() = _p2pStatus
-    override val syncStatus: Flow<Map<String, SpaceSyncUpdate>> get() = _syncStatus
+    override val p2pStatus: Flow<Map<Id, P2PStatusUpdate>> get() = _p2pStatus
+    override val syncStatus: Flow<Map<Id, SpaceSyncUpdate>> get() = _syncStatus
 
     private val jobs = mutableListOf<Job>()
 
     override fun start() {
         Timber.i("SyncAndP2PStatusEventsStoreImpl start")
-        jobs.forEach { it.cancel() }
-        jobs.clear()
-        jobs += scope.launch(Dispatchers.IO) {
+        jobs.cancel()
+        jobs += scope.launch(dispatcher) {
             channel.flow()
                 .catch { e ->
-                    Timber.e(e, "Error collecting P2P status updates")
+                    Timber.e(e, "Error collecting P2P & Sync status updates")
                 }
                 .collect { emission ->
                     emission.messages.forEach { message ->
                         message.p2pStatusUpdate?.let { processP2PStatusUpdate(it) }
-                    }
-                }
-        }
-        jobs += scope.launch(Dispatchers.IO) {
-            channel.flow()
-                .catch { e ->
-                    Timber.e(e, "Error collecting Sync status updates")
-                }
-                .collect { emission ->
-                    emission.messages.forEach { message ->
                         message.spaceSyncStatusUpdate?.let { processSpaceSyncUpdate(it) }
                     }
                 }
@@ -59,8 +52,7 @@ class SyncAndP2PStatusEventsStoreImpl(
 
     override fun stop() {
         Timber.i("SyncAndP2PStatusEventsStoreImpl stop")
-        jobs.forEach { it.cancel() }
-        jobs.clear()
+        jobs.cancel()
     }
 
     private fun processP2PStatusUpdate(update: MP2PStatusUpdate) {

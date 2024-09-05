4db605ce8b8df8c02683cf0505714e8846cd55a7
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2024-09-05 09:32:34 +0200

    DROID-2789 refact Relations

--- domain/src/main/java/com/anytypeio/anytype/domain/search/RelationsSubscriptionManager.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/search/RelationsSubscriptionManager.kt
@@ -12,7 +12,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 
@@ -21,9 +23,38 @@ class RelationsSubscriptionManager @Inject constructor(
     private val container: RelationsSubscriptionContainer,
     private val spaceManager: SpaceManager
 ) {
-    private val pipeline = spaceManager.observe().flatMapLatest { config ->
-        val params = buildParams(listOf(config.space, config.techSpace, Marketplace.MARKETPLACE_SPACE_ID))
-        container.observe(params)
+    val pipeline get() = spaceManager.state().flatMapLatest { state ->
+        when(state) {
+            is SpaceManager.State.Space.Active -> {
+                val params = buildParams(
+                    spaces = listOf(
+                        state.config.space,
+                        state.config.techSpace,
+                        Marketplace.MARKETPLACE_SPACE_ID
+                    )
+                )
+                container.observe(params)
+            }
+            is SpaceManager.State.Space.Idle -> {
+                flow {
+                    container.unsubscribe()
+                    emit(
+                        RelationsSubscriptionContainer.Index.empty()
+                    )
+                }
+            }
+            SpaceManager.State.Vault -> {
+                flow {
+                    container.unsubscribe()
+                    emit(
+                        RelationsSubscriptionContainer.Index.empty()
+                    )
+                }
+            }
+            SpaceManager.State.Init -> {
+                emptyFlow()
+            }
+        }
     }
 
     private var job: Job? = null
@@ -33,63 +64,65 @@ class RelationsSubscriptionManager @Inject constructor(
         job = scope.launch { pipeline.collect() }
     }
 
-    private fun buildParams(spaces: List<Id>) = RelationsSubscriptionContainer.Params(
-        subscription = RelationsSubscriptionContainer.SUBSCRIPTION_ID,
-        filters = listOf(
-            DVFilter(
-                relation = Relations.LAYOUT,
-                condition = DVFilterCondition.EQUAL,
-                value = ObjectType.Layout.RELATION.code.toDouble()
-            ),
-            DVFilter(
-                relation = Relations.IS_DELETED,
-                condition = DVFilterCondition.NOT_EQUAL,
-                value = true
-            ),
-            DVFilter(
-                relation = Relations.IS_ARCHIVED,
-                condition = DVFilterCondition.NOT_EQUAL,
-                value = true
-            ),
-            DVFilter(
-                relation = Relations.SPACE_ID,
-                condition = DVFilterCondition.IN,
-                value = spaces
-            )
-        ),
-        limit = 0,
-        offset = 0L,
-        sorts = emptyList(),
-        sources = emptyList(),
-        keys = listOf(
-            Relations.ID,
-            Relations.SPACE_ID,
-            Relations.TYPE,
-            Relations.LAYOUT,
-            Relations.NAME,
-            Relations.RELATION_FORMAT,
-            Relations.RELATION_KEY,
-            Relations.SCOPE,
-            Relations.IS_READ_ONLY,
-            Relations.IS_HIDDEN,
-            Relations.IS_DELETED,
-            Relations.IS_ARCHIVED,
-            Relations.IS_FAVORITE,
-            Relations.RESTRICTIONS,
-            Relations.MAX_COUNT,
-            Relations.RELATION_READ_ONLY_VALUE,
-            Relations.RELATION_DEFAULT_VALUE,
-            Relations.RELATION_FORMAT_OBJECT_TYPES,
-            Relations.SOURCE_OBJECT
-        ),
-        ignoreWorkspace = true
-    )
-
     fun onStop() {
         scope.launch {
             container.unsubscribe()
             job?.cancel()
             job = null
         }
+    }
+
+    companion object {
+        fun buildParams(spaces: List<Id>) = RelationsSubscriptionContainer.Params(
+            subscription = RelationsSubscriptionContainer.SUBSCRIPTION_ID,
+            filters = listOf(
+                DVFilter(
+                    relation = Relations.LAYOUT,
+                    condition = DVFilterCondition.EQUAL,
+                    value = ObjectType.Layout.RELATION.code.toDouble()
+                ),
+                DVFilter(
+                    relation = Relations.IS_DELETED,
+                    condition = DVFilterCondition.NOT_EQUAL,
+                    value = true
+                ),
+                DVFilter(
+                    relation = Relations.IS_ARCHIVED,
+                    condition = DVFilterCondition.NOT_EQUAL,
+                    value = true
+                ),
+                DVFilter(
+                    relation = Relations.SPACE_ID,
+                    condition = DVFilterCondition.IN,
+                    value = spaces
+                )
+            ),
+            limit = 0,
+            offset = 0L,
+            sorts = emptyList(),
+            sources = emptyList(),
+            keys = listOf(
+                Relations.ID,
+                Relations.SPACE_ID,
+                Relations.TYPE,
+                Relations.LAYOUT,
+                Relations.NAME,
+                Relations.RELATION_FORMAT,
+                Relations.RELATION_KEY,
+                Relations.SCOPE,
+                Relations.IS_READ_ONLY,
+                Relations.IS_HIDDEN,
+                Relations.IS_DELETED,
+                Relations.IS_ARCHIVED,
+                Relations.IS_FAVORITE,
+                Relations.RESTRICTIONS,
+                Relations.MAX_COUNT,
+                Relations.RELATION_READ_ONLY_VALUE,
+                Relations.RELATION_DEFAULT_VALUE,
+                Relations.RELATION_FORMAT_OBJECT_TYPES,
+                Relations.SOURCE_OBJECT
+            ),
+            ignoreWorkspace = true
+        )
     }
 }

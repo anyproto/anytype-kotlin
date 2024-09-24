a6ca1069a1f90538d1c301ae51be091c8f7c89a7
konstantiniiv <ki@anytype.io>
2024-09-24 11:15:19 +0200

    DROID-2851 title

--- feature-all-content/src/main/java/com/anytypeio/anytype/feature_allcontent/ui/AllContentViews.kt
+++ feature-all-content/src/main/java/com/anytypeio/anytype/feature_allcontent/ui/AllContentViews.kt
@@ -1,15 +1,47 @@
 package com.anytypeio.anytype.feature_allcontent.ui
 
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.anytypeio.anytype.core_ui.views.Title1
 import com.anytypeio.anytype.core_ui.views.Title2
 import com.anytypeio.anytype.feature_allcontent.R
 
 @Composable
-fun AllContentTitle() {
-    Text(text = "All Content",
-        style = Title2,
-        color = colorResource(id = R.color.text_primary)
-    )
+fun BoxScope.AllContentTitle(state: AllContentTitleState) {
+    when (state) {
+        AllContentTitleState.Hidden -> return
+        AllContentTitleState.AllContent -> {
+            Text(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .align(Center),
+                text = stringResource(id = R.string.all_content_title_all_content),
+                style = Title1,
+                color = colorResource(id = R.color.text_primary)
+            )
+        }
+
+        AllContentTitleState.OnlyUnlinked -> {
+            Text(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .align(Center),
+                text = stringResource(id = R.string.all_content_title_only_unlinked),
+                style = Title2,
+                color = colorResource(id = R.color.text_primary)
+            )
+        }
+    }
+}
+
+sealed class AllContentTitleState {
+    data object Hidden : AllContentTitleState()
+    data object AllContent : AllContentTitleState()
+    data object OnlyUnlinked : AllContentTitleState()
 }

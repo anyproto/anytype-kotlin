0a4a03f4e4e756cdf8d19ffdb48bea3c67be7539
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2024-09-26 19:37:18 +0200

    DROID-2769 App | Tech | Make tests compile (#1592)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/objects/ObjectIcon.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/objects/ObjectIcon.kt
@@ -23,7 +23,11 @@ sealed class ObjectIcon {
 
     data class Bookmark(val image: Url) : ObjectIcon()
 
-    data class File(val mime: String?, val fileName: String?, val extensions: String?) : ObjectIcon()
+    data class File(
+        val mime: String?,
+        val fileName: String?,
+        val extensions: String? = null
+    ) : ObjectIcon()
 
     object Deleted : ObjectIcon()
     data class Checkbox(val isChecked: Boolean) : ObjectIcon()

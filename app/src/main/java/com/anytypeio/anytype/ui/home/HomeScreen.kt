37dee7aec23bd97f561236f22ee856948d64b730
Evgenii Kozlov <ubuphobos@gmail.com>
2024-09-26 19:27:10 +0200

    DROID-2731 Vault | Design | Update nav toolbar icons and background (#1594)

--- app/src/main/java/com/anytypeio/anytype/ui/home/HomeScreen.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/home/HomeScreen.kt
@@ -822,7 +822,7 @@ fun HomeScreenBottomToolbar(
             .width(BottomNavigationDefaults.Width)
             .background(
                 shape = RoundedCornerShape(16.dp),
-                color = colorResource(id = R.color.home_screen_button)
+                color = colorResource(id = R.color.shape_tertiary)
             )
     ) {
         Box(

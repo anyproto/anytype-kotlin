b5121a0d049c77fbb39d4fa96589165c73c08111
konstantiniiv <ki@anytype.io>
2024-10-08 23:59:47 +0300

    DROID-2880 legacy

--- presentation/src/main/java/com/anytypeio/anytype/presentation/editor/render/DefaultBlockViewRenderer.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/editor/render/DefaultBlockViewRenderer.kt
@@ -27,6 +27,7 @@ import com.anytypeio.anytype.presentation.editor.editor.model.BlockView.Appearan
 import com.anytypeio.anytype.presentation.editor.toggle.ToggleStateHolder
 import com.anytypeio.anytype.presentation.extension.getProperObjectName
 import com.anytypeio.anytype.presentation.mapper.marks
+import com.anytypeio.anytype.presentation.mapper.objectIcon
 import com.anytypeio.anytype.presentation.mapper.toFileView
 import com.anytypeio.anytype.presentation.mapper.toPictureView
 import com.anytypeio.anytype.presentation.mapper.toVideoView
@@ -1668,11 +1669,7 @@ class DefaultBlockViewRenderer @Inject constructor(
         val inEditorAppearance = factory.createInEditorLinkAppearance()
         val isCard = inEditorAppearance.isCard
         val objectIcon = if (inEditorAppearance.showIcon) {
-            ObjectIcon.getEditorLinkToObjectIcon(
-                obj = obj,
-                layout = obj.layout,
-                builder = urlBuilder
-            )
+            obj.objectIcon(urlBuilder)
         } else {
             ObjectIcon.None
         }
@@ -2269,11 +2266,7 @@ class DefaultBlockViewRenderer @Inject constructor(
                     isCollection = isCollection
                 )
             }
-            val icon = ObjectIcon.getEditorLinkToObjectIcon(
-                obj = targetSet,
-                layout = targetSet.layout,
-                builder = urlBuilder
-            )
+            val icon = targetSet.objectIcon(urlBuilder)
             val isSetNoQuery = targetSet.setOf.all { it.isNullOrBlank() }
             if (isSetNoQuery && !content.isCollection) {
                 return BlockView.DataView.EmptyData(
--- presentation/src/main/java/com/anytypeio/anytype/presentation/objects/ObjectIcon.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/objects/ObjectIcon.kt
@@ -1,7 +1,6 @@
 package com.anytypeio.anytype.presentation.objects
 
 import com.anytypeio.anytype.core_models.Hash
-import com.anytypeio.anytype.core_models.ObjectType
 import com.anytypeio.anytype.core_models.ObjectWrapper
 import com.anytypeio.anytype.core_models.Url
 import com.anytypeio.anytype.domain.misc.UrlBuilder
@@ -41,61 +40,6 @@ sealed class ObjectIcon {
     data object Deleted : ObjectIcon()
 
     data class Checkbox(val isChecked: Boolean) : ObjectIcon()
-
-    companion object {
-
-        fun getEditorLinkToObjectIcon(
-            obj: ObjectWrapper.Basic,
-            layout: ObjectType.Layout?,
-            builder: UrlBuilder
-        ): ObjectIcon {
-            val img = obj.iconImage
-            val emoji = obj.iconEmoji
-            return ObjectIcon.Empty.Page
-//            return when (layout) {
-//                ObjectType.Layout.BASIC -> when {
-//                    !img.isNullOrBlank() -> Basic.Image(hash = builder.thumbnail(img))
-//                    !emoji.isNullOrBlank() -> Basic.Emoji(unicode = emoji)
-//                    else -> None
-//                }
-//                ObjectType.Layout.OBJECT_TYPE -> when {
-//                    !img.isNullOrBlank() -> Basic.Image(hash = builder.thumbnail(img))
-//                    !emoji.isNullOrBlank() -> Basic.Emoji(unicode = emoji)
-//                    else -> Basic.Avatar(obj.name.orEmpty())
-//                }
-//                ObjectType.Layout.PROFILE, ObjectType.Layout.PARTICIPANT -> if (!img.isNullOrBlank()) {
-//                    Profile.Image(hash = builder.thumbnail(img))
-//                } else {
-//                    Profile.Avatar(name = obj.name.orEmpty())
-//                }
-//                ObjectType.Layout.SET, ObjectType.Layout.COLLECTION -> if (!img.isNullOrBlank()) {
-//                    Basic.Image(hash = builder.thumbnail(img))
-//                } else if (!emoji.isNullOrBlank()) {
-//                    Basic.Emoji(unicode = emoji)
-//                } else {
-//                    None
-//                }
-//                ObjectType.Layout.IMAGE -> if (!img.isNullOrBlank()) {
-//                    Basic.Image(hash = builder.thumbnail(img))
-//                } else {
-//                    None
-//                }
-//                ObjectType.Layout.TODO -> Task(isChecked = obj.done ?: false)
-//                ObjectType.Layout.NOTE -> Basic.Avatar(obj.snippet.orEmpty())
-//                ObjectType.Layout.FILE -> Basic.Avatar(obj.name.orEmpty())
-//                ObjectType.Layout.BOOKMARK -> when {
-//                    !img.isNullOrBlank() -> Bookmark(image = builder.thumbnail(img))
-//                    !emoji.isNullOrBlank() -> Basic.Emoji(unicode = emoji)
-//                    else -> None
-//                }
-//                ObjectType.Layout.RELATION -> None
-//                ObjectType.Layout.DASHBOARD -> None
-//                ObjectType.Layout.SPACE -> None
-//                ObjectType.Layout.PDF -> None
-//                else -> None
-//            }
-        }
-    }
 }
 
 sealed class SpaceMemberIconView {

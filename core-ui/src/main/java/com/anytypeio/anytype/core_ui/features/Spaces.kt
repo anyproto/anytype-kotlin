21a2128e0315ed1160a0d391f8da5100bcec89b2
Evgenii Kozlov <ubuphobos@gmail.com>
2024-10-09 14:22:13 +0200

    DROID-2864 Multispaces | Design | Update placeholder icons (#1662)

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/features/Spaces.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/features/Spaces.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,11 +15,15 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import coil.compose.rememberAsyncImagePainter
+import com.anytypeio.anytype.core_models.SystemColor
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.foundation.noRippleClickable
 import com.anytypeio.anytype.presentation.spaces.SpaceIconView
@@ -33,18 +38,31 @@ fun SpaceIconView(
     icon: SpaceIconView,
     onSpaceIconClick: () -> Unit,
 ) {
+    val radius = when(mainSize) {
+        40.dp -> 5.dp
+        48.dp -> 6.dp
+        64.dp -> 8.dp
+        96.dp -> 12.dp
+        else -> 6.dp
+    }
+
+    val fontSize = when(mainSize) {
+        40.dp -> 24.sp
+        48.dp -> 28.sp
+        64.dp -> 40.sp
+        96.dp -> 65.sp
+        else -> 28.sp
+    }
+
     when (icon) {
         is SpaceIconView.Image -> {
             Image(
-                painter = rememberAsyncImagePainter(
-                    model = icon.url,
-                    error = painterResource(id = R.drawable.ic_home_widget_space)
-                ),
+                painter = rememberAsyncImagePainter(model = icon.url),
                 contentDescription = "Custom image space icon",
                 contentScale = ContentScale.Crop,
                 modifier = modifier
                     .size(mainSize)
-                    .clip(RoundedCornerShape(4.dp))
+                    .clip(RoundedCornerShape(radius))
                     .noRippleClickable {
                         onSpaceIconClick.invoke()
                     }
@@ -74,14 +92,53 @@ fun SpaceIconView(
             }
 
         }
-        else -> {
-            Image(
-                painter = painterResource(id = R.drawable.ic_home_widget_space),
-                contentDescription = "Placeholder space icon",
-                contentScale = ContentScale.Crop,
+        is SpaceIconView.Placeholder -> {
+            Box(
                 modifier = modifier
                     .size(mainSize)
-                    .clip(RoundedCornerShape(4.dp))
+                    .background(
+                        color = when (icon.color) {
+                            SystemColor.YELLOW -> colorResource(id = R.color.palette_system_yellow)
+                            SystemColor.AMBER -> colorResource(id = R.color.palette_system_amber_100)
+                            SystemColor.RED -> colorResource(id = R.color.palette_system_red)
+                            SystemColor.PINK -> colorResource(id = R.color.palette_system_pink)
+                            SystemColor.PURPLE -> colorResource(id = R.color.palette_system_purple)
+                            SystemColor.BLUE -> colorResource(id = R.color.palette_system_blue)
+                            SystemColor.SKY -> colorResource(id = R.color.palette_system_sky)
+                            SystemColor.TEAL -> colorResource(id = R.color.palette_system_teal)
+                            SystemColor.GREEN -> colorResource(id = R.color.palette_system_green)
+                        },
+                        shape = RoundedCornerShape(radius)
+                    )
+                    .clip(RoundedCornerShape(radius))
+                    .noRippleClickable { onSpaceIconClick.invoke() }
+            ) {
+                Text(
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    ),
+                    text = icon
+                        .name
+                        .ifEmpty { stringResource(id = R.string.u) }
+                        .take(1)
+                        .uppercase(),
+                    style = TextStyle(
+                        fontSize = fontSize,
+                        fontWeight = FontWeight(600),
+                        color = colorResource(id = R.color.text_label_inversion),
+                    )
+                )
+            }
+        }
+        else -> {
+            Box(
+                modifier = modifier
+                    .size(mainSize)
+                    .background(
+                        color = colorResource(id = R.color.palette_system_yellow),
+                        shape = RoundedCornerShape(radius)
+                    )
+                    .clip(RoundedCornerShape(radius))
                     .noRippleClickable { onSpaceIconClick.invoke() }
             )
         }

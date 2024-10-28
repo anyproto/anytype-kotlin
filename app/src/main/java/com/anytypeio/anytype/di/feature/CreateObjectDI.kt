cb652bc270605ac022067a3a79a5b3fda878135a
konstantiniiv <ki@anytype.io>
2024-10-28 12:00:09 +0100

    DROID-2989 fix

--- app/src/main/java/com/anytypeio/anytype/di/feature/CreateObjectDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/CreateObjectDI.kt
@@ -4,10 +4,10 @@ import com.anytypeio.anytype.core_utils.di.scope.PerScreen
 import com.anytypeio.anytype.domain.account.AwaitAccountStartManager
 import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.block.repo.BlockRepository
-import com.anytypeio.anytype.domain.config.ConfigStorage
 import com.anytypeio.anytype.domain.config.UserSettingsRepository
+import com.anytypeio.anytype.domain.debugging.Logger
 import com.anytypeio.anytype.domain.launch.GetDefaultObjectType
-import com.anytypeio.anytype.domain.page.CreateObject
+import com.anytypeio.anytype.domain.page.CreateObjectByTypeAndTemplate
 import com.anytypeio.anytype.domain.templates.GetTemplates
 import com.anytypeio.anytype.domain.workspace.SpaceManager
 import com.anytypeio.anytype.presentation.objects.CreateObjectViewModel
@@ -37,13 +37,12 @@ object CreateObjectModule {
     @PerScreen
     fun getCreateObject(
         repo: BlockRepository,
-        getDefaultObjectType: GetDefaultObjectType,
         dispatchers: AppCoroutineDispatchers,
-        spaceManager: SpaceManager
-    ): CreateObject = CreateObject(
+        logger: Logger
+    ): CreateObjectByTypeAndTemplate = CreateObjectByTypeAndTemplate(
         repo = repo,
-        getDefaultObjectType = getDefaultObjectType,
-        dispatchers = dispatchers
+        dispatchers = dispatchers,
+        logger = logger
     )
 
     @JvmStatic
@@ -52,9 +51,7 @@ object CreateObjectModule {
     fun provideGetDefaultPageType(
         blockRepository: BlockRepository,
         userSettingsRepository: UserSettingsRepository,
-        dispatchers: AppCoroutineDispatchers,
-        spaceManager: SpaceManager,
-        configStorage: ConfigStorage
+        dispatchers: AppCoroutineDispatchers
     ): GetDefaultObjectType = GetDefaultObjectType(
         userSettingsRepository = userSettingsRepository,
         blockRepository = blockRepository,
@@ -79,9 +76,9 @@ object CreateObjectModule {
     @Provides
     @PerScreen
     fun provideViewModelFactory(
-        createObject: CreateObject,
+        createObject: CreateObjectByTypeAndTemplate,
         spaceManager: SpaceManager,
-         awaitAccountStartManager: AwaitAccountStartManager
+        awaitAccountStartManager: AwaitAccountStartManager
     ): CreateObjectViewModel.Factory = CreateObjectViewModel.Factory(
         createObject = createObject,
         spaceManager = spaceManager,
--- presentation/src/main/java/com/anytypeio/anytype/presentation/objects/CreateObjectViewModel.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/objects/CreateObjectViewModel.kt
@@ -3,7 +3,6 @@ package com.anytypeio.anytype.presentation.objects
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.anytypeio.anytype.core_models.Config
 import com.anytypeio.anytype.core_models.Id
 import com.anytypeio.anytype.core_models.Key
 import com.anytypeio.anytype.core_models.ObjectType
@@ -11,8 +10,9 @@ import com.anytypeio.anytype.core_models.primitives.SpaceId
 import com.anytypeio.anytype.core_models.primitives.TypeKey
 import com.anytypeio.anytype.domain.account.AwaitAccountStartManager
 import com.anytypeio.anytype.domain.base.fold
-import com.anytypeio.anytype.domain.page.CreateObject
+import com.anytypeio.anytype.domain.page.CreateObjectByTypeAndTemplate
 import com.anytypeio.anytype.domain.workspace.SpaceManager
+import com.anytypeio.anytype.presentation.search.ObjectSearchConstants
 import javax.inject.Inject
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -23,7 +23,7 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class CreateObjectViewModel(
-    private val createObject: CreateObject,
+    private val createObject: CreateObjectByTypeAndTemplate,
     private val spaceManager: SpaceManager,
     private val awaitAccountStart: AwaitAccountStartManager
 ) : ViewModel() {
@@ -32,6 +32,7 @@ class CreateObjectViewModel(
     private val jobs = mutableListOf<Job>()
 
     fun onStart(type: Key) {
+        Timber.d("CreateObjectViewModel: onStart, type:$type")
         onCreatePage(type)
     }
 
@@ -56,25 +57,34 @@ class CreateObjectViewModel(
         type: Key,
         state: SpaceManager.State
     ) {
+        Timber.d("CreateObjectViewModel: proceedWithObjectCreation, type:$type, state:$state")
         when(state) {
             is SpaceManager.State.Space.Active -> {
-                createObject.execute(
-                    CreateObject.Param(
-                        type = TypeKey(type),
-                        space = SpaceId(state.config.space)
-                    )
-                ).fold(
+                val params = CreateObjectByTypeAndTemplate.Param(
+                    typeKey = TypeKey(type),
+                    space = SpaceId(state.config.space),
+                    keys = ObjectSearchConstants.defaultKeysObjectType
+                )
+                createObject.async(params).fold(
                     onFailure = { e ->
                         Timber.e(e, "Error while creating a new object with type:$type")
+                        createObjectStatus.emit(State.Error("Error while creating a new object"))
                     },
                     onSuccess = { result ->
-                        createObjectStatus.emit(
-                            State.Success(
-                                id = result.objectId,
-                                layout = result.obj.layout,
-                                space = requireNotNull(result.obj.spaceId)
-                            )
-                        )
+                        when (result) {
+                            CreateObjectByTypeAndTemplate.Result.ObjectTypeNotFound -> {
+                                createObjectStatus.emit(State.Error("Object type not found"))
+                            }
+                            is CreateObjectByTypeAndTemplate.Result.Success -> {
+                                createObjectStatus.emit(
+                                    State.Success(
+                                        id = result.objectId,
+                                        layout = result.obj.layout,
+                                        space = requireNotNull(result.obj.spaceId)
+                                    )
+                                )
+                            }
+                        }
                     }
                 )
             }
@@ -91,6 +101,7 @@ class CreateObjectViewModel(
     }
 
     fun onStop() {
+        Timber.d("CreateObjectViewModel: onStop")
         viewModelScope.launch {
             jobs.apply {
                 forEach { it.cancel() }
@@ -106,7 +117,7 @@ class CreateObjectViewModel(
     }
 
     class Factory @Inject constructor(
-        private val createObject: CreateObject,
+        private val createObject: CreateObjectByTypeAndTemplate,
         private val spaceManager: SpaceManager,
         private val awaitAccountStart: AwaitAccountStartManager
     ) : ViewModelProvider.Factory {

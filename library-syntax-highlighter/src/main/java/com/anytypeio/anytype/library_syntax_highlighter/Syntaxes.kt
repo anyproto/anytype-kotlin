585686275b34400ecbd6f592b2873eb86f1acce6
nvgurova <nyud91@gmail.com>
2024-11-19 12:14:33 +0100

    DROID-3003 - Updated logic to set "Plain Text" as the default language for code snippets. - Ensured text color matches desktop implementation

--- library-syntax-highlighter/src/main/java/com/anytypeio/anytype/library_syntax_highlighter/Syntaxes.kt
+++ library-syntax-highlighter/src/main/java/com/anytypeio/anytype/library_syntax_highlighter/Syntaxes.kt
@@ -2,6 +2,6 @@ package com.anytypeio.anytype.library_syntax_highlighter
 
 object Syntaxes {
     const val KOTLIN = "kotlin"
-    const val PYTHON = "python"
     const val GENERIC = "generic"
+    const val PLAIN = "plain"
 }

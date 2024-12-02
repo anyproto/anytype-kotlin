a6d50a321c2cc73888ef6c71905df8d08785c52a
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2024-12-02 12:28:22 +0100

    DROID-3118 fix

--- app/src/main/java/com/anytypeio/anytype/ui/widgets/types/DataViewWidget.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/widgets/types/DataViewWidget.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -309,11 +310,13 @@ fun GalleryWidgetCard(
                                         }
                                 ) {
                                     Text(
-                                        text = stringResource(id = R.string.widget_view_see_all_objects),
+                                        text = "Показать все объекты",
                                         style = Caption1Medium,
                                         color = colorResource(id = R.color.glyph_active),
                                         modifier = Modifier
                                             .align(Alignment.Center)
+                                            .padding(horizontal = 12.dp),
+                                        textAlign = TextAlign.Center
                                     )
                                 }
                             }

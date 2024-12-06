7a63519a1fc2ce6bfd89f560af568f123f046a8c
konstantiniiv <ki@anytype.io>
2024-12-06 14:10:56 +0100

    DROID-3126 new header ui

--- feature-date/src/main/java/com/anytypeio/anytype/feature_date/ui/HeaderScreen.kt
+++ feature-date/src/main/java/com/anytypeio/anytype/feature_date/ui/HeaderScreen.kt
@@ -2,11 +2,14 @@ package com.anytypeio.anytype.feature_date.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,13 +21,18 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.anytypeio.anytype.core_models.DayOfWeekCustom
+import com.anytypeio.anytype.core_models.RelativeDate
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
 import com.anytypeio.anytype.core_ui.common.ShimmerEffect
+import com.anytypeio.anytype.core_ui.extensions.getLocalizedDayName
+import com.anytypeio.anytype.core_ui.extensions.getPrettyName
 import com.anytypeio.anytype.core_ui.foundation.noRippleThrottledClickable
 import com.anytypeio.anytype.core_ui.views.HeadlineTitle
+import com.anytypeio.anytype.core_ui.views.Relations2
 import com.anytypeio.anytype.feature_date.R
-import com.anytypeio.anytype.feature_date.viewmodel.UiHeaderState
 import com.anytypeio.anytype.feature_date.ui.models.DateEvent
+import com.anytypeio.anytype.feature_date.viewmodel.UiHeaderState
 
 @Composable
 fun HeaderScreen(
@@ -32,80 +40,142 @@ fun HeaderScreen(
     uiState: UiHeaderState,
     onDateEvent: (DateEvent) -> Unit
 ) {
-    Row(
-        modifier = modifier
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
+        RelativeDateAndDayOfWeek(
             modifier = Modifier
-                .height(48.dp)
-                .width(52.dp)
-                .rotate(180f)
-                .noRippleThrottledClickable {
-                    onDateEvent(DateEvent.Header.OnPreviousClick)
-                },
-            contentDescription = "Previous day",
-            painter = painterResource(id = R.drawable.ic_arrow_disclosure_18),
-            contentScale = ContentScale.None
+                .wrapContentSize(),
+            uiState = uiState
         )
-        when (uiState) {
-            is UiHeaderState.Content -> {
-                Text(
-                    textAlign = TextAlign.Center,
-                    text = uiState.title,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .align(Alignment.CenterVertically),
-                    style = HeadlineTitle,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = colorResource(id = R.color.text_primary)
-                )
-            }
-
-            UiHeaderState.Loading -> {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .align(Alignment.CenterVertically),
-                    contentAlignment = Alignment.Center
-                ) {
-                    ShimmerEffect(
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(32.dp)
+                    .width(52.dp)
+                    .rotate(180f)
+                    .noRippleThrottledClickable {
+                        onDateEvent(DateEvent.Header.OnPreviousClick)
+                    },
+                contentDescription = "Previous day",
+                painter = painterResource(id = R.drawable.ic_arrow_disclosure_18),
+                contentScale = ContentScale.None
+            )
+            when (uiState) {
+                is UiHeaderState.Content -> {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = uiState.title,
                         modifier = Modifier
-                            .width(200.dp)
-                            .height(30.dp)
+                            .wrapContentHeight()
+                            .fillMaxWidth()
+                            .weight(1f),
+                        style = HeadlineTitle,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = colorResource(id = R.color.text_primary)
                     )
                 }
-            }
 
-            UiHeaderState.Empty -> {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .align(Alignment.CenterVertically),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Spacer(
+                UiHeaderState.Loading -> {
+                    Box(
                         modifier = Modifier
-                            .width(200.dp)
-                            .height(30.dp)
-                    )
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .align(Alignment.CenterVertically),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        ShimmerEffect(
+                            modifier = Modifier
+                                .width(200.dp)
+                                .height(30.dp)
+                        )
+                    }
+                }
+
+                UiHeaderState.Empty -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .align(Alignment.CenterVertically),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Spacer(
+                            modifier = Modifier
+                                .width(200.dp)
+                                .height(30.dp)
+                        )
+                    }
                 }
             }
+            Image(
+                modifier = Modifier
+                    .height(32.dp)
+                    .width(52.dp)
+                    .noRippleThrottledClickable {
+                        onDateEvent(DateEvent.Header.OnNextClick)
+                    },
+                contentDescription = "Next day",
+                painter = painterResource(id = R.drawable.ic_arrow_disclosure_18),
+                contentScale = ContentScale.None
+            )
         }
-        Image(
-            modifier = Modifier
-                .height(48.dp)
-                .width(52.dp)
-                .noRippleThrottledClickable {
-                    onDateEvent(DateEvent.Header.OnNextClick)
-                },
-            contentDescription = "Next day",
-            painter = painterResource(id = R.drawable.ic_arrow_disclosure_18),
-            contentScale = ContentScale.None
-        )
+    }
+}
+
+@Composable
+fun RelativeDateAndDayOfWeek(
+    modifier: Modifier,
+    uiState: UiHeaderState
+) {
+    Row(modifier = modifier) {
+        if (uiState is UiHeaderState.Content) {
+            when (val relativeDate = uiState.relativeDate) {
+                is RelativeDate.Today -> {
+                    Text(
+                        modifier = Modifier.wrapContentSize(),
+                        text = "${relativeDate.getPrettyName()} \u2022 ",
+                        style = Relations2,
+                        color = colorResource(id = R.color.text_secondary)
+                    )
+                }
+
+                is RelativeDate.Tomorrow -> {
+                    Text(
+                        modifier = Modifier.wrapContentSize(),
+                        text = "${relativeDate.getPrettyName()} \u2022 ",
+                        style = Relations2,
+                        color = colorResource(id = R.color.text_secondary)
+                    )
+                }
+
+                is RelativeDate.Yesterday -> {
+                    Text(
+                        modifier = Modifier.wrapContentSize(),
+                        text = "${relativeDate.getPrettyName()} \u2022 ",
+                        style = Relations2,
+                        color = colorResource(id = R.color.text_secondary)
+                    )
+                }
+
+                else -> {}
+            }
+            val dayOfWeek = getLocalizedDayName(uiState.relativeDate.dayOfWeek)
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                text = dayOfWeek,
+                style = Relations2,
+                color = colorResource(id = R.color.text_secondary)
+            )
+        }
     }
 }
 
@@ -115,8 +185,7 @@ fun DateLayoutHeaderEmptyPreview() {
     val state = UiHeaderState.Empty
     HeaderScreen(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(48.dp), uiState = state
+            .fillMaxWidth(), uiState = state
     ) {}
 }
 
@@ -126,18 +195,24 @@ fun DateLayoutHeaderLoadingPreview() {
     val state = UiHeaderState.Loading
     HeaderScreen(
         Modifier
-            .fillMaxWidth()
-            .height(48.dp), state
+            .fillMaxWidth(), state
     ) {}
 }
 
 @Composable
 @DefaultPreviews
 fun DateLayoutHeaderPreview() {
-    val state = UiHeaderState.Content("Tue, 12 Oct")
+    val state = UiHeaderState.Content(
+        title = "Tue, 12 Oct",
+        relativeDate = RelativeDate.Other(
+            initialTimeInMillis = 1634016000000,
+            dayOfWeek = DayOfWeekCustom.MONDAY,
+            formattedDate = "Tue, 12 Oct",
+            formattedTime = "12:00 AM"
+        )
+    )
     HeaderScreen(
         Modifier
-            .fillMaxWidth()
-            .height(48.dp), state
+            .fillMaxWidth(), state
     ) {}
 }

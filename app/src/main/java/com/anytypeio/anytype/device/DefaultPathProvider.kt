fecbea1f87a7e116543ec670873a85d1df883834
konstantiniiv <ki@anytype.io>
2024-12-19 22:04:30 +0100

    DROID-3164 fix

--- app/src/main/java/com/anytypeio/anytype/device/DefaultPathProvider.kt
+++ app/src/main/java/com/anytypeio/anytype/device/DefaultPathProvider.kt
@@ -1,10 +1,16 @@
 package com.anytypeio.anytype.device
 
 import android.content.Context
+import android.os.Environment
 import com.anytypeio.anytype.domain.device.PathProvider
 
 class DefaultPathProvider(
     private val context: Context
 ) : PathProvider {
     override fun providePath(): String = context.filesDir.path
+
+    override fun provideSharedPath(): String {
+        val textDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        return textDir?.path ?: ""
+    }
 }

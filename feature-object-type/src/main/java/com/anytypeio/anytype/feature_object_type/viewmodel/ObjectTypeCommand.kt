e81914c4d44b446112a69e9f42d4ff84a5846a80
konstantiniiv <ki@anytype.io>
2024-12-19 13:49:34 +0100

    DROID-3013 new screen

--- feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/viewmodel/ObjectTypeCommand.kt
+++ feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/viewmodel/ObjectTypeCommand.kt
@@ -1,4 +1,12 @@
 package com.anytypeio.anytype.feature_object_type.viewmodel
 
-class ObjectTypeCommand {
+
+sealed class ObjectTypeCommand {
+
+    sealed class SendToast : ObjectTypeCommand() {
+        data class Error(val message: String) : SendToast()
+        data class UnexpectedLayout(val layout: String) : SendToast()
+    }
+
+    data object Back : ObjectTypeCommand()
 }

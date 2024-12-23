8c1f6a0ba77a15836596df8735d6f1f169c16a18
konstantiniiv <ki@anytype.io>
2024-12-23 18:07:14 +0100

    DROID-2898 test

--- presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/file_layout/FileLayoutTest.kt
+++ presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/file_layout/FileLayoutTest.kt
@@ -118,7 +118,7 @@ class FileLayoutTest : EditorPresentationTestSetup() {
         val firstTimeExpected = ViewState.Success(
             listOf(
                 BlockView.Title.File(
-                    mode = BlockView.Mode.EDIT,
+                    mode = BlockView.Mode.READ, //in this case for a File Object title is always locked!
                     isFocused = false,
                     id = title.id,
                     text = title.content<TXT>().text,
@@ -189,7 +189,7 @@ class FileLayoutTest : EditorPresentationTestSetup() {
                     isFocused = false,
                     id = title.id,
                     text = title.content<TXT>().text,
-                    mode = BlockView.Mode.READ //for Image Object title are always locked!
+                    mode = BlockView.Mode.READ //in this case for a Image Object title is always locked!
                 ),
                 BlockView.ButtonOpenFile.ImageButton(
                     id = fileBlock.id,

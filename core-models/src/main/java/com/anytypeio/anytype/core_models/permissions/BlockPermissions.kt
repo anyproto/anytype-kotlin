95b3499bf2767de066eacb491bce44336052aa41
konstantiniiv <ki@anytype.io>
2025-01-13 11:30:53 +0100

    DROID-2192 legacy

--- core-models/src/main/java/com/anytypeio/anytype/core_models/permissions/BlockPermissions.kt
+++ core-models/src/main/java/com/anytypeio/anytype/core_models/permissions/BlockPermissions.kt
@@ -1,12 +1,5 @@
 package com.anytypeio.anytype.core_models.permissions
 
-sealed class BlocksReadonlyReason {
-    object Locked : BlocksReadonlyReason()
-    object Archived : BlocksReadonlyReason()
-    object SpaceIsReadonly : BlocksReadonlyReason()
-    object Restrictions : BlocksReadonlyReason()
-}
-
 sealed class EditBlocksPermission {
     data object Edit : EditBlocksPermission()
     data object ReadOnly: EditBlocksPermission()

7e2dcdd24ac2501a696a39f7b5eb12be6f32df64
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-01-20 19:49:01 +0100

    DROID-3224 fixes

--- domain/src/main/java/com/anytypeio/anytype/domain/auth/interactor/CancelAccountMigration.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/auth/interactor/CancelAccountMigration.kt
@@ -1,4 +1,34 @@
 package com.anytypeio.anytype.domain.auth.interactor
 
-class CancelAccountMigration {
+import com.anytypeio.anytype.core_models.Id
+import com.anytypeio.anytype.domain.auth.repo.AuthRepository
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.base.ResultInteractor
+import javax.inject.Inject
+
+class CancelAccountMigration @Inject constructor(
+    private val repo: AuthRepository,
+    dispatchers: AppCoroutineDispatchers
+) : ResultInteractor<CancelAccountMigration.Params, Unit>(dispatchers.io) {
+
+    override suspend fun doWork(params: Params) {
+        when(params) {
+            is Params.Current -> {
+                val acc = repo.getCurrentAccount()
+                repo.cancelAccountMigration(
+                    account = acc.id
+                )
+            }
+            is Params.Other -> {
+                repo.cancelAccountMigration(
+                    account = params.acc
+                )
+            }
+        }
+    }
+
+    sealed class Params {
+        data object Current : Params()
+        data class Other(val acc: Id) : Params()
+    }
 }

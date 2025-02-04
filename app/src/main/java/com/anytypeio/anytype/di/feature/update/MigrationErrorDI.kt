6bdb102a58ef49b8217472adddb4373b38792a6e
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-02-04 22:10:42 +0100

    DROID-3317 fixes

--- app/src/main/java/com/anytypeio/anytype/di/feature/update/MigrationErrorDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/update/MigrationErrorDI.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.anytypeio.anytype.analytics.base.Analytics
 import com.anytypeio.anytype.core_utils.di.scope.PerScreen
 import com.anytypeio.anytype.di.common.ComponentDependencies
+import com.anytypeio.anytype.domain.auth.repo.AuthRepository
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.device.PathProvider
+import com.anytypeio.anytype.presentation.auth.account.MigrationHelperDelegate
 import com.anytypeio.anytype.presentation.update.MigrationErrorViewModel
 import com.anytypeio.anytype.ui.update.MigrationErrorFragment
 import dagger.Binds
@@ -40,9 +44,17 @@ object MigrationErrorModule {
             factory: MigrationErrorViewModel.Factory
         ): ViewModelProvider.Factory
 
+        @Binds
+        @PerScreen
+        fun bindMigrationHelperDelegate(
+            impl: MigrationHelperDelegate.Impl
+        ): MigrationHelperDelegate
     }
 }
 
 interface MigrationErrorDependencies : ComponentDependencies {
     fun analytics(): Analytics
+    fun auth(): AuthRepository
+    fun path(): PathProvider
+    fun dispatcher(): AppCoroutineDispatchers
 }

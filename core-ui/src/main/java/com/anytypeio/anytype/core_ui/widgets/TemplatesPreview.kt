d5eb7830138455b903b9c934735940c2884555b2
konstantiniiv <ki@anytype.io>
2025-02-27 14:54:33 +0100

    DROID-3419 templates ui

---
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/TemplatesPreview.kt
@@ -0,0 +1,73 @@
+package com.anytypeio.anytype.core_ui.widgets
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.anytypeio.anytype.core_models.ObjectType
+import com.anytypeio.anytype.core_models.ObjectWrapper
+import com.anytypeio.anytype.core_models.Relations
+import com.anytypeio.anytype.core_models.primitives.TypeId
+import com.anytypeio.anytype.core_models.primitives.TypeKey
+import com.anytypeio.anytype.core_ui.common.DefaultPreviews
+import com.anytypeio.anytype.presentation.editor.cover.CoverColor
+import com.anytypeio.anytype.presentation.templates.TemplateObjectTypeView
+import com.anytypeio.anytype.presentation.templates.TemplateView
+import com.anytypeio.anytype.presentation.templates.TemplateView.Companion.DEFAULT_TEMPLATE_ID_BLANK
+import com.anytypeio.anytype.presentation.widgets.TypeTemplatesWidgetUI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+@DefaultPreviews
+@Composable
+fun ComposablePreview() {
+    val items = listOf(
+        TemplateView.Blank(
+            id = DEFAULT_TEMPLATE_ID_BLANK,
+            targetTypeId = TypeId("page"),
+            targetTypeKey = TypeKey("ot-page"),
+            typeName = "Page",
+            layout = ObjectType.Layout.BASIC.code
+        ),
+        TemplateView.Template(
+            id = "1",
+            name = "Template Title",
+            targetTypeId = TypeId("page"),
+            targetTypeKey = TypeKey("ot-page"),
+            layout = ObjectType.Layout.PROFILE,
+            image = null,
+            emoji = "📄",
+            coverColor = CoverColor.RED,
+            coverGradient = null,
+            coverImage = null,
+            isDefault = true
+        ),
+    )
+    val state = TypeTemplatesWidgetUI.Data(
+        templates = items,
+        showWidget = true,
+        isEditing = true,
+        moreMenuItem = null,
+        objectTypes = listOf(
+            TemplateObjectTypeView.Search,
+            TemplateObjectTypeView.Item(
+                type = ObjectWrapper.Type(
+                    map = mapOf(Relations.ID to "123", Relations.NAME to "Page"),
+                )
+            )
+        ),
+        viewerId = "",
+        isPossibleToChangeType = true,
+        isPossibleToChangeTemplate = false
+    )
+    TypeTemplatesWidget(
+        state = state,
+        onDismiss = {},
+        editClick = {},
+        doneClick = {},
+        moreClick = {},
+        scope = CoroutineScope(
+            Dispatchers.Main
+        ),
+        menuClick = {},
+        action = {}
+    )
+}

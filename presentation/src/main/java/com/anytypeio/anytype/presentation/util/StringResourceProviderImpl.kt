264ce62323fc98155a82b1e237d5d64a50732d61
konstantiniiv <ki@anytype.io>
2025-03-04 12:41:26 +0100

    DROID-3429 string provider

--- presentation/src/main/java/com/anytypeio/anytype/presentation/util/StringResourceProviderImpl.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/util/StringResourceProviderImpl.kt
@@ -1,6 +1,8 @@
 package com.anytypeio.anytype.presentation.util
 
 import android.content.Context
+import com.anytypeio.anytype.core_models.Relation
+import com.anytypeio.anytype.core_models.RelationFormat
 import com.anytypeio.anytype.core_models.RelativeDate
 import com.anytypeio.anytype.domain.resources.StringResourceProvider
 import com.anytypeio.anytype.presentation.R
@@ -30,5 +32,25 @@ class StringResourceProviderImpl @Inject constructor(private val context: Contex
 
     override fun getSetOfObjectsTitle(): String {
         return context.getString(R.string.object_set_of_title)
+    }
+
+    override fun getPropertiesFormatPrettyString(format: RelationFormat): String {
+        return when (format) {
+            RelationFormat.LONG_TEXT -> context.getString(R.string.relation_format_long_text)
+            RelationFormat.SHORT_TEXT -> context.getString(R.string.relation_format_short_text)
+            RelationFormat.NUMBER -> context.getString(R.string.relation_format_number)
+            RelationFormat.STATUS -> context.getString(R.string.relation_format_status)
+            RelationFormat.TAG -> context.getString(R.string.relation_format_tag)
+            RelationFormat.DATE -> context.getString(R.string.relation_format_date)
+            RelationFormat.FILE -> context.getString(R.string.relation_format_file)
+            RelationFormat.CHECKBOX -> context.getString(R.string.relation_format_checkbox)
+            RelationFormat.URL -> context.getString(R.string.relation_format_url)
+            RelationFormat.EMAIL -> context.getString(R.string.relation_format_email)
+            RelationFormat.PHONE -> context.getString(R.string.relation_format_phone)
+            RelationFormat.EMOJI -> context.getString(R.string.relation_format_emoji)
+            RelationFormat.OBJECT -> context.getString(R.string.relation_format_object)
+            RelationFormat.RELATIONS -> context.getString(R.string.relation_format_relation)
+            RelationFormat.UNDEFINED -> context.getString(R.string.undefined)
+        }
     }
 }

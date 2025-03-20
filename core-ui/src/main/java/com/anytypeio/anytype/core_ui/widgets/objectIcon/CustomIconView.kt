4e23fe8d8e6edf98d55b461e77efa665a9467892
konstantiniiv <ki@anytype.io>
2025-03-20 10:57:23 +0100

    DROID-3367 fix

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/CustomIconView.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/CustomIconView.kt
@@ -1,12 +1,9 @@
 package com.anytypeio.anytype.core_ui.widgets.objectIcon
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
@@ -20,43 +17,28 @@ import com.anytypeio.anytype.presentation.objects.ObjectIcon
 import com.anytypeio.anytype.presentation.objects.custom_icon.CustomIcon
 import com.anytypeio.anytype.presentation.objects.custom_icon.CustomIconColor
 import com.anytypeio.anytype.core_ui.R
-import com.anytypeio.anytype.core_ui.widgets.cornerRadius
 
 @Composable
 fun CustomIconView(
     modifier: Modifier = Modifier,
     icon: ObjectIcon.ObjectType,
-    backgroundSize: Dp,
-    iconWithoutBackgroundMaxSize: Dp = 20.dp,
-    imageMultiplier: Float = 0.625f,
+    iconSize: Dp
 ) {
     val tint = colorResource(id = icon.icon.color.colorRes())
 
     val imageVector = CustomIcons.getImageVector(icon.icon)
 
-    val (containerModifier, iconModifier) = if (backgroundSize > iconWithoutBackgroundMaxSize) {
-        modifier.size(backgroundSize) to Modifier.size(
-            width = backgroundSize * imageMultiplier,
-            height = backgroundSize * imageMultiplier
-        )
-    } else {
-        modifier.size(backgroundSize) to Modifier
-    }
-
-    Box(
-        modifier = containerModifier,
-        contentAlignment = Alignment.Center,
-    ) {
+    Box(modifier = modifier) {
         if (imageVector != null) {
             Image(
-                modifier = iconModifier,
+                modifier = Modifier.size(iconSize),
                 imageVector = imageVector,
                 contentDescription = "Object Type icon",
                 colorFilter = ColorFilter.tint(tint),
             )
         } else {
             Image(
-                modifier = iconModifier,
+                modifier = Modifier.size(iconSize),
                 painter = painterResource(id = R.drawable.ic_empty_state_page),
                 contentDescription = "Object Type icon",
                 colorFilter = ColorFilter.tint(tint),
@@ -76,6 +58,6 @@ fun CustomIconViewPreview() {
             ),
         ),
         modifier = Modifier,
-        backgroundSize = 18.dp
+        iconSize = 18.dp
     )
 }

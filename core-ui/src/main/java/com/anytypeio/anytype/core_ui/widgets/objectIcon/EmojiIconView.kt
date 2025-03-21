c8e69e2a351710bd1166ac3849ed61601e01426a
konstantiniiv <ki@anytype.io>
2025-03-21 15:21:39 +0100

    DROID-3367 emoji icon view

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/EmojiIconView.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/EmojiIconView.kt
@@ -3,22 +3,26 @@ package com.anytypeio.anytype.core_ui.widgets.objectIcon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
+import com.anytypeio.anytype.core_ui.extensions.colorRes
 import com.anytypeio.anytype.core_ui.widgets.cornerRadius
-import com.anytypeio.anytype.core_ui.widgets.imageAsset
+import com.anytypeio.anytype.core_ui.widgets.objectIcon.custom_icons.CustomIcons
 import com.anytypeio.anytype.emojifier.Emojifier
 import com.anytypeio.anytype.presentation.objects.ObjectIcon
+import com.anytypeio.anytype.presentation.objects.custom_icon.CustomIconColor
 
 @Composable
 fun EmojiIconView(
@@ -56,13 +60,32 @@ fun EmojiIconView(
                 modifier = iconModifier
             )
         } else {
-            val imageAsset = imageAsset(icon.emptyState)
-            Image(
-                painter = painterResource(id = imageAsset),
-                contentDescription = "Empty Object Icon",
-                modifier = iconModifier
-            )
+            val fallback = icon.fallback
 
+            val tint = colorResource(id = fallback.color.colorRes())
+
+            val imageVector = CustomIcons.getImageVector(fallback.rawValue)
+            val fallbackIcon = CustomIcons.getImageVector(fallback.fallback)
+
+            Box(modifier = Modifier) {
+                if (imageVector != null) {
+                    Image(
+                        modifier = iconModifier,
+                        imageVector = imageVector,
+                        contentDescription = "Object Type icon",
+                        colorFilter = ColorFilter.tint(tint),
+                    )
+                } else {
+                    if (fallbackIcon != null) {
+                        Image(
+                            modifier = iconModifier,
+                            imageVector = fallbackIcon,
+                            contentDescription = "Object Type icon",
+                            colorFilter = ColorFilter.tint(tint),
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -70,8 +93,35 @@ fun EmojiIconView(
 @DefaultPreviews
 @Composable
 fun Emoji20ObjectIconViewPreview() {
-    EmojiIconView(
-        icon = ObjectIcon.Basic.Emoji("😀", ObjectIcon.Empty.Page),
-        backgroundSize = 20.dp
-    )
+    Column {
+        Text("Proper emoji case:")
+        EmojiIconView(
+            icon = ObjectIcon.Basic.Emoji(
+                "😀"
+            ),
+            backgroundSize = 20.dp
+        )
+        Text("Fallback on type icon:")
+        EmojiIconView(
+            icon = ObjectIcon.Basic.Emoji(
+                "😀1111",
+                fallback = ObjectIcon.TypeIcon.Default(
+                    rawValue = CustomIcons.iconsMap.entries.first().key,
+                    color = CustomIconColor.Teal
+                )
+            ),
+            backgroundSize = 20.dp
+        )
+        Text("Fallback on default icon in case of invalid type icon:")
+        EmojiIconView(
+            icon = ObjectIcon.Basic.Emoji(
+                "😀1111",
+                fallback = ObjectIcon.TypeIcon.Default(
+                    rawValue = "no-icon-like-that",
+                    color = CustomIconColor.DEFAULT
+                )
+            ),
+            backgroundSize = 20.dp
+        )
+    }
 }

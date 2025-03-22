cc84221a96c3ff9a3d62bde123d2e62a835d9bb5
konstantiniiv <ki@anytype.io>
2025-03-22 13:38:28 +0100

    DROID-3367 profile icons compose

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/AvatarIconView.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/widgets/objectIcon/AvatarIconView.kt
@@ -1,5 +1,6 @@
 package com.anytypeio.anytype.core_ui.widgets.objectIcon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -7,13 +8,19 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
 import com.anytypeio.anytype.core_ui.views.AvatarTitle
@@ -55,6 +62,62 @@ fun AvatarIconView(
             fontSize = fontSize.sp,
             color = colorResource(id = avatarTextColor)
         )
+    }
+}
+
+@Composable
+fun AvatarIconLoadingView(
+    modifier: Modifier,
+    iconSize: Dp
+) {
+    val (radius, fontSize) = getAvatarIconParams(iconSize)
+
+    Box(
+        modifier = modifier
+            .size(iconSize)
+            .background(
+                shape = CircleShape,
+                color = colorResource(id = avatarBackgroundColor)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+    }
+}
+
+@Composable
+fun DefaultProfileIconImage(
+    icon: ObjectIcon.Profile.Image,
+    modifier: Modifier,
+    iconSize: Dp
+) {
+    val painter = rememberAsyncImagePainter(icon.hash)
+    val state by painter.state.collectAsState()
+
+    when (state) {
+        AsyncImagePainter.State.Empty,
+        is AsyncImagePainter.State.Loading -> {
+            AvatarIconLoadingView(
+                modifier = Modifier.size(iconSize),
+                iconSize = iconSize
+            )
+        }
+        is AsyncImagePainter.State.Error -> {
+            AvatarIconView(
+                modifier = modifier,
+                iconSize = iconSize,
+                icon = ObjectIcon.Profile.Avatar(name = icon.name)
+            )
+        }
+        is AsyncImagePainter.State.Success -> {
+            Image(
+                painter = painter,
+                contentDescription = "Icon from URI",
+                modifier = modifier
+                    .size(iconSize)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop,
+            )
+        }
     }
 }
 

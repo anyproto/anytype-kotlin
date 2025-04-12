97b043604169b652795609b2e37aca23290a9155
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-04-12 15:12:48 +0200

    DROID-3264 fix

--- presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/file_layout/FileLayoutTest.kt
+++ presentation/src/test/java/com/anytypeio/anytype/presentation/editor/editor/file_layout/FileLayoutTest.kt
@@ -184,11 +184,12 @@ class FileLayoutTest : EditorPresentationTestSetup() {
 
         val firstTimeExpected = ViewState.Success(
             listOf(
-                BlockView.Title.Basic(
+                BlockView.Title.Image(
                     isFocused = false,
                     id = title.id,
-                    text = "${fileObject.name}.$fileExt",
-                    mode = BlockView.Mode.READ //in this case for a Image Object title is always locked!
+                    text = title.content.asText().text,
+                    mode = BlockView.Mode.READ,
+                    icon = ObjectIcon.None
                 ),
                 BlockView.ButtonOpenFile.ImageButton(
                     id = fileBlock.id,

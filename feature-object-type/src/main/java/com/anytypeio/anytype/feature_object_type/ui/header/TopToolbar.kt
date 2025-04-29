105f058928fb025808857e6be79530cbec27bba1
konstantiniiv <ki@anytype.io>
2025-04-29 15:46:02 +0200

    DROID-3618 toolbar fixes

--- feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/ui/header/TopToolbar.kt
+++ feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/ui/header/TopToolbar.kt
@@ -1,21 +1,13 @@
 package com.anytypeio.anytype.feature_object_type.ui.header
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -25,11 +17,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.anytypeio.anytype.core_models.multiplayer.P2PStatusUpdate
 import com.anytypeio.anytype.core_models.multiplayer.SpaceSyncAndP2PStatusState
@@ -40,8 +29,6 @@ import com.anytypeio.anytype.core_models.multiplayer.SpaceSyncUpdate
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
 import com.anytypeio.anytype.core_ui.foundation.noRippleThrottledClickable
 import com.anytypeio.anytype.core_ui.syncstatus.StatusBadge
-import com.anytypeio.anytype.core_ui.views.BodyRegular
-import com.anytypeio.anytype.core_ui.views.PreviewTitle2Regular
 import com.anytypeio.anytype.feature_object_type.R
 import com.anytypeio.anytype.feature_object_type.ui.TypeEvent
 import com.anytypeio.anytype.feature_object_type.ui.UiEditButton
@@ -63,55 +50,35 @@ fun TopToolbar(
         mutableStateOf(false)
     }
 
-    CenterAlignedTopAppBar(
-        modifier = modifier.fillMaxWidth()
-            .padding(horizontal = 0.dp),
-        expandedHeight = 48.dp,
-        scrollBehavior = topBarScrollBehavior,
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = colorResource(id = R.color.background_primary),
-            scrolledContainerColor = colorResource(id = R.color.background_primary),
-            titleContentColor = colorResource(id = R.color.palette_system_red)
-        ),
-        title = {
-            if (topBarScrollBehavior.state.overlappedFraction > 0.7f) {
-                Box(
-                    modifier = Modifier.fillMaxHeight(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = uiTitleState.title,
-                        style = PreviewTitle2Regular,
-                        color = colorResource(R.color.text_primary),
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
-        },
-        navigationIcon = {
-            Box(
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .width(56.dp)
+                .fillMaxHeight()
+                .noRippleThrottledClickable {
+                    onTypeEvent(TypeEvent.OnBackClick)
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
                 modifier = Modifier
-                    .width(56.dp)
-                    .height(48.dp)
-                    .noRippleThrottledClickable {
-                        onTypeEvent(TypeEvent.OnBackClick)
-                    },
-                contentAlignment = Alignment.CenterStart
-            ) {
-                Image(
-                    modifier = Modifier
-                        .padding(start = 12.dp)
-                        .wrapContentSize(),
-                    painter = painterResource(R.drawable.ic_default_top_back),
-                    contentDescription = stringResource(R.string.content_desc_back_button)
-                )
-            }
-        },
-        actions = {
+                    .wrapContentSize(),
+                painter = painterResource(R.drawable.ic_default_top_back),
+                contentDescription = stringResource(R.string.content_desc_back_button)
+            )
+        }
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
             if (uiSyncStatusBadgeState is UiSyncStatusBadgeState.Visible) {
                 Box(
                     modifier = Modifier
-                        .size(48.dp)
+                        .height(48.dp)
+                        .width(56.dp)
                         .noRippleThrottledClickable {
                             onTypeEvent(
                                 TypeEvent.OnSyncStatusClick(
@@ -127,52 +94,52 @@ fun TopToolbar(
                     )
                 }
             }
-            if (uiEditButtonState is UiEditButton.Visible) {
-                IconButton(
-                    modifier = Modifier
-                        .size(48.dp),
-                    onClick = {
-                        isIconMenuExpanded.value = !isIconMenuExpanded.value
-                    }
-                ) {
-                    Image(
-                        modifier = Modifier.size(24.dp),
-                        painter = painterResource(id = R.drawable.ic_space_list_dots),
-                        contentDescription = "More options"
-                    )
-                    DropdownMenu(
-                        modifier = Modifier
-                            .width(244.dp),
-                        expanded = isIconMenuExpanded.value,
-                        offset = DpOffset(x = 0.dp, y = 0.dp),
-                        onDismissRequest = {
-                            isIconMenuExpanded.value = false
-                        },
-                        shape = RoundedCornerShape(10.dp),
-                        containerColor = colorResource(id = R.color.background_secondary),
-                        border = BorderStroke(
-                            width = 0.5.dp,
-                            color = colorResource(id = R.color.background_secondary)
-                        )
-                    ) {
-                        DropdownMenuItem(
-                            text = {
-                                Text(
-                                    text = stringResource(R.string.object_type_settings_item_remove),
-                                    style = BodyRegular,
-                                    color = colorResource(id = R.color.palette_system_red)
-                                )
-                            },
-                            onClick = {
-                                onTypeEvent(TypeEvent.OnMenuItemDeleteClick)
-                                isIconMenuExpanded.value = false
-                            },
-                        )
-                    }
-                }
-            }
+//            if (uiEditButtonState is UiEditButton.Visible) {
+//                IconButton(
+//                    modifier = Modifier
+//                        .size(48.dp),
+//                    onClick = {
+//                        isIconMenuExpanded.value = !isIconMenuExpanded.value
+//                    }
+//                ) {
+//                    Image(
+//                        modifier = Modifier.size(24.dp),
+//                        painter = painterResource(id = R.drawable.ic_space_list_dots),
+//                        contentDescription = "More options"
+//                    )
+//                    DropdownMenu(
+//                        modifier = Modifier
+//                            .width(244.dp),
+//                        expanded = isIconMenuExpanded.value,
+//                        offset = DpOffset(x = 0.dp, y = 0.dp),
+//                        onDismissRequest = {
+//                            isIconMenuExpanded.value = false
+//                        },
+//                        shape = RoundedCornerShape(10.dp),
+//                        containerColor = colorResource(id = R.color.background_secondary),
+//                        border = BorderStroke(
+//                            width = 0.5.dp,
+//                            color = colorResource(id = R.color.background_secondary)
+//                        )
+//                    ) {
+//                        DropdownMenuItem(
+//                            text = {
+//                                Text(
+//                                    text = stringResource(R.string.object_type_settings_item_remove),
+//                                    style = BodyRegular,
+//                                    color = colorResource(id = R.color.palette_system_red)
+//                                )
+//                            },
+//                            onClick = {
+//                                onTypeEvent(TypeEvent.OnMenuItemDeleteClick)
+//                                isIconMenuExpanded.value = false
+//                            },
+//                        )
+//                    }
+//                }
+//            }
         }
-    )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)

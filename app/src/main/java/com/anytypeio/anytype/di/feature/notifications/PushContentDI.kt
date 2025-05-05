56561ba957dfdfdd6642a9d344eef2ad741e4914
konstantiniiv <ki@anytype.io>
2025-05-05 14:25:48 +0200

    DROID-3628 fix

--- app/src/main/java/com/anytypeio/anytype/di/feature/notifications/PushContentDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/notifications/PushContentDI.kt
@@ -2,11 +2,15 @@ package com.anytypeio.anytype.di.feature.notifications
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.anytypeio.anytype.data.notifications.PushNotificationRepositoryImpl
 import com.anytypeio.anytype.device.notifications.AnytypeFirebaseMessagingService
 import com.anytypeio.anytype.device.notifications.DecryptionPushContentService
 import com.anytypeio.anytype.device.notifications.DecryptionPushContentServiceProtocol
+import com.anytypeio.anytype.device.notifications.PushNotificationService
 import com.anytypeio.anytype.di.common.ComponentDependencies
 import com.anytypeio.anytype.domain.debugging.Logger
+import com.anytypeio.anytype.domain.notifications.PushNotificationRepository
+import dagger.Binds
 import dagger.Component
 import dagger.Module
 import dagger.Provides
@@ -34,6 +38,14 @@ object PushContentModule {
 
     @Singleton
     @Provides
+    fun providePushNotificationService(
+        repository: PushNotificationRepository
+    ): PushNotificationService {
+        return PushNotificationService(repository)
+    }
+
+    @Singleton
+    @Provides
     fun provideDecryptionPushContentService(
         @Named("encrypted") sharedPreferences: SharedPreferences
     ): DecryptionPushContentServiceProtocol {
@@ -44,6 +56,10 @@ object PushContentModule {
 
     @Module
     interface Declarations {
+        @Binds
+        fun bindPushNotificationRepository(
+            impl: PushNotificationRepositoryImpl
+        ): PushNotificationRepository
 
     }
 }

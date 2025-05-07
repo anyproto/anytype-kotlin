dec00e8d0fcffb956a04603eb7dddb12b8bb187e
konstantiniiv <ki@anytype.io>
2025-05-07 15:36:38 +0200

    DROID-3636 fix

--- app/src/main/java/com/anytypeio/anytype/device/AnytypePushService.kt
+++ app/src/main/java/com/anytypeio/anytype/device/AnytypePushService.kt
@@ -1,24 +1,13 @@
 package com.anytypeio.anytype.device
 
 import com.anytypeio.anytype.app.AndroidApplication
-import com.anytypeio.anytype.di.main.ConfigModule.DEFAULT_APP_COROUTINE_SCOPE
-import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
 import com.anytypeio.anytype.domain.device.DeviceTokenStoringService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import javax.inject.Inject
-import javax.inject.Named
-import kotlinx.coroutines.CoroutineScope
 import timber.log.Timber
 
 class AnytypePushService : FirebaseMessagingService() {
-
-    @Inject
-    lateinit var dispatchers: AppCoroutineDispatchers
-
-    @Inject
-    @Named(DEFAULT_APP_COROUTINE_SCOPE)
-    lateinit var scope: CoroutineScope
 
     @Inject
     lateinit var deviceTokenSavingService: DeviceTokenStoringService
--- app/src/main/java/com/anytypeio/anytype/di/feature/notifications/PushDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/notifications/PushDI.kt
@@ -1,27 +1,16 @@
 package com.anytypeio.anytype.di.feature.notifications
 
-import android.content.Context
 import com.anytypeio.anytype.device.AnytypePushService
 import com.anytypeio.anytype.di.common.ComponentDependencies
-import com.anytypeio.anytype.di.main.ConfigModule.DEFAULT_APP_COROUTINE_SCOPE
-import com.anytypeio.anytype.domain.auth.repo.AuthRepository
-import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
-import com.anytypeio.anytype.domain.debugging.Logger
 import com.anytypeio.anytype.domain.device.DeviceTokenStoringService
-import com.anytypeio.anytype.domain.notifications.RegisterDeviceTokenUseCase
 import dagger.Component
 import dagger.Module
-import dagger.Provides
-import javax.inject.Named
 import javax.inject.Singleton
-import kotlinx.coroutines.CoroutineScope
 
 @Singleton
 @Component(
     dependencies = [PushContentDependencies::class],
-    modules = [
-        PushContentModule::class,
-        PushContentModule.Declarations::class],
+    modules = [PushContentModule::class],
 )
 interface PushContentComponent {
     @Component.Factory
@@ -35,32 +24,8 @@ interface PushContentComponent {
 @Module
 object PushContentModule {
 
-    @Singleton
-    @Provides
-    fun provideUseCase(
-        repository: AuthRepository,
-        dispatchers: AppCoroutineDispatchers
-    ): RegisterDeviceTokenUseCase {
-        return RegisterDeviceTokenUseCase(
-            repository = repository,
-            dispatchers = dispatchers
-        )
-    }
-
-    @Module
-    interface Declarations {
-
-    }
 }
 
 interface PushContentDependencies : ComponentDependencies {
-    fun context(): Context
-    fun logger(): Logger
-    fun authRepository(): AuthRepository
-    fun dispatchers(): AppCoroutineDispatchers
-
     fun deviceTokenSavingService(): DeviceTokenStoringService
-
-    @Named(DEFAULT_APP_COROUTINE_SCOPE)
-    fun coroutineScope(): CoroutineScope
 }

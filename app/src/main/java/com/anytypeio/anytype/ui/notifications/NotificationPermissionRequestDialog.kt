56125206622bb834c5bb3b9cb2b319d4cdbb7ca4
konstantiniiv <ki@anytype.io>
2025-05-12 13:34:48 +0200

    DROID-3654 dialog

--- app/src/main/java/com/anytypeio/anytype/ui/notifications/NotificationPermissionRequestDialog.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/notifications/NotificationPermissionRequestDialog.kt
@@ -8,17 +8,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.anytypeio.anytype.R
-import com.anytypeio.anytype.core_ui.foundation.Prompt
+import com.anytypeio.anytype.core_ui.views.ButtonPrimary
+import com.anytypeio.anytype.core_ui.views.ButtonSecondary
+import com.anytypeio.anytype.core_ui.views.ButtonSize
+import com.anytypeio.anytype.core_ui.views.HeadlineHeading
+import com.anytypeio.anytype.core_ui.views.Title2
 import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
 import com.anytypeio.anytype.presentation.notifications.NotificationPermissionManager
 import com.anytypeio.anytype.ui.settings.typography
@@ -40,77 +59,154 @@ class NotificationPermissionRequestDialog : BaseBottomSheetComposeFragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                val bottomSheetState = rememberModalBottomSheetState()
-                val scope = rememberCoroutineScope()
-
-                val launcher = rememberLauncherForActivityResult(
-                    ActivityResultContracts.RequestPermission()
-                ) { isGranted: Boolean ->
-                    Timber.d("Permission granted: $isGranted")
-                    activity?.onRequestPermissionsResult(
-                        REQUEST_CODE,
-                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-                        if (isGranted)
-                            intArrayOf(PackageManager.PERMISSION_GRANTED)
-                        else
-                            intArrayOf(PackageManager.PERMISSION_DENIED)
-                    )
-                    scope.launch {
-                        bottomSheetState.hide()
-                    }.invokeOnCompletion {
-                        if (isGranted) {
-                            permissionManager.onPermissionGranted()
-                        } else {
-                            permissionManager.onPermissionDenied()
-                        }
+                NotificationPermissionRequestContent(
+                    onPermissionRequested = { permissionManager.onPermissionRequested() },
+                    onPermissionGranted = { permissionManager.onPermissionGranted() },
+                    onPermissionDenied = { permissionManager.onPermissionDenied() },
+                    onPermissionDismissed = { permissionManager.onPermissionDismissed() },
+                    onRequestPermissionsResult = { requestCode, permissions, grantResults ->
+                        activity?.onRequestPermissionsResult(requestCode, permissions, grantResults)
                     }
-                }
-
-                MaterialTheme(typography = typography) {
-                    ModalBottomSheet(
-                        onDismissRequest = {
-                            scope.launch {
-                                bottomSheetState.hide()
-                            }.invokeOnCompletion {
-                                permissionManager.onPermissionDismissed()
-                            }
-                        },
-                        dragHandle = {},
-                        containerColor = colorResource(id = R.color.background_secondary),
-                        sheetState = bottomSheetState
-                    ) {
-                        Prompt(
-                            title = stringResource(R.string.notifications_prompt_get_notified),
-                            description = stringResource(R.string.notifications_prompt_description),
-                            primaryButtonText = stringResource(R.string.notifications_prompt_primary_button_text),
-                            secondaryButtonText = stringResource(R.string.notifications_prompt_secondary_button_text),
-                            onPrimaryButtonClicked = {
-                                permissionManager.onPermissionRequested()
-                                launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                            },
-                            onSecondaryButtonClicked = {
-                                scope.launch {
-                                    bottomSheetState.hide()
-                                }.invokeOnCompletion {
-                                    permissionManager.onPermissionDismissed()
-                                }
-                            }
-                        )
-                    }
-                }
+                )
             }
         }
     }
 
     override fun injectDependencies() {
-        // Do nothing
+        componentManager().notificationPermissionComponent.get().inject(this)
     }
 
     override fun releaseDependencies() {
-        // Do nothing
+        componentManager().notificationPermissionComponent.release()
     }
 
     companion object {
         const val REQUEST_CODE = 0
     }
-} 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NotificationPermissionRequestContent(
+    onPermissionRequested: () -> Unit,
+    onPermissionGranted: () -> Unit,
+    onPermissionDenied: () -> Unit,
+    onPermissionDismissed: () -> Unit,
+    onRequestPermissionsResult: (Int, Array<String>, IntArray) -> Unit
+) {
+    val bottomSheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        Timber.d("Permission granted: $isGranted")
+        onRequestPermissionsResult(
+            NotificationPermissionRequestDialog.REQUEST_CODE,
+            arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+            if (isGranted)
+                intArrayOf(PackageManager.PERMISSION_GRANTED)
+            else
+                intArrayOf(PackageManager.PERMISSION_DENIED)
+        )
+        scope.launch {
+            bottomSheetState.hide()
+        }.invokeOnCompletion {
+            if (isGranted) {
+                onPermissionGranted()
+            } else {
+                onPermissionDenied()
+            }
+        }
+    }
+
+    MaterialTheme(typography = typography) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                scope.launch {
+                    bottomSheetState.hide()
+                }.invokeOnCompletion {
+                    onPermissionDismissed()
+                }
+            },
+            dragHandle = {},
+            containerColor = colorResource(id = R.color.widget_background),
+            sheetState = bottomSheetState
+        ) {
+            NotificationPermissionContent(
+                onEnableNotifications = {
+                    onPermissionRequested()
+                    launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                },
+                onDismiss = {
+                    scope.launch {
+                        bottomSheetState.hide()
+                    }.invokeOnCompletion {
+                        onPermissionDismissed()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun NotificationPermissionContent(
+    onEnableNotifications: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .background(color = colorResource(id = R.color.widget_background)),
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth().height(232.dp),
+            painter = painterResource(id = R.drawable.push_modal_illustration),
+            contentDescription = "Push notifications illustration",
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.notifications_modal_title),
+            style = HeadlineHeading,
+            textAlign = TextAlign.Center,
+            color = colorResource(id = R.color.text_primary),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(R.string.notifications_modal_description),
+            style = Title2,
+            textAlign = TextAlign.Center,
+            color = colorResource(id = R.color.text_primary),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ButtonPrimary(
+            text = stringResource(R.string.notifications_modal_success_button),
+            onClick = onEnableNotifications,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            size = ButtonSize.Large,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonSecondary(
+            text = stringResource(R.string.notifications_modal_cancel_button),
+            onClick = onDismiss,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            size = ButtonSize.Large
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@DefaultPreviews
+@Composable
+fun NotificationPermissionRequestDialogPreview() {
+    NotificationPermissionRequestContent(
+        onPermissionRequested = {},
+        onPermissionGranted = {},
+        onPermissionDenied = {},
+        onPermissionDismissed = {},
+        onRequestPermissionsResult = { _, _, _ -> }
+    )
+}

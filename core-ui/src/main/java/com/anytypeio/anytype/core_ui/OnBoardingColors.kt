d62ecb5df474a2e95f1d94040f42a2d2b4da66d7
konstantiniiv <ki@anytype.io>
2025-05-12 19:14:45 +0200

    DROID-3629 colors

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/OnBoardingColors.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/OnBoardingColors.kt
@@ -11,12 +11,12 @@ val OnboardingSubtitleColor = Color(0xFF505050)
 val OnBoardingTextPrimaryColor = Color(0xFFF3F2EC)
 val OnBoardingTextSecondaryColor = Color(0x80F3F2EC)
 
-val ColorButtonPrimaryActive = Color(0xFF252525)
+val ColorButtonPrimaryActive = Color(0xFF3A3A3A)
 val ColorButtonPrimaryPressed = Color(0xFF464646)
 val ColorButtonPrimaryText = Color(0xFFF2F3EC)
 
-val ColorButtonPrimaryInactive = Color(0xFF1F1E1D)
-val ColorButtonPrimaryInactiveText = Color(0xFF64635B)
+val ColorButtonPrimaryInactive = Color(0xFF1F1F1F)
+val ColorButtonPrimaryInactiveText = Color(0xFF646464)
 
 val ColorButtonSecondaryActive = Color.Transparent
 val ColorButtonSecondaryPressed = Color(0xFF1F1E1C)

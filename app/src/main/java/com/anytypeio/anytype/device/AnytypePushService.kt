1e33e215990999bc7a37311838c5cda57cbbb787
konstantiniiv <ki@anytype.io>
2025-05-23 11:47:36 +0200

    DROID-3690 refactoring

--- app/src/main/java/com/anytypeio/anytype/device/AnytypePushService.kt
+++ app/src/main/java/com/anytypeio/anytype/device/AnytypePushService.kt
@@ -1,6 +1,7 @@
 package com.anytypeio.anytype.device
 
 import com.anytypeio.anytype.app.AndroidApplication
+import com.anytypeio.anytype.core_utils.ext.isAppInForeground
 import com.anytypeio.anytype.core_utils.ext.runSafely
 import com.anytypeio.anytype.domain.device.DeviceTokenStoringService
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -36,7 +37,7 @@ class AnytypePushService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Timber.d("Received message: $message")
-
+        
         // Skip showing notification if app is in foreground
         if (isAppInForeground()) {
             Timber.d("App is in foreground, skipping notification")
@@ -47,21 +48,6 @@ class AnytypePushService : FirebaseMessagingService() {
                 processor.process(message.data)
             }
         }
-    }
-
-    private fun isAppInForeground(): Boolean {
-        val activityManager = getSystemService(ACTIVITY_SERVICE) as android.app.ActivityManager
-        val appProcesses = activityManager.runningAppProcesses ?: return false
-
-        val packageName = packageName
-        for (appProcess in appProcesses) {
-            if (appProcess.importance == android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
-                && appProcess.processName == packageName
-            ) {
-                return true
-            }
-        }
-        return false
     }
 
     companion object {

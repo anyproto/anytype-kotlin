75de48df089a0af6f10e135516404b4fc6b95bba
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-05-24 15:35:46 +0200

    DROID-2966 fixes

--- app/src/main/java/com/anytypeio/anytype/ui/widgets/types/SpaceChatWidget.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/widgets/types/SpaceChatWidget.kt
@@ -1,10 +1,10 @@
 package com.anytypeio.anytype.ui.widgets.types
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,28 +18,40 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.anytypeio.anytype.R
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
 import com.anytypeio.anytype.core_ui.views.Caption1Regular
 import com.anytypeio.anytype.core_ui.views.HeadlineSubheading
 import com.anytypeio.anytype.presentation.home.InteractionMode
+import com.anytypeio.anytype.presentation.widgets.DropDownMenuAction
+import com.anytypeio.anytype.ui.widgets.menu.WidgetMenu
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SpaceChatWidgetCard(
     mode: InteractionMode,
     onWidgetClicked: () -> Unit = {},
+    onDropDownMenuAction: (DropDownMenuAction) -> Unit = {},
     unReadMessageCount: Int = 0,
     unReadMentionCount: Int = 0
 ) {
+    val isCardMenuExpanded = remember {
+        mutableStateOf(false)
+    }
+    val haptic = LocalHapticFeedback.current
     Row(
         modifier = Modifier
             .padding(start = 20.dp, end = 20.dp, top = 6.dp, bottom = 6.dp)
@@ -52,13 +64,21 @@ fun SpaceChatWidgetCard(
             .clip(RoundedCornerShape(16.dp))
             .then(
                 if (mode !is InteractionMode.Edit) {
-                    Modifier.clickable {
-                        onWidgetClicked()
-                    }
+                    Modifier.combinedClickable(
+                        onClick = onWidgetClicked,
+                        onLongClick = {
+                            haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                            isCardMenuExpanded.value = true
+                        }
+                    )
                 } else {
                     Modifier
                 }
-            ),
+            )
+            .alpha(
+                if (isCardMenuExpanded.value) 0.8f else 1f
+            )
+        ,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
@@ -122,6 +142,12 @@ fun SpaceChatWidgetCard(
             }
             Spacer(modifier = Modifier.width(16.dp))
         }
+        WidgetMenu(
+            isExpanded = isCardMenuExpanded,
+            onDropDownMenuAction = onDropDownMenuAction,
+            canEditWidgets = mode !is InteractionMode.Edit,
+            canChangeType = false
+        )
     }
 }
 

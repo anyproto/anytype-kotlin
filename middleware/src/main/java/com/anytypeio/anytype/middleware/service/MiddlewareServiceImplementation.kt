da7037aaf91b663145f4813a786ddf190bca06b1
konstantiniiv <ki@anytype.io>
2025-06-16 13:08:32 +0200

    DROID-3736 service

--- middleware/src/main/java/com/anytypeio/anytype/middleware/service/MiddlewareServiceImplementation.kt
+++ middleware/src/main/java/com/anytypeio/anytype/middleware/service/MiddlewareServiceImplementation.kt
@@ -2651,4 +2651,18 @@ class MiddlewareServiceImplementation @Inject constructor(
             return response
         }
     }
+
+    override fun setSpaceMode(request: Rpc.PushNotification.SetSpaceMode.Request): Rpc.PushNotification.SetSpaceMode.Response {
+        throw RuntimeException("Not implemented in middleware service")
+//        val encoded = Service.pushNotificationSetSpaceMode(
+//            Rpc.PushNotification.SetSpaceMode.Request.ADAPTER.encode(request)
+//        )
+//        val response = Rpc.PushNotification.SetSpaceMode.Response.ADAPTER.decode(encoded)
+//        val error = response.error
+//        if (error != null && error.code != Rpc.PushNotification.SetSpaceMode.Response.Error.Code.NULL) {
+//            throw Exception(error.description)
+//        } else {
+//            return response
+//        }
+    }
 }

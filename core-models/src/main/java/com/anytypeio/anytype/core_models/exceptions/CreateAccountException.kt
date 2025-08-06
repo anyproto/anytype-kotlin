fc1eb1a7c67f9b6d3405db5c988c8b2ee7dfcd15
konstantiniiv <ki@anytype.io>
2025-08-06 17:24:30 +0200

    DROID-3766 create account exceptions

--- core-models/src/main/java/com/anytypeio/anytype/core_models/exceptions/CreateAccountException.kt
+++ core-models/src/main/java/com/anytypeio/anytype/core_models/exceptions/CreateAccountException.kt
@@ -1,8 +1,16 @@
 package com.anytypeio.anytype.core_models.exceptions
 
 sealed class CreateAccountException : Exception() {
-    @Deprecated("To be delete")
-    object BadInviteCode : CreateAccountException()
-    object NetworkError: CreateAccountException()
-    object OfflineDevice: CreateAccountException()
+    // Account creation partial failures
+    data object AccountCreatedButFailedToStartNode: CreateAccountException()
+    data object AccountCreatedButFailedToSetName: CreateAccountException()
+    data object FailedToStopRunningNode: CreateAccountException()
+    data object FailedToWriteConfig: CreateAccountException()
+    data object FailedToCreateLocalRepo: CreateAccountException()
+    data object AccountCreationCanceled: CreateAccountException()
+    
+    // Config file errors
+    data object ConfigFileNotFound: CreateAccountException()
+    data object ConfigFileInvalid: CreateAccountException()
+    data object ConfigFileNetworkIdMismatch: CreateAccountException()
 }

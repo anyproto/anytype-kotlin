a1633f80fae6fdd734cc394e0d273107de065d37
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-08-07 15:33:50 +0200

    DROID-3767 fix

--- feature-chats/src/main/java/com/anytypeio/anytype/feature_chats/tools/Ext.kt
+++ feature-chats/src/main/java/com/anytypeio/anytype/feature_chats/tools/Ext.kt
@@ -7,16 +7,16 @@ import com.anytypeio.anytype.feature_chats.presentation.ChatView
 
 fun ObjectWrapper.Basic.syncStatus(): ChatView.Message.Attachment.SyncStatus {
     val code = getSingleValue<Double>(Relations.SYNC_STATUS)
-    if (code != null) {
+    return if (code != null) {
         val status = SyncStatus.fromCode(code.toInt())
         return when(status) {
-            SyncStatus.Syncing -> return ChatView.Message.Attachment.SyncStatus.Syncing
-            SyncStatus.Synced -> return ChatView.Message.Attachment.SyncStatus.Synced
-            SyncStatus.Error -> return ChatView.Message.Attachment.SyncStatus.Failed
-            SyncStatus.Queued -> return ChatView.Message.Attachment.SyncStatus.Syncing
+            SyncStatus.Syncing -> ChatView.Message.Attachment.SyncStatus.Syncing
+            SyncStatus.Synced -> ChatView.Message.Attachment.SyncStatus.Synced
+            SyncStatus.Error -> ChatView.Message.Attachment.SyncStatus.Failed
+            SyncStatus.Queued -> ChatView.Message.Attachment.SyncStatus.Syncing
             else -> ChatView.Message.Attachment.SyncStatus.Unknown
         }
     } else {
-        return ChatView.Message.Attachment.SyncStatus.Unknown
+        ChatView.Message.Attachment.SyncStatus.Unknown
     }
 }

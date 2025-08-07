0b733fa0d353da744424a15c67d0b9cd9541fef0
konstantiniiv <ki@anytype.io>
2025-08-07 16:58:34 +0200

    DROID-3897 move to fix

--- presentation/src/main/java/com/anytypeio/anytype/presentation/moving/MoveToViewModelFactory.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/moving/MoveToViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.anytypeio.anytype.presentation.moving
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.anytypeio.anytype.analytics.base.Analytics
-import com.anytypeio.anytype.domain.block.interactor.sets.GetObjectTypes
 import com.anytypeio.anytype.domain.misc.UrlBuilder
 import com.anytypeio.anytype.domain.objects.StoreOfObjectTypes
 import com.anytypeio.anytype.domain.primitives.FieldParser
@@ -13,7 +12,6 @@ import com.anytypeio.anytype.presentation.analytics.AnalyticSpaceHelperDelegate
 class MoveToViewModelFactory(
     private val vmParams: MoveToViewModel.VmParams,
     private val urlBuilder: UrlBuilder,
-    private val getObjectTypes: GetObjectTypes,
     private val searchObjects: SearchObjects,
     private val analytics: Analytics,
     private val analyticSpaceHelperDelegate: AnalyticSpaceHelperDelegate,
@@ -26,7 +24,6 @@ class MoveToViewModelFactory(
         return MoveToViewModel(
             vmParams = vmParams,
             urlBuilder = urlBuilder,
-            getObjectTypes = getObjectTypes,
             searchObjects = searchObjects,
             analytics = analytics,
             analyticSpaceHelperDelegate = analyticSpaceHelperDelegate,

20c935b7d6e78ff3642b57a7fb79bd327e86903f
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2025-08-21 12:21:35 +0200

    DROID-3935 Onboarding | Ui | Spalsh screen, bottom text (#2700)

--- app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingScreenContract.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/onboarding/OnboardingScreenContract.kt
@@ -2,11 +2,8 @@ package com.anytypeio.anytype.ui.onboarding
 
 enum class OnboardingPage(val num: Int, val visible: Boolean) {
     AUTH(0, false),
-    VOID(1, true),
     MNEMONIC(2, false),
     SET_PROFILE_NAME(1, false),
-    SOUL_CREATION_ANIM(4, false),
     RECOVERY(5, false),
-    ENTER_THE_VOID(6, false),
     SET_EMAIL(3, false)
 }

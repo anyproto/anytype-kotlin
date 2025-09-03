0ff55bb54249efce74efcc65023d3c0468ef362d
konstantiniiv <ki@anytype.io>
2025-09-03 17:26:05 +0200

    DROID-3944 new link button

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/views/DesignOnboardingButtons.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/views/DesignOnboardingButtons.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -113,6 +114,58 @@ fun ButtonOnboardingSecondaryLarge(
                     backgroundColor = backgroundColor,
                     contentColor = colorResource(id = R.color.text_primary),
                     disabledBackgroundColor = colorResource(id = R.color.shape_tertiary),
+                    disabledContentColor = colorResource(id = R.color.text_tertiary)
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp
+                ),
+                contentPadding = PaddingValues(0.dp, 14.dp, 0.dp, 14.dp)
+            ) {
+                Text(
+                    text = if (loading) "" else text,
+                    modifier = Modifier,
+                    style = ButtonRegular,
+                    textAlign = TextAlign.Center
+                )
+            }
+            DotsLoadingIndicator(
+                animating = loading,
+                modifier = Modifier.graphicsLayer { alpha = loadingAlpha },
+                animationSpecs = FadeAnimationSpecs(itemCount = loadingItemsCount),
+                color = colorResource(id = R.color.text_white),
+                size = size
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ButtonOnboardingLinkLarge(
+    text: String = "",
+    modifierBox: Modifier = Modifier,
+    size: ButtonSize = ButtonSize.Large,
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
+    loading: Boolean = false,
+    loadingItemsCount: Int = 3
+) {
+    val loadingAlpha by animateFloatAsState(targetValue = if (loading) 1f else 0f)
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    CompositionLocalProvider(LocalRippleConfiguration provides null) {
+        Box(modifier = modifierBox, contentAlignment = Alignment.Center) {
+            Button(
+                onClick = { if (!loading) onClick() },
+                interactionSource = interactionSource,
+                enabled = enabled,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent,
+                    contentColor = colorResource(id = R.color.text_primary),
+                    disabledBackgroundColor = Color.Transparent,
                     disabledContentColor = colorResource(id = R.color.text_tertiary)
                 ),
                 modifier = Modifier.fillMaxWidth(),

cd0b832f29055e9a1dea28a3ede0eaf584d6291c
konstantiniiv <ki@anytype.io>
2025-10-01 16:59:09 +0200

    DROID-3944 button fixes

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/views/TypographyCompose.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/views/TypographyCompose.kt
@@ -323,7 +323,7 @@ val ButtonMedium = TextStyle(
     fontWeight = FontWeight.W500,
     fontSize = 17.sp,
     lineHeight = 24.sp,
-    letterSpacing = (-0.024).em
+    letterSpacing = (-0.41).sp
 )
 
 //UX/Button/Regular
@@ -332,7 +332,7 @@ val ButtonRegular = TextStyle(
     fontWeight = FontWeight.W400,
     fontSize = 17.sp,
     lineHeight = 24.sp,
-    letterSpacing = (-0.024).em
+    letterSpacing = (-0.41).sp
 )
 
 val ModalTitle = TextStyle(

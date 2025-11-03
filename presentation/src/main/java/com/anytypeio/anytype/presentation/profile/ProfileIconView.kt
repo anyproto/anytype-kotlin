3f305f17b48ada9c938431662fdca81247590e26
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2025-11-03 12:41:45 +0100

    DROID-4073 Account settings | Adds user identity and global name display functionality to the profile settings screen (#2852)

--- presentation/src/main/java/com/anytypeio/anytype/presentation/profile/ProfileIconView.kt
+++ presentation/src/main/java/com/anytypeio/anytype/presentation/profile/ProfileIconView.kt
@@ -24,6 +24,8 @@ sealed class AccountProfile {
     data object Idle: AccountProfile()
     class Data(
         val name: String,
-        val icon: ProfileIconView
+        val icon: ProfileIconView,
+        val identity: String? = null,
+        val globalName: String? = null
     ): AccountProfile()
 }

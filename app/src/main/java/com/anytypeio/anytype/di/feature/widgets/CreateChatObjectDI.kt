c19be9da0e70bd714685dc80003dd318d2707836
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-11-11 17:24:33 +0100

    DROID-4120 fixes

---
+++ app/src/main/java/com/anytypeio/anytype/di/feature/widgets/CreateChatObjectDI.kt
@@ -0,0 +1,50 @@
+package com.anytypeio.anytype.di.feature.widgets
+
+import androidx.lifecycle.ViewModelProvider
+import com.anytypeio.anytype.core_utils.di.scope.PerScreen
+import com.anytypeio.anytype.di.common.ComponentDependencies
+import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
+import com.anytypeio.anytype.domain.block.repo.BlockRepository
+import com.anytypeio.anytype.domain.config.UserSettingsRepository
+import com.anytypeio.anytype.presentation.widgets.CreateChatObjectViewModel
+import com.anytypeio.anytype.ui.widgets.CreateChatObjectFragment
+import dagger.Binds
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+
+@Component(
+    dependencies = [CreateChatObjectDependencies::class],
+    modules = [
+        CreateChatObjectModule::class,
+        CreateChatObjectModule.Declarations::class
+    ]
+)
+@PerScreen
+interface CreateChatObjectComponent {
+    @Component.Factory
+    interface Builder {
+        fun create(
+            @BindsInstance vmParams: CreateChatObjectViewModel.VmParams,
+            dependencies: CreateChatObjectDependencies
+        ): CreateChatObjectComponent
+    }
+
+    fun inject(fragment: CreateChatObjectFragment)
+}
+
+@Module
+object CreateChatObjectModule {
+    @Module
+    interface Declarations {
+        @Binds
+        @PerScreen
+        fun bindViewModelFactory(factory: CreateChatObjectViewModel.Factory): ViewModelProvider.Factory
+    }
+}
+
+interface CreateChatObjectDependencies : ComponentDependencies {
+    fun repo(): BlockRepository
+    fun dispatchers(): AppCoroutineDispatchers
+    fun settings(): UserSettingsRepository
+}

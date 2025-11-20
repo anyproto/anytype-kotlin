22f3ab2c870bf89bb82bd092fa1a3c05cffbe171
konstantiniiv <ki@anytype.io>
2025-11-20 17:22:52 +0100

    DROID-4127 import

--- feature-ui-settings/src/main/java/com/anytypeio/anytype/ui_settings/space/new_settings/NotificationsPreferenceSheet.kt
+++ feature-ui-settings/src/main/java/com/anytypeio/anytype/ui_settings/space/new_settings/NotificationsPreferenceSheet.kt
@@ -1,6 +1,5 @@
 package com.anytypeio.anytype.ui_settings.space.new_settings
 
-import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -44,7 +43,6 @@ import com.anytypeio.anytype.core_ui.views.Relations3
 import com.anytypeio.anytype.core_ui.views.Title1
 import com.anytypeio.anytype.core_ui.views.Title2
 import com.anytypeio.anytype.core_ui.widgets.ListWidgetObjectIcon
-import com.anytypeio.anytype.core_utils.insets.EDGE_TO_EDGE_MIN_SDK
 import com.anytypeio.anytype.presentation.objects.ObjectIcon
 import com.anytypeio.anytype.presentation.spaces.ChatNotificationItem
 import com.anytypeio.anytype.presentation.spaces.UiEvent

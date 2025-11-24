a161aa084e1b905dcb8ec70f4801dc3cdbb0a469
konstantiniiv <ki@anytype.io>
2025-11-24 18:28:45 +0100

    DROID-4023 keyboard action

--- feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/ui/header/DescriptionWidget.kt
+++ feature-object-type/src/main/java/com/anytypeio/anytype/feature_object_type/ui/header/DescriptionWidget.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,9 +13,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import com.anytypeio.anytype.core_ui.R
 import com.anytypeio.anytype.core_ui.common.DefaultPreviews
 import com.anytypeio.anytype.core_ui.views.Relations1
@@ -35,6 +41,11 @@ fun DescriptionWidget(
     var text by remember(uiDescriptionState.description) {
         mutableStateOf(uiDescriptionState.description)
     }
+
+    val focusManager = LocalFocusManager.current
+
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     BasicTextField(
         value = text,
@@ -59,7 +70,14 @@ fun DescriptionWidget(
                 }
                 innerTextField()
             }
-        }
+        },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions {
+            keyboardController?.hide()
+            focusManager.clearFocus()
+        },
     )
 }
 

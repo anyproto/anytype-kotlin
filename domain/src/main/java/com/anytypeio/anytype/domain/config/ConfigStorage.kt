104f4f320fc1aa1f6bb7473fdd95785b9c645438
konstantiniiv <ki@anytype.io>
2025-12-29 16:18:36 +0100

    DROID-4258 Add accountId to ConfigStorage interface and CacheStorage implementation

--- domain/src/main/java/com/anytypeio/anytype/domain/config/ConfigStorage.kt
+++ domain/src/main/java/com/anytypeio/anytype/domain/config/ConfigStorage.kt
@@ -8,20 +8,27 @@ interface ConfigStorage : TechSpaceProvider {
     @Deprecated("Unsafe method. Use getOrNull() instead")
     @Throws(IllegalStateException::class)
     fun get(): Config
+    fun getAccountId(): String?
     fun getOrNull(): Config?
-    fun set(config: Config)
+    fun set(config: Config, accountId: String)
     fun clear()
     class CacheStorage : ConfigStorage {
         private var instance: Config? = null
+        private var accountId: String? = null
 
         override fun getOrNull(): Config? = instance
+
+        override fun getAccountId(): String? {
+            return accountId
+        }
 
         override fun get(): Config {
             return instance ?: throw IllegalStateException("Config is not initialized")
         }
 
-        override fun set(config: Config) {
+        override fun set(config: Config, accountId: String) {
             instance = config
+            this.accountId = accountId
         }
 
         override fun clear() {

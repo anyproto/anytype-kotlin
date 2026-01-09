129de25f99fe5277a88ff6fa80d38b9844c9054d
konstantiniiv <ki@anytype.io>
2026-01-09 13:57:59 +0100

    DROID-4282 Refactor ObjectTypeDI to remove GetDefaultObjectType provider and simplify dependency injection

--- app/src/main/java/com/anytypeio/anytype/di/feature/ObjectTypeDI.kt
+++ app/src/main/java/com/anytypeio/anytype/di/feature/ObjectTypeDI.kt
@@ -1,10 +1,6 @@
 package com.anytypeio.anytype.di.feature
 
 import com.anytypeio.anytype.core_utils.di.scope.PerScreen
-import com.anytypeio.anytype.domain.base.AppCoroutineDispatchers
-import com.anytypeio.anytype.domain.block.repo.BlockRepository
-import com.anytypeio.anytype.domain.config.UserSettingsRepository
-import com.anytypeio.anytype.domain.launch.GetDefaultObjectType
 import com.anytypeio.anytype.presentation.objects.ObjectTypeChangeViewModel
 import com.anytypeio.anytype.ui.objects.types.pickers.AppDefaultObjectTypeFragment
 import com.anytypeio.anytype.ui.objects.types.pickers.DataViewSelectSourceFragment
@@ -12,7 +8,6 @@ import com.anytypeio.anytype.ui.objects.types.pickers.EmptyDataViewSelectSourceF
 import com.anytypeio.anytype.ui.objects.types.pickers.ObjectSelectTypeFragment
 import dagger.BindsInstance
 import dagger.Module
-import dagger.Provides
 import dagger.Subcomponent
 
 @Subcomponent(modules = [ObjectTypeChangeModule::class])
@@ -34,18 +29,5 @@ interface ObjectTypeChangeSubComponent {
 
 @Module
 object ObjectTypeChangeModule {
-
-    @JvmStatic
-    @PerScreen
-    @Provides
-    fun provideGetDefaultPageType(
-        userSettingsRepository: UserSettingsRepository,
-        blockRepository: BlockRepository,
-        dispatchers: AppCoroutineDispatchers
-    ): GetDefaultObjectType = GetDefaultObjectType(
-        userSettingsRepository = userSettingsRepository,
-        blockRepository = blockRepository,
-        dispatchers = dispatchers
-    )
 
 }

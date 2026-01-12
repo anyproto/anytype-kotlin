fa61292e857d890c7484af5ae2b84ef9f27c45a2
konstantiniiv <ki@anytype.io>
2026-01-12 10:14:56 +0100

    DROID-4282 Object type | Add CollectionAddObjectTypeFragment for selecting object types in collections

---
+++ app/src/main/java/com/anytypeio/anytype/ui/objects/types/pickers/CollectionAddObjectTypeFragment.kt
@@ -0,0 +1,49 @@
+package com.anytypeio.anytype.ui.objects.types.pickers
+
+import androidx.core.os.bundleOf
+import com.anytypeio.anytype.R
+import com.anytypeio.anytype.core_models.Id
+import com.anytypeio.anytype.core_models.ObjectWrapper
+import com.anytypeio.anytype.core_models.primitives.SpaceId
+import com.anytypeio.anytype.core_utils.ext.withParentSafe
+import com.anytypeio.anytype.di.common.componentManager
+import com.anytypeio.anytype.presentation.objects.ObjectTypeChangeViewModel
+import com.anytypeio.anytype.ui.objects.BaseObjectTypeChangeFragment
+
+/**
+ * Selecting an object type to create when adding a new object to a collection.
+ * Includes list types (Set, Collection) and bookmarks.
+ * Excludes media types (Image, File, Video, Audio) and templates.
+ */
+class CollectionAddObjectTypeFragment : BaseObjectTypeChangeFragment() {
+
+    override fun onItemClicked(item: ObjectWrapper.Type) {
+        withParentSafe<ObjectTypeSelectionListener> {
+            onSelectObjectType(objType = item)
+        }
+    }
+
+    override fun resolveTitle(): String = getString(R.string.change_type)
+
+    override fun injectDependencies() {
+        val params = ObjectTypeChangeViewModel.VmParams(
+            spaceId = SpaceId(space),
+            screen = ObjectTypeChangeViewModel.Screen.CREATE_OBJECT_FOR_COLLECTION,
+            excludeTypes = excludeTypes
+        )
+        componentManager().objectTypeChangeComponent.get(params).inject(this)
+    }
+
+    override fun releaseDependencies() {
+        componentManager().objectTypeChangeComponent.release()
+    }
+
+    companion object {
+        fun newInstance(space: Id, excludeTypes: List<Id>) = CollectionAddObjectTypeFragment().apply {
+            arguments = bundleOf(
+                ARG_SPACE to space,
+                ARG_EXCLUDE_TYPES to excludeTypes
+            )
+        }
+    }
+}

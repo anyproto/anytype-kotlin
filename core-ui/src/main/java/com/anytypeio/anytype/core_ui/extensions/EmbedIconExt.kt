119e0a58fd916422a7986990f502b528e8eec264
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2026-01-22 00:38:30 +0100

    DROID-4153 Editor | Fix | Added embed block icons (#3081)

---
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/extensions/EmbedIconExt.kt
@@ -0,0 +1,39 @@
+package com.anytypeio.anytype.core_ui.extensions
+
+import androidx.annotation.DrawableRes
+import com.anytypeio.anytype.core_ui.R
+
+/**
+ * Maps embed processor display name to its corresponding icon drawable resource.
+ * Returns the generic embed icon as a fallback if no specific icon is found.
+ */
+@DrawableRes
+fun String.toEmbedIconResource(): Int {
+    return when (this) {
+        "Mermaid" -> R.drawable.ic_embed_mermaid
+        "Chart" -> R.drawable.ic_embed_chart
+        "YouTube" -> R.drawable.ic_embed_youtube
+        "Vimeo" -> R.drawable.ic_embed_vimeo
+        "SoundCloud" -> R.drawable.ic_embed_soundcloud
+        "Google Maps" -> R.drawable.ic_embed_google_maps
+        "Miro" -> R.drawable.ic_embed_miro
+        "Figma" -> R.drawable.ic_embed_figma
+        "Twitter" -> R.drawable.ic_embed_twitter
+        "OpenStreetMap" -> R.drawable.ic_embed_open_street_map
+        "Reddit" -> R.drawable.ic_embed_reddit
+        "Facebook" -> R.drawable.ic_embed_facebook
+        "Instagram" -> R.drawable.ic_embed_generic_embed_icon // Instagram icon not available (PDF format)
+        "Telegram" -> R.drawable.ic_embed_telegram
+        "GitHub Gist" -> R.drawable.ic_embed_github_gist
+        "CodePen" -> R.drawable.ic_embed_codepen
+        "Bilibili" -> R.drawable.ic_embed_bilibili
+        "Excalidraw" -> R.drawable.ic_embed_excalidraw
+        "Kroki" -> R.drawable.ic_embed_kroki
+        "Graphviz" -> R.drawable.ic_embed_graphviz
+        "Sketchfab" -> R.drawable.ic_embed_sketchfab
+        "Image" -> R.drawable.ic_embed_external_image
+        "Draw.io" -> R.drawable.ic_embed_drawio
+        "Spotify" -> R.drawable.ic_embed_spotify
+        else -> R.drawable.ic_embed_generic_embed_icon
+    }
+}

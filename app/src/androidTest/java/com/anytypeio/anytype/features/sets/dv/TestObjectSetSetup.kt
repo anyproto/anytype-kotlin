37c0de9de4f362db09f6ade46f8496cb1d0cf0ac
konstantiniiv <ki@anytype.io>
2026-01-28 11:08:22 +0100

    DROID-4312 Refactor | Add getDefaultObjectType to TestObjectSetSetup

--- app/src/androidTest/java/com/anytypeio/anytype/features/sets/dv/TestObjectSetSetup.kt
+++ app/src/androidTest/java/com/anytypeio/anytype/features/sets/dv/TestObjectSetSetup.kt
@@ -352,7 +352,8 @@ abstract class TestObjectSetSetup {
             setDataViewProperties = mock(),
             createBlock = mock(),
             emojiProvider = mock(),
-            emojiSuggester = mock()
+            emojiSuggester = mock(),
+            getDefaultObjectType = getDefaultObjectType
         )
 
         Mockito.`when`(localeProvider.locale()).thenReturn(Locale.getDefault())

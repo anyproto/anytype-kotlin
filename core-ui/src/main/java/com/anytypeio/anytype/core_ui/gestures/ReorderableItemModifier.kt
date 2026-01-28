bcc74dfbe5114c6ae0dd1a10ef381582858cc4ec
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2026-01-28 08:20:13 +0100

    DROID-4309 fix

--- core-ui/src/main/java/com/anytypeio/anytype/core_ui/gestures/ReorderableItemModifier.kt
+++ core-ui/src/main/java/com/anytypeio/anytype/core_ui/gestures/ReorderableItemModifier.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.view.HapticFeedbackConstantsCompat
 import androidx.core.view.ViewCompat

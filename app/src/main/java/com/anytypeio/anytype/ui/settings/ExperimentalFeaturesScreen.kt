d5a92e982193abf457ad296f21380b8ca9df44cb
Konstantin Ivanov <54908981+konstantiniiv@users.noreply.github.com>
2026-03-25 06:56:24 +0100

    status bar padding

--- app/src/main/java/com/anytypeio/anytype/ui/settings/ExperimentalFeaturesScreen.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/settings/ExperimentalFeaturesScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,6 +36,7 @@ fun ExperimentalFeaturesScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .statusBarsPadding()
             .background(
                 color = colorResource(R.color.background_secondary),
                 shape = RoundedCornerShape(

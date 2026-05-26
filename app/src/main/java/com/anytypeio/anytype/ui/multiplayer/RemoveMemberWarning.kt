fdf3d84b5958d6afa89ac93ec4dabe23240eac26
konstantiniiv <ki@anytype.io>
2026-05-26 14:02:13 +0200

    DROID-4250 Chat Moderation | Replace Warning component with TwoVerticalButtonsAlert for member removal confirmation

--- app/src/main/java/com/anytypeio/anytype/ui/multiplayer/RemoveMemberWarning.kt
+++ app/src/main/java/com/anytypeio/anytype/ui/multiplayer/RemoveMemberWarning.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.core.os.bundleOf
 import com.anytypeio.anytype.R
-import com.anytypeio.anytype.core_ui.foundation.Warning
+import com.anytypeio.anytype.core_ui.foundation.TwoVerticalButtonsAlert
 import com.anytypeio.anytype.core_utils.ext.arg
 import com.anytypeio.anytype.core_utils.ui.BaseBottomSheetComposeFragment
 import com.anytypeio.anytype.ui.settings.typography
@@ -31,20 +31,18 @@ class RemoveMemberWarning : BaseBottomSheetComposeFragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 MaterialTheme(typography = typography) {
-                    Warning(
-                        actionButtonText = stringResource(R.string.remove),
-                        cancelButtonText = stringResource(R.string.cancel),
-                        title = stringResource(R.string.multiplayer_remove_member),
-                        subtitle = stringResource(
-                            id = R.string.multiplayer_remove_member_warning_text,
-                            name
-                        ),
-                        onNegativeClick = {
+                    TwoVerticalButtonsAlert(
+                        icon = com.anytypeio.anytype.core_ui.R.drawable.ic_popup_remove_member_56,
+                        title = stringResource(R.string.multiplayer_remove_member_title),
+                        description = stringResource(R.string.multiplayer_remove_member_description),
+                        actionText = stringResource(R.string.multiplayer_remove_member_button),
+                        cancelText = stringResource(R.string.cancel),
+                        isActionDestructive = true,
+                        onActionClicked = { onAccepted() },
+                        onCancelClicked = {
                             onCancelled()
                             dismiss()
-                        },
-                        onPositiveClick = { onAccepted() },
-                        isInProgress = false
+                        }
                     )
                 }
             }
